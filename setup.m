8c1b4d8e1d44fde51d6c2b74ddfeec3164efdc82
Anton Petrov <anton.i.petrov@gmail.com>
2011-12-27 12:10:55 -0500

    Create setup.m

---
+++ setup.m
@@ -0,0 +1,9 @@
+function [] = setup()
+
+    addpath(genpath('FR3DSource'));
+    addpath(genpath('FR3DDevelopment'));
+    addpath(genpath('FR3DMotifs'));
+    addpath(genpath('PrecomputedData'));
+    addpath(genpath('PDBFiles'));
+
+end
