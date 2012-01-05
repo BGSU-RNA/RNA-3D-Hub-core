386e063ba584d25993abbabba57637a0607c80ae
Anton Petrov <anton.i.petrov@gmail.com>
2012-01-04 20:36:55 -0500

    Save mat files passed by python using the File.Filename attribute

---
+++ FR3DMotifs/aSaveLoops.m
@@ -0,0 +1,25 @@
+function [status,err_msg] = aSaveLoops(Loops, save_location)
+
+    try
+        status  = 0;
+        err_msg = '';
+
+        % save loops from each pdb in a separate folder
+        save_location = fullfile(save_location, Loops(1).Filename(4:7));
+        if ~exist(save_location,'dir'), mkdir(save_location); end
+        
+        N = length(Loops);
+        
+        for i = 1:N
+            File = Loops(i);
+            filename = fullfile(save_location, File.Filename);
+            save(filename,'File');
+        end
+
+    catch err
+        err_msg = sprintf('Error "%s" in aGetCoordinates on line %i\n', err.message, err.stack.line);
+        disp(err_msg);
+        status = 1;        
+    end
+
+end
