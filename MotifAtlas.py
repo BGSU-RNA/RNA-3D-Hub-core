d08876934e32f901763d036c1503b7405f8a4d52
Anton Petrov <anton.i.petrov@gmail.com>
2012-01-06 17:08:10 -0500

    Minor refactoring after experimenting with pylint

--- MotifAtlas.py
+++ MotifAtlas.py
@@ -8,7 +8,6 @@ __author__ = 'Anton Petrov'
 
 import sys, getopt, logging
 
-from MLSqlAlchemyClasses import *
 from DistancesAndCoordinatesLoader import DistancesAndCoordinatesLoader
 from PdbInfoLoader import PdbInfoLoader
 from LoopExtractor import LoopExtractor
@@ -29,19 +28,19 @@ def main(argv):
 #     pdbs = ['1EKA','1HLX']#,'1S72','2AVY']
 
     """get new pdb files, import descriptions into the database"""
-    P = PdbInfoLoader()
-    P.get_all_rna_pdbs()
+    p = PdbInfoLoader()
+    p.get_all_rna_pdbs()
 #     P.update_rna_containing_pdbs()
     """import coordinates and distances into the database"""
-    D = DistancesAndCoordinatesLoader()
-    D.import_distances(P.pdbs[1:200])
-    D.import_coordinates(P.pdbs[1:200])
+    d = DistancesAndCoordinatesLoader()
+    d.import_distances(P.pdbs[1:200])
+    d.import_coordinates(P.pdbs[1:200])
     """extract all loops and import into the database"""
-    E = LoopExtractor()
-    E.extract_and_import_loops(P.pdbs[1:200])
+    e = LoopExtractor()
+    e.extract_and_import_loops(P.pdbs[1:200])
     """do loop QA, import into the database"""
-    Q = LoopQualityChecker()
-    Q.check_loop_quality(P.pdbs[1:200])
+    q = LoopQualityChecker()
+    q.check_loop_quality(P.pdbs[1:200])
 
     # compute new non-redundant lists, import into the database
 
