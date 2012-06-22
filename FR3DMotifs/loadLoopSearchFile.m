95b52585eabdc437efd31c754d325b063d005ab2
Anton Petrov <anton.i.petrov@gmail.com>
2012-06-22 11:02:18 -0400

    Bug fixes and speedups

--- FR3DMotifs/loadLoopSearchFile.m
+++ FR3DMotifs/loadLoopSearchFile.m
@@ -5,7 +5,7 @@
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function [filename, err_msg] = loadLoopSearchFile_folder(input_folder)
+function [filename, err_msg] = loadLoopSearchFile(input_folder)
     
     filename = '';
     err_msg = '';
@@ -16,34 +16,39 @@ function [filename, err_msg] = loadLoopSearchFile_folder(input_folder)
         fid = fopen(filename, 'w');        
         
         files = dir([input_folder filesep '*.mat']);
-        
+                
         for f = 1:length(files)
             
             load(fullfile(input_folder, files(f).name));
             
             disc = Search.Discrepancy;
             if disc < 0.001
-                disc = 0; %
-            end            
-            
-            % IL_1S72_001
-            Search.Query.PDBFilename = Search.Query.Filename(4:7); % aGetNTId needs this field
-            Search.File.PDBFilename  = Search.Query.Filename(4:7); % aGetNTId needs this field
+                disc = 0;
+            end
 
             loop_id1 = Search.Query.Filename;
             loop_id2 = Search.Query.SearchFiles;
+            
+            % figure out which pdb file each loop came from
+            if f == 1
+                load(getPrecomputedDataAddress(loop_id1));
+                loop_id1_pdb = File.PDBFilename;
+            end                
+            Search.Query.PDBFilename = loop_id1_pdb;
+            load(getPrecomputedDataAddress(loop_id2));
+            Search.File.PDBFilename = File.PDBFilename;            
 
             nt_list1 = '';                
             for i = 1:length(Search.Query.NT)
                 nt_list1 = strcat(nt_list1, ',', aGetNTId(Search.Query, i));            
             end
-            nt_list1 = nt_list1(2:end-1); % remove the first and the last commas
+            nt_list1 = nt_list1(2:end); % remove the last comma
 
             nt_list2 = '';
             for i = 1:length(Search.Candidates(1,1:end-1))
                 nt_list2 = strcat(nt_list2, ',', aGetNTId(Search.File, i));
             end
-            nt_list2 = nt_list2(2:end-1); % remove the first and the last commas
+            nt_list2 = nt_list2(2:end); % remove the last comma
             
             fprintf(fid,'"%s","%s","%f","%s","%s"\n', loop_id1, loop_id2, disc, nt_list1, nt_list2);
             
