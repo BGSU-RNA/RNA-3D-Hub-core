93d558d8175bc5ae9252b63a61fc9c6619a56d96
Anton Petrov <anton.i.petrov@gmail.com>
2012-08-29 16:17:31 -0400

    Import best chains and best models from zBestChains and zBestModels

---
+++ FR3DMotifs/loadBestChainsAndModels.m
@@ -0,0 +1,28 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Used in conjunction with .py to import information
+% about best chains and models within pdb files into the database.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [best_chains, best_models, err_msg] = loadBestChainsAndModels(pdb_id)
+    
+    best_chains = '';
+    best_models = '';
+    err_msg = '';
+    
+    try
+        F = zAddNTData(pdb_id);                   
+                
+        % cell array containing a string without separators
+        best_chains = cell2mat(zBestChains(F));
+        
+        % cell array with integer arrays
+        best_models = num2str(cell2mat(zBestModels(F)));
+        
+    catch err
+        err_msg = sprintf('Error "%s" on line %i\n', err.message, err.stack.line);
+        disp(err_msg);      
+    end    
+            
+end
