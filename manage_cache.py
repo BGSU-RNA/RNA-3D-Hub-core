deaeffab95c472dcdef4b84e0ed325579206ff50
Anton Petrov <anton.i.petrov@gmail.com>
2012-09-21 16:14:07 -0400

    Multiple improvements

--- manage_cache.py
+++ manage_cache.py
@@ -3,8 +3,10 @@
 A python script for managing the RNA 3D Hub cache.
 
 Usage:
-python manage_cache.py rna3dhub
-python manage_cache.py rna3dhub_dev
+python manage_cache.py rna3dhub [option]
+python manage_cache.py rna3dhub_dev [option]
+
+where [option] is nrlist/motifs/loops/pdb
 
 """
 
@@ -28,14 +30,18 @@ class CacheManager():
             env = rna3dhub or rna3dhub_dev
         """
         self.cache_dir = os.path.join('/Servers', 'rna.bgsu.edu', env ,'application', 'cache')
+        if not os.path.exists(self.cache_dir):
+            os.mkdir(self.cache_dir)
         self.baseurl = 'http://rna.bgsu.edu/' + env
 
-    def update_pdb_cache(self, pdbs):
+    def update_pdb_cache(self):
         """
-           get a list of pdb files, loop over them, delete existing cache and
-           request a new version
+           get a list of pdb files, loop over them, refresh cache
         """
-        for pdb in pdbs:
+        self._refresh_cache('pdb')
+        P = PdbInfoLoader()
+        P.get_all_rna_pdbs()
+        for pdb in P.pdbs:
             logging.info(pdb)
             subpages = [pdb, pdb + '/motifs']
             for subpage in subpages:
@@ -43,14 +49,18 @@ class CacheManager():
 
     def update_nrlist_cache(self):
         """
-            get all nr_releases and nr_classes. Loop over and refresh cache
+            get all nr_releases and nr_classes, then refresh cache
         """
         # homepage
         self._refresh_cache('nrlist')
+        self._refresh_cache('nrlist/release/current')
 
-        # http://rna.bgsu.edu/rna3dhub/nrlist/release/0.87
+        # http://rna.bgsu.edu/rna3dhub/nrlist/release/0.87/1.5A
+        resolutions = ['1.5A', '2.0A', '2.5A', '3.0A', '3.5A', '4.0A', '20.0A', 'all']
         for release in session.query(NR_release).all():
             self._refresh_cache('nrlist/release/' + release.id)
+            for resolution in resolutions:
+                self._refresh_cache('nrlist/release/' + release.id + '/' + resolution)
 
         # http://rna.bgsu.edu/rna3dhub/nrlist/view/NR_4.0_81883.10
         for eq_class in session.query(distinct(NR_class.id)).all():
@@ -58,13 +68,15 @@ class CacheManager():
 
     def update_loop_cache(self):
         """
+            Delete old cache so that the pages can be regenerated with new data.
         """
         # http://rna.bgsu.edu/rna3dhub/loops/view/IL_157D_001
         for loop in session.query(AllLoops).all():
-            self._refresh_cache('loops/view/' + loop.id)
+            self._refresh_cache('loops/view/' + loop.id, False)
 
-    def update_motifs_cache(self):
+    def update_motif_cache(self):
         """
+            Refresh cache
         """
         # http://rna.bgsu.edu/rna3dhub/motifs/release/IL/current
         self._refresh_cache('motifs/release/IL/current')
@@ -77,15 +89,16 @@ class CacheManager():
 
         # http://rna.bgsu.edu/rna3dhub/motif/view/IL_44742.1
         for motif in session.query(distinct(Motif.id)).all():
-            self._refresh_cache('motifs/view' + motif.id)
+            self._refresh_cache('motif/view/' + motif[0])
 
-    def _refresh_cache(self, uri_string):
+    def _refresh_cache(self, uri_string, reload=True):
         """
             pdb/ + subpage
              NB! due to a bug in CodeIgniter, the urls are cached as
             'http://rna.bgsu.edu/rna3dhubpdb/1S72'
             no leading slash in uri_string
             example: pdb/1S72/motifs
+            Reloads the page by default.
         """
         ci_url = self.baseurl + uri_string
         url = self.baseurl + '/' + uri_string
@@ -97,10 +110,11 @@ class CacheManager():
             os.remove(cached_file)
         else:
             logging.info('File %s doesnt exist' % cached_file)
-        try:
-            response = urllib2.urlopen( url )
-        except:
-            logging.error('Failed to retrieve %s' % url)
+        if reload:
+            try:
+                response = urllib2.urlopen( url )
+            except:
+                logging.error('Failed to retrieve %s' % url)
 
 
 def main(argv):
@@ -108,15 +122,18 @@ def main(argv):
     """
     logging.basicConfig(level=logging.DEBUG)
 
-    P = PdbInfoLoader()
-    P.get_all_rna_pdbs()
-
     C = CacheManager( argv[0] )
 
-    C.update_pdb_cache( P.pdbs )
-    C.update_nrlist_cache()
-    C.update_motif_cache()
-    C.update_loop_cache()
+    if argv[1] == 'pdb':
+        C.update_pdb_cache()
+    elif argv[1] == 'nrlist':
+        C.update_nrlist_cache()
+    elif argv[1] == 'motifs':
+        C.update_motif_cache()
+    elif argv[1] == 'loops':
+        C.update_loop_cache()
+    else:
+        logging.error("Unrecognized option")
 
 
 if __name__ == "__main__":
