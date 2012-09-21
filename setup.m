3a525f1ce8f6774712ea84ccfe92b0cd174d5c18
Anton Petrov <anton.i.petrov@gmail.com>
2012-09-21 16:22:05 -0400

    Set environment variable MA_root during setup

--- setup.m
+++ setup.m
@@ -1,5 +1,7 @@
 function [] = setup()
 
+    setenv('MA_root', '/Users/anton/FR3D/MotifAtlas');
+
     addpath(genpath('FR3DSource'));
     addpath(genpath('FR3DDevelopment'));
     addpath(genpath('FR3DMotifs'));
