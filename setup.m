b80051cfdaaef9cdfd3fc4771eb1336c45b2b4c0
Anton Petrov <anton.i.petrov@gmail.com>
2012-10-04 14:23:03 -0400

    Dynamically set MA_root

--- setup.m
+++ setup.m
@@ -1,7 +1,11 @@
 function [] = setup()
 
-    setenv('MA_root', '/Users/anton/FR3D/MotifAtlas');
-
+    % set the environmental variable with the path to the MotifAtlas folder
+    motifatlas_path = [fileparts(which(mfilename)) filesep 'MotifAtlas'];
+    if ~exist(motifatlas_path, 'dir'), mkdir(motifatlas_path); end        
+    % set environmental variable
+    setenv('MA_root', motifatlas_path);
+            
     addpath(genpath('FR3DSource'));
     addpath(genpath('FR3DDevelopment'));
     addpath(genpath('FR3DMotifs'));
