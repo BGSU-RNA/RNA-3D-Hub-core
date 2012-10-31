2a44f8d43ff9439c7a4968204f77c5587359e95c
Anton Petrov <anton.i.petrov@gmail.com>
2012-10-31 19:26:03 -0400

    Improvements and fixes
    - catching viral structures error
    - proper parsing of new ids
    - move on if ids can't be generated for a file
    - better error handling

--- pymotifs/UnitIdLoader.py
+++ pymotifs/UnitIdLoader.py
@@ -1,6 +1,6 @@
 """
 
-Program used for storing correspondences between the old and new style unit ids.
+Program for storing correspondences between the old and new style unit ids.
 
 Example:
 python UnitIdLoader.py
@@ -22,9 +22,10 @@ from MotifAtlasBaseClass import MotifAtlasBaseClass
 
 
 # add dependencies to the python path
+id_translation_submodule = 'UnitIdTranslation'
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 os.sys.path.append(parent_dir)
-os.sys.path.append(os.path.join(parent_dir, 'UnitIdTranslation'))
+os.sys.path.append(os.path.join(parent_dir, id_translation_submodule))
 
 # now import from the UnitIdTranslation folder
 import id_translate as idt
@@ -46,39 +47,42 @@ class UnitIdLoader(MotifAtlasBaseClass):
             necessary, loops over the pdbs, generates unit ids and stores them
             in the database.
         """
-        try:
-            logging.info('Inside import_unit_ids')
-            if not recalculate:
-                recalculate = self.config['recalculate']['unit_ids']
-            if recalculate:
-                pdb_list = pdbs
-                self.__delete_unit_ids(pdbs)
-            else:
-                pdb_list = self.filter_out_analyzed_pdbs(pdbs,'unit_ids')
+        logging.info('Inside import_unit_ids')
+        if not recalculate:
+            recalculate = self.config['recalculate']['unit_ids']
+        if recalculate:
+            pdb_list = pdbs
+            self.__delete_unit_ids(pdbs)
+        else:
+            pdb_list = self.filter_out_analyzed_pdbs(pdbs,'unit_ids')
 
-            for pdb_id in pdb_list:
-                for file_type in self.pdb_file_types:
-                    logging.info('Analyzing %s%s' % (pdb_id, file_type))
-                    pdb_file = os.path.join(self.pdb_files_folder, pdb_id + file_type)
-                    cif_file = os.path.join(self.pdb_files_folder, pdb_id + '.cif')
+        for pdb_id in pdb_list:
+            for file_type in self.pdb_file_types:
+                logging.info('Analyzing %s%s' % (pdb_id, file_type))
+                pdb_file = os.path.join(self.pdb_files_folder, pdb_id + file_type)
+                cif_file = os.path.join(self.pdb_files_folder, pdb_id + '.cif')
 
-                    if not os.path.exists(pdb_file):
-                        logging.warning('Skipping %s because %s.pdb was not found in %s' % (pdb_id, pdb_id, self.pdb_files_folder))
-                        continue
-                    elif not os.path.exists(cif_file):
-                        logging.warning('Skipping %s because %s.cif was not found in %s' % (pdb_id, pdb_id, self.pdb_files_folder))
-                        continue
+                if not os.path.exists(pdb_file):
+                    logging.warning('Skipping %s because %s.pdb was not found in %s' % (pdb_id, pdb_id, self.pdb_files_folder))
+                    continue
+                elif not os.path.exists(cif_file):
+                    logging.warning('Skipping %s because %s.cif was not found in %s' % (pdb_id, pdb_id, self.pdb_files_folder))
+                    continue
 
+                try:
                     unit_ids = idt.get_id_correspondences(pdb_file, cif_file)
-                    logging.info('Found %i id pairs' % len(unit_ids))
+                except idt.LooksLikeAVirusStructureError:
+                    logging.warning('%s looks like a viral structure' % pdb_id)
+                    continue
+                except Exception, err:
+                    logging.critical('Crash on %s' % pdb_id)
+                    continue
+                logging.info('Found %i id pairs' % len(unit_ids))
 
-                    self.__store_unit_ids(unit_ids)
-                self.mark_pdb_as_analyzed(pdb_id, 'unit_ids')
+                self.__store_unit_ids(unit_ids)
+            self.mark_pdb_as_analyzed(pdb_id, 'unit_ids')
 
-            logging.info('%s', '='*40)
-        except:
-            e = sys.exc_info()[1]
-            MotifAtlasBaseClass._crash(self,e)
+        logging.info('%s', '='*40)
 
     def __delete_unit_ids(self, pdbs):
         """
@@ -100,23 +104,32 @@ class UnitIdLoader(MotifAtlasBaseClass):
         for (old_id, new_id) in unit_ids:
             # old id 1EKA_AU_1_B_8_C_
             (pdb_id,au_ba,model,chain,seq_id,comp_id,ins_code) = old_id.split('_')
-
             pdb_file = 'pdb' if au_ba == 'AU' else 'pdb' + au_ba[2:] # BA1, BA10
 
-            # new id. short form: 1D4R_1_B_C_25, long form: 1D4R_1_B_C_25_1_555
-            #                     1D4R_1_B_U_28___1_555
             underscores = new_id.count('_')
-            if underscores == 8: # id contains sym op
+            if underscores == 8: # all fields present
                 (pdb_id,model,chain,comp_id,seq_id,alt_id,ins_code,sym_op1,sym_op2) = new_id.split('_')
                 sym_op = sym_op1 + '_' + sym_op2
-            elif underscores == 6: # defaulting to 1_555
+            elif underscores == 6: # default sym_op
                 (pdb_id,model,chain,comp_id,seq_id,alt_id,ins_code) = new_id.split('_')
                 sym_op = '1_555'
+            elif underscores == 5: # default sym_op and ins_code
+                (pdb_id,model,chain,comp_id,seq_id,alt_id) = new_id.split('_')
+                sym_op   = '1_555'
+                ins_code = ''
+            elif underscores == 4: # default sym_op, ins_code, alt_id
+                (pdb_id,model,chain,comp_id,seq_id) = new_id.split('_')
+                sym_op   = '1_555'
+                ins_code = ''
+                alt_id   = ''
             else:
-                logging.critical('Unknown id format %s' % new_id)
+                msg = 'Unknown id format %s' % new_id
+                logging.critical(msg)
+                session.rollback()
+                self._crash(msg)
 
             U = PdbUnitIdCorrespondence(old_id   = old_id,
-                                        unit_id  = new_id.replace('_', '-'),
+                                        unit_id  = new_id,
                                         pdb      = pdb_id,
                                         model    = model,
                                         chain    = chain,
@@ -141,14 +154,22 @@ def usage():
 def main(argv):
     """
     """
-    logging.basicConfig(level=logging.DEBUG)
-
-    pdbs = ['1D4R']
-
     U = UnitIdLoader()
-    U.import_unit_ids(pdbs, recalculate=True)
+    U.start_logging()
 
+    from PdbInfoLoader import PdbInfoLoader
+    P = PdbInfoLoader()
+    P.get_all_rna_pdbs()
 
+    try:
+        U.import_unit_ids(P.pdbs, recalculate=True)
+    except:
+        e = sys.exc_info()[1]
+        U.config['email']['subject'] = 'Unit id update failed'
+        U._crash(e)
+
+    U.config['email']['subject'] = 'Unit ids successfully updated'
+    U.send_report()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
