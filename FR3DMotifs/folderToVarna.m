839b39be7843cddc3df2e652f23ec79c2649a23d
Anton Petrov <anton.i.petrov@gmail.com>
2013-01-05 23:08:19 -0500

    Simplify script name

--- FR3DMotifs/folderToVarna.m
+++ FR3DMotifs/folderToVarna.m
@@ -3,13 +3,13 @@ function [] = folderToVarna(location)
     groups_location = [location filesep 'Groups'];
     files = dir( fullfile(groups_location, '*.mat') );
     N = length(files);
-    
-    filename = fullfile(pwd, ['varna_' strrep(location, filesep, '-') '.txt']);
+
+    filename = fullfile(pwd, 'varna_script.txt');
 
     fid = fopen(filename,'w');
-    
+
     for i = 1:N
-        
+
         clear Search;
         load( fullfile(groups_location, files(i).name) );
         if ~exist('Search','var')
@@ -17,15 +17,15 @@ function [] = folderToVarna(location)
         end
         Search.FileName = files(i).name;
         command = motifToVarna(Search, location, 0);
-        
+
         fprintf(fid, '%s\n', command);
-        
+
     end
 
     fclose(fid);
 
     unix(sprintf('bash %s',filename));
-    
+
     movefile(filename, location);
 
 end
