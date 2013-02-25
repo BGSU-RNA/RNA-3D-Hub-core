88888b243749bb3f7be777dc0091d851bf2e814e
Blake Sweeney <blakes.85@gmail.com>
2013-02-25 10:53:47 -0500

    Import ordering.
    This can import the ordering of known units in a pdb file. This relies upon the
    modified_correspondence table to determine which things are RNA and which are
    not. A better approach would be to get all units and then mark units as RNA or
    not later.

--- pymotifs/unit_ordering_loader.py
+++ pymotifs/unit_ordering_loader.py
@@ -1,5 +1,3 @@
-#from future import with_statment
-
 """
 Compute the ordering of units in a PDB file.
 
@@ -12,14 +10,24 @@ import logging
 
 from MotifAtlasBaseClass import MotifAtlasBaseClass
 from models import session, PdbUnitOrdering
+from models import PdbModifiedCorrespondecies
 
 from Bio.PDB.PDBParser import PDBParser
 
 
 class UnitOrderingLoader(MotifAtlasBaseClass):
-    commit_every = 100
-    known = []
-    file_types = ['.pdb', '.pdb1']
+
+    def __init__(self):
+        MotifAtlasBaseClass.__init__(self)
+        self.commit_every = 100
+        self.file_types = ['.pdb', '.pdb1']
+        root = self.config['locations']['fr3d_root']
+        self.pdb_files_folder = os.path.join(root, 'FR3D', 'PDBFiles')
+
+        self.known = ['A', 'C', 'G', 'U']
+        query = session.query(PdbModifiedCorrespondecies.modified_unit)
+        for entry in query.all():
+            self.known.append(entry.modified_unit)
 
     def load(self, pdbs, recalculate=False):
         """Imports the unit id ordering for the given pdb files. Will only
@@ -33,52 +41,63 @@ class UnitOrderingLoader(MotifAtlasBaseClass):
             analyze = self.filter_out_analyzed_pdbs(analyze, 'unit_ordering')
 
         for pdb in analyze:
-            path = os.path.join(self.pdb_files_folder, pdb)
             for file_type in self.file_types:
                 try:
-                    self.__import_file__(path, file_type)
-                except:
+                    self.__import_file__(pdb, file_type)
+                except Exception as err:
                     logging.error("Failed ordering import: %s%s",
                                   pdb, file_type)
+                    raise err
 
     def __import_file__(self, pdb, extension):
         """Attempt to import a file. If the file does not exists, a warning is
         logged otherwise importing occurs.
         """
-        filename = pdb + extension
+        filename = os.path.join(self.pdb_files_folder, pdb + extension)
 
         if not os.path.exists(filename):
             logging.info("Skipping missing file: %s", filename)
+            return None
 
         try:
             with open(filename, 'r') as raw:
-                ordering = self.__pdb_ordering__(raw, pdb, extension)
+                pdb_type = 'AU'
+                if extension[-1] != 'b':
+                    pdb_type = 'BA' + extension[-1]
+                ordering = self.__pdb_ordering__(raw, pdb, pdb_type)
                 self.__store__(ordering)
-        except:
-            logging.cricical('Crash on: %s', filename)
+                self.mark_pdb_as_analyzed(pdb, 'unit_ordering')
+        except Exception as err:
+            logging.critical('Crash on: %s', filename)
+            raise err
 
     def __store__(self, data):
         """Store the results of generating an ordering.
         """
         logging.info("Storing ordered ids")
-        for count, (unit_id, index) in enumerate(data.entries()):
-            entry = PdbUnitOrdering(unit_id=unit_id, index=index)
-            try:
-                session.add(entry)
-                if count % self.commit_every == 0:
-                    session.commit()
-            except:
-                logging.error("Failed to add: %s", unit_id)
-                pass
-        session.commit()
-        logging.info("ID ordering added")
+        try:
+            for count, (unit_id, info) in enumerate(data.items()):
+                try:
+                    entry = PdbUnitOrdering(nt_id=unit_id, pdb=info['pdb'],
+                                            index=info['index'])
+                    session.add(entry)
+                    if count % self.commit_every == 0:
+                        session.commit()
+                except Exception as err:
+                    logging.error("Failed to add: %s", unit_id)
+                    raise err
+            session.commit()
+            logging.info("ID ordering added")
+        except Exception as err:
+            raise err
+            logging.info("Could not commit all ids")
 
     def __pdb_ordering__(self, raw, pdb_id, pdb_type):
-        """Generate a dict of the form: { unit_id: index } for all nucleotides
-        in the given structure. Nucleotides are identified by being in the list
-        of known units.
+        """Generate a dict of the form: { unit_id: {index: index, pdb: pdb }
+        for all nucleotides in the given structure. Nucleotides are identified
+        by being in the list of known units in self.known.
         """
-        parser = PDBParser()
+        parser = PDBParser(QUIET=True)
         structure = parser.get_structure(pdb_id, raw)
         data = {}
         index = 0
@@ -94,7 +113,7 @@ class UnitOrderingLoader(MotifAtlasBaseClass):
                                    chain_id, res_id[1], name, res_id[2]]
                         id_data = [str(part).strip() for part in id_data]
                         unit_id = '_'.join(id_data)
-                        data[unit_id] = index
+                        data[unit_id] = {'index': index, 'pdb': pdb_id}
                         index += 1
 
         return data
