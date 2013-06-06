1c6c733149cf045bbe138b4389222166372981b0
Blake Sweeney <blakes.85@gmail.com>
2013-06-06 09:14:05 -0400

    Outilne fabfile for deploying
    It would be nice to use fab to deploy stuff instead of having to do it myself.
    This is a basic script to do so. Needs some testing but this is the general
    idea.

---
+++ fabfile.py
@@ -0,0 +1,21 @@
+from fabric.api import cd
+from fabric.api import env
+from fabric.api import run
+from fabric.api import task
+
+
+env.hosts = ['anton@rna.bgsu.edu']
+env.dir = '/Code/RNA-3D-Hub_dev'
+env.branch = 'dev'
+
+
+@task
+def prod():
+    env.dir = '/Code/RNA-3D-Hub'
+    env.branch = 'master'
+
+
+@task
+def deploy():
+    with cd(env.dir):
+        run("git pull origin %s" % env.branch)
