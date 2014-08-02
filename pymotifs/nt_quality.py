1432a4a5bf9a389cfcf29991b9840266ae51311d
Blake Sweeney <blakes.85@gmail.com>
2014-08-02 17:46:43 -0400

    Improved quality importer
    It's not yet complete but it can now download and read the files. Next will be
    to generate the data correctly and then import it as needed.

--- pymotifs/nt_quality.py
+++ pymotifs/nt_quality.py
@@ -1,14 +1,19 @@
 import sys
-import zlib
+import gzip
+import hashlib
 import logging
 import traceback
+import cStringIO as sio
 import xml.etree.ElementTree as ET
 
 from MotifAtlasBaseClass import MotifAtlasBaseClass
 
+import utils as ut
 from models import NtQuality
-from utils import FTPFetchHelper
-from utils import DatabaseHelper
+
+ut.append_libs()
+from rnastructure.util import unit_ids as uids
+
 
 logger = logging.getLogger(__name__)
 
@@ -20,31 +25,73 @@ class FileHelper(object):
         if not pdb:
             raise ValueError("Must give a pdb")
 
-        return self.path.format(pdb=pdb, short=pdb[1:3])
+        return self.path.format(pdb=pdb.lower(), short=pdb[1:3].lower())
 
 
 class Parser(object):
-    """A class to parse the results of getting the quality file.
+    """A class to parse the results of getting the quality file. Right now it
+    only processes the RsR related data.
     """
+    entry_keys = [
+        'absolute-percentile-percent-RSRZ-outliers',
+        'high-resol-relative-percentile-percent-RSRZ-outliers',
+        'low-resol-relative-percentile-percent-RSRZ-outliers',
+        'numPDBids-absolute-percentile-percent-RSRZ-outliers',
+        'numPDBids-relative-percentile-percent-RSRZ-outliers',
+    ]
 
-    def __call__(self, gz_content):
-        content = zlib.decompress(gz_content)
-        tree = ET.fromstring(content)
-        root = tree.getroot()
-        pass
+    def __init__(self, gz_content):
+        filehandle = sio.StringIO(gz_content)
+        content = gzip.GzipFile(fileobj=filehandle).read()
+        md5 = hashlib.md5()
+        md5.update(content)
+        self.generator = uids.UnitIdGenerator()
+        self.digest = md5.hexdigest()
+        self.root = ET.fromstring(content)
+
+    def has_rsr(self):
+        entry = self.root.find("Entry")
+        return 'absolute-percentile-percent-RSRZ-outliers' in entry.attrib
+
+    def nts(self, root):
+        pdb = self.root.find("Entry").attrib['pdbid'].upper()
+        for residue in root.findall("ModeledSubgroup"):
+            if 'rsr' in residue.attrib and 'rsrz' in residue.attrib:
+                yield {
+                    'id': self._unit_id(pdb, residue.attrib),
+                    'rsr': float(residue.attrib['rsr']),
+                    'rsrz': float(residue.attrib['rsrz'])
+                }
+
+    def _unit_id(self, pdb, attributes):
+        return self.generator({
+            'pdb': pdb,
+            'model': int(attributes['model']),
+            'chain': attributes['chain'],
+            'number': int(attributes['resnum']),
+            'residue': attributes['resname'],
+            'insertion_code': attributes['icode'],
+            'symmetry_operator': '*'
+        })
 
 
-class Loader(MotifAtlasBaseClass, DatabaseHelper):
+class Loader(MotifAtlasBaseClass, ut.DatabaseHelper):
     def __init__(self, session_builder):
-        self.fetcher = FTPFetchHelper('ftp.wwpdb.org', parser=Parser())
+        self.fetcher = ut.FTPFetchHelper('ftp.wwpdb.org')
         self.finder = FileHelper()
         MotifAtlasBaseClass.__init__(self)
-        DatabaseHelper.__init__(self, session_builder)
+        ut.DatabaseHelper.__init__(self, session_builder)
 
     def data(self, pdb):
         filename = self.finder(pdb)
         logger.info("Using filename %s for pdb %s ", filename, pdb)
-        for entry in self.fetcher(filename):
+        response = self.fetcher(filename)
+        parser = Parser(response)
+        if not parser.has_rsr():
+            logging.info("No RsR found for %s", pdb)
+            return
+
+        for entry in parser.nts():
             yield NtQuality(**entry)
 
     def __call__(self, pdbs, **kwargs):
---
+++ pymotifs/test/nt_quality_test.py
@@ -0,0 +1,53 @@
+import unittest
+
+import utils as u
+import nt_quality as ntq
+
+
+class FileHelperTest(unittest.TestCase):
+    def setUp(self):
+        self.helper = ntq.FileHelper()
+
+    def test_can_generate_a_filepath(self):
+        val = self.helper('1J5E')
+        ans = 'pub/pdb/validation_reports/j5/1j5e/1j5e_validation.xml.gz'
+        self.assertEqual(val, ans)
+
+
+class MissingRsRParserTest(unittest.TestCase):
+    def setUp(self):
+        ftp = u.FTPFetchHelper('ftp.wwpdb.org')
+        filename = 'pub/pdb/validation_reports/j5/1j5e/1j5e_validation.xml.gz'
+        self.parser = ntq.Parser(ftp(filename))
+
+    def test_can_tell_has_no_rsr(self):
+        self.assertFalse(self.parser.has_rsr())
+
+
+class HasRsRParserTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        ftp = u.FTPFetchHelper('ftp.wwpdb.org')
+        filename = 'pub/pdb/validation_reports/og/3ogy/3ogy_validation.xml.gz'
+        cls.parser = ntq.Parser(ftp(filename))
+
+    def setUp(self):
+        self.parser = self.__class__.parser
+
+    def test_can_get_tree_from_gz_content(self):
+        self.assertTrue(self.parser.root)
+
+    def test_can_detect_has_rsr(self):
+        self.assertTrue(self.parser.has_rsr())
+
+    def test_can_generate_a_unit_id(self):
+        data = {
+            'model': '1',
+            'chain': 'A',
+            'resname': 'C',
+            'resnum': '10',
+            'icode': ' '
+        }
+        val = self.parser._unit_id('1J5E', data)
+        ans = '1J5E|1|A|C|10||||*'
+        self.assertEqual(val, ans)
