ea9680601dbb41e02e444020c812366f1ca40977
Blake Sweeney <blakes.85@gmail.com>
2014-10-20 14:14:44 -0400

    Add a core.py
    Core.py is meant to be a replacement for MotifAtlasBaseClass.py. That is to say
    it will contain the classes that all loaders (and exporters eventually) will
    inherit. It is meant to centralize logic about whether or not we should compute
    some annotations. It also contains things for handling sessions, storing to the
    database and matlab interfaces. My hope is that by doing this it is much simpler
    and more reliable to add new importers. Right now any class using it need only
    add 3 methods and will get lots of useful functionality. Doing this will provide
    a uniform interface to all loaders, which will be very useful. It differs from
    the current base class in that it is a new style class and does not contain all
    functionality.
    
    It will eventually supplant MotifAtlasBaseClass but for now it cannot. Because
    it is a new style class the initialization of child classes will be different.
    This will not contain all functionality of the current base class. For example,
    it will not setup logging, this will be done at the application level. However,
    we need to add things such as emailing in the future. Once this module is more
    mature I will begin to move older classes over to it. Probably starting with
    simple ones like distances and coordinates. For now I will use it on the new
    parts of the pipeline I'm developing, such as storing polymer alignments. These
    parts aren't ready to be in the core but are useful and will provide a good test
    case for this module.

---
+++ pymotifs/core.py
@@ -0,0 +1,274 @@
+import os
+import abc
+import sys
+import logging
+import inspect
+import datetime
+import traceback
+import itertools as it
+import collections as coll
+from contextlib import contextmanager
+
+from mlabwrap import mlab
+
+from models import PdbAnalysisStatus
+
+logger = logging.getLogger(__name__)
+
+
+class StageFailed(Exception):
+    """This is raised when one stage of the pipeline fails.
+    """
+    pass
+
+
+class InvalidState(Exception):
+    """This is an exception meant to be used when we have entered into some
+    sort of invalid state in a stage. For example, we require that chain breaks
+    be loaded before we do nt-nt correspondences but if the programs are run
+    out of order this will be raised.
+    """
+    pass
+
+
+class Matlab(object):
+    """A simple wrapper around mlab. This is useful because it sets the root as
+    well as calling the setup function before running matlab.
+    """
+
+    def __init__(self, root):
+        logger.debug('Starting up matlab')
+        os.chdir(root)
+        self.mlab = mlab
+        self.mlab._autosync_dirs = False
+        self.mlab.setup()
+        logger.debug('Matlab started')
+
+    def __getattr__(self, key):
+        return getattr(self.mlab, key)
+
+
+class Session(object):
+    """A wrapper around a session maker to provide the types of logging and
+    rollbacks that we desire.
+    """
+
+    def __init__(self, session_maker):
+        self.maker = session_maker
+
+    @contextmanager
+    def __call__(self):
+        """Context handler for the session. This creates a new session and
+        yields it. It will catch, log, and re raise any exceptions that occur.
+        It will also commit and close all sessions.
+        """
+        session = self.maker()
+        try:
+            yield session
+            session.commit()
+        except:
+            logger.error("Transaction failed. Rolling back.")
+            logger.error(traceback.format_exc(sys.exc_info()))
+            session.rollback()
+            raise
+        finally:
+            session.close()
+
+
+class Loader(object):
+    """An abstract baseclass for all loaders. This provides a constient
+    interface for all loaders to use.
+    """
+
+    __metaclass__ = abc.ABCMeta
+
+    """ The name of the loader. """
+    name = None
+
+    """ Maximum length of time between updates. False for forever. """
+    update_gap = None
+
+    """ If we should stop the whole loader if one part fails. """
+    stop_on_failure = True
+
+    """ Max number of things to insert at once. """
+    insert_max = 1000
+
+    """ A flag to indicate it is ok to produce no data. """
+    allow_no_data = False
+
+    def __init__(self, config, session_maker):
+        self.config = coll.defaultdict(dict)
+        self.config.update(config)
+        if not self.name:
+            raise AttributeError("Must set name")
+
+        if self.update_gap is None:
+            raise AttributeError("Must set update gap")
+
+        self.session = Session(session_maker)
+
+    @abc.abstractmethod
+    def data(self, pdb, **kwargs):
+        """Compute the data for the given pdb file.
+        """
+        pass
+
+    @abc.abstractmethod
+    def has_data(self, pdb):
+        """Check if we have already stored data for this pdb file in the
+        database. This is used to determine if we should attempt to compute new
+        data.
+        """
+        pass
+
+    @abc.abstractmethod
+    def remove(self, pdb):
+        """Remove any old data for this pdb. This is used both when we have
+        failed to store all data as well as when we are overwriting existing
+        data. This should never raise anything unless something is drastically
+        wrong.
+        """
+        pass
+
+    def step(self):
+        """Gets the name of this step. Basically it is used to normalize
+        self.name.
+        """
+        return self.name.lower()
+
+    def store(self, data):
+        """Store the given data. The data is written in chunks of
+        self.insert_max at a time. The data can be a list or a nested set of
+        iterables.
+        """
+
+        logger.debug("Storing data for %s", self.name)
+        with self.session() as session:
+            if not isinstance(data, coll.Iterable):
+                session.add(data)
+            else:
+                iterator = enumerate(data)
+                if inspect.isgenerator(data) or \
+                   isinstance(data[0], coll.Iterable) or \
+                   inspect.isgenerator(data[0]):
+                    iterator = enumerate(it.chain.from_iterable(data))
+
+                for index, datum in iterator:
+                    session.add(datum)
+                    if index % self.insert_max == 0:
+                        logger.debug("Committing a chunk of %s",
+                                     self.insert_max)
+                        session.commit()
+                logger.debug("Final commit")
+
+            session.commit()
+
+    def been_long_enough(self, pdb):
+        """Determine if it has been long enough to recompute the data for the
+        given pdb. This uses the udpate_gap property which tells how long to
+        wait between updates. If that is False then we never update based upon
+        time.
+        """
+        if not self.update_gap:
+            return False
+
+        with self.session() as session:
+            current = session.query(PdbAnalysisStatus).\
+                filter_by(id=pdb, step=self.step()).\
+                first()
+            if not current:
+                return True
+            current = current.time
+        # If this has been marked as done in the far future do it anyway. That
+        # is a silly thing to do
+        diff = abs(datetime.datetime.now() - current)
+        return diff > self.update_gap()
+
+    def should_compute(self, pdb, recompute=False, **kwargs):
+        """Determine if we should recompute the data for this loader and pdb.
+        This is true if we are told to recompute, if we do not have data for
+        this pdb or it has been long enough since the last update.
+        """
+        return recompute or self.config[self.name].get('recompute') or \
+            not self.has_data(pdb) or self.been_long_enough(pdb)
+
+    def mark_analyzed(self, pdb):
+        """Mark that we have finished computing the results for the given pdb.
+        """
+        with self.session() as session:
+            status = PdbAnalysisStatus(id=pdb, step=self.step(),
+                                       time=datetime.datetime.now())
+            session.merge(status)
+            session.commit()
+            logging.info('Updated %s status for pdb %s', self.name, pdb)
+
+    def __call__(self, pdbs, **kwargs):
+        """Load all data for the list of pdbs. This will load things as needed
+        into the database. It checks if we should recompute or if we are forced
+        to and then stores the computed data.
+        """
+
+        if not pdbs:
+            logging.critical("Must give pdbs to %s", self.name)
+            raise Exception("Not pdbs given")
+
+        for pdb in pdbs:
+            pdb = pdb.upper()
+            logger.info("Loader %s is processing %s", self.name, pdb)
+
+            if not self.should_compute(pdb, **kwargs):
+                logger.debug("Skipping computing %s for %s", self.name, pdb)
+                continue
+
+            try:
+                data = self.data(pdb, **kwargs)
+            except:
+                logger.error("Error raised when getting data for %s",
+                             self.name)
+                logger.error(traceback.format_exc(sys.exc_info()))
+                self.remove(pdb)
+                if self.stop_on_failure:
+                    raise StageFailed(self.name)
+                continue
+
+            if not self.allow_no_data and not data:
+                logger.error("No data produced for %s", self.name)
+                raise InvalidState("Missing data")
+            elif not data:
+                logger.warning("No data produced for %s", self.name)
+                continue
+
+            try:
+                self.remove(pdb)
+                self.store(data)
+            except:
+                logger.error("Error raised when storing data for %s",
+                             self.name)
+                logger.error(traceback.format_exc(sys.exc_info()))
+                self.remove(pdb)
+                if self.stop_on_failure:
+                    raise StageFailed(self.name)
+                continue
+
+            try:
+                self.mark_analyzed(pdb)
+            except:
+                logger.error("Could not mark %s as done on %s", self.name, pdb)
+                logger.error(traceback.format_exc(sys.exc_info()))
+                self.remove(pdb)
+                if self.stop_on_failure:
+                    raise StageFailed(self.name)
+
+
+class MultiLoader(object):
+    stages = []
+
+    def __init__(self, *args):
+        self.steps = []
+        for stage in self.stages:
+            self.steps.append(stage(*args))
+
+    def __call__(self, pdbs, **kwargs):
+        for step in self.steps:
+            step(pdbs, **kwargs)
