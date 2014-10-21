bf2f222f2a69f1ab4f84de115ce20db5fc9b43ae
Blake Sweeney <blakes.85@gmail.com>
2014-10-21 15:09:05 -0400

    Comment out incomplete classes

--- pymotifs/correspondence/__init__.py
+++ pymotifs/correspondence/__init__.py
@@ -3,8 +3,9 @@ import core
 from correspondence.nts import Loader as NtLoader
 from correspondence.loops import Loader as LoopLoader
 from correspondence.info import Loader as InfoLoader
-from correspondence.discrepancy import Loader as DiscLoader
+# from correspondence.discrepancy import Loader as DiscLoader
 
 
 class Loader(core.MultiLoader):
-    stages = [InfoLoader, NtLoader, LoopLoader, DiscLoader]
+    stages = [InfoLoader, NtLoader, LoopLoader]
+    # stages = [InfoLoader, NtLoader, LoopLoader, DiscLoader]
