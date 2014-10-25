444ec784383bddc09dcb83c968ec35aba0e6bba2
Blake Sweeney <blakes.85@gmail.com>
2014-10-25 15:26:54 -0400

    Add loader for experimental mapping information
    This loads up the mapping between the experimental sequence and the observed
    sequence. This will be useful for finding unobserved residues and for aligning
    different structures.

---
+++ pymotifs/experimental_mapping.py
@@ -0,0 +1,42 @@
+import logging
+
+import core
+import utils
+from models import ObsSequenceMapping as Mapping
+
+from rnastructure.tertiary.cif import CIF
+
+logger = logging.getLogger(__name__)
+
+
+class Loader(core.Loader):
+    name = 'experimental_mapping'
+    update_gap = False
+
+    def __init__(self, config, maker):
+        self.finder = utils.CifFileFinder(config)
+        super(Loader, self).__init__(config, maker)
+
+    def has_data(self, pdb):
+        with self.session() as session:
+            query = session.query(Mapping).filter_by(pdb=pdb)
+            return bool(query.count())
+
+    def remove(self, pdb):
+        with self.session() as session:
+            session.query(Mapping).filter_by(pdb=pdb).delete()
+
+    def data(self, pdb, **kwargs):
+        mapping = []
+        seen = set()
+        with open(self.finder(pdb), 'rb') as raw:
+            cif = CIF(raw)
+            for chain in cif.chains():
+                seq_mapping = chain.experimental_sequence_mapping()
+                for (_, seq_id, unit_id) in seq_mapping:
+                    if unit_id not in seen:
+                        obj = Mapping(pdb=pdb, unit_id=unit_id,
+                                      sequence_unit_id=seq_id)
+                        mapping.append(obj)
+                        seen.add(unit_id)
+        return mapping
