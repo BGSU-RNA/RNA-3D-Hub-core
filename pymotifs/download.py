b137c6b21874ac541e11f93862e678338108e2ec
Blake Sweeney <blakes.85@gmail.com>
2014-12-10 17:32:50 -0500

    Update downloader to only use cif files
    The downloader should only get cif files because those are the only ones we need
    to analyze.

--- pymotifs/download.py
+++ pymotifs/download.py
@@ -1,10 +1,9 @@
 """
-A program for downloading .pdb, .cif, and .pdb1 files for all RNA-containing
+A program for downloading .cif, files for all RNA-containing
 3D structures.
 """
 
 import os
-import xml.dom.minidom
 
 import core
 import utils
@@ -12,44 +11,28 @@ import utils
 
 class Downloader(core.Loader):
     file_url = 'http://www.rcsb.org/pdb/files/'
-    entity_info_url = 'http://www.pdb.org/pdb/rest/getEntityInfo?structureId='
     name = 'downloader'
     update_gap = False
+    allow_fail = True
 
     def __init__(self, config, maker):
-        self.helper = utils.WebRequestHelper(parser=self.parse)
         self.gzip = utils.GzipFetchHelper(allow_fail=True)
         self.location = os.path.join(config['locations']['fr3d_root'],
                                      'PDBFiles')
         super(core.Loader, self).__init__(config, maker)
 
-    def parse(self, response):
-        dom = xml.dom.minidom.parseString(response.text)
-        tag = dom.getElementsByTagName('PDB')[0]
-        raw = tag.attributes['bioAssemblies'].value
-        return int(raw)
+    def filename(self, name):
+        return os.path.join(self.location, name + '.cif')
 
-    def assemblies_count(self, pdb):
-        """Find the number of biological assemblies associated with a pdb id.
-        """
-        try:
-            return self.helper(self.entity_info_url + pdb)
-        except:
-            self.logger.error('Bioassembly query failed for  %s' % pdb)
-            return None
+    def url(self, name):
+        return self.file_url + name + '.cif.gz'
 
     def remove(self, entry):
-        pass
+        if self.has_data(entry):
+            os.remove(self.filename(entry))
 
-    def transform(self, pdb):
-        values = [(pdb, '.cif'), (pdb, '.pdb')]
-        if self.assembly_count(pdb):
-            values.append((pdb, '.pdb1'))
-        return values
-
-    def has_data(self, filename):
-        full = os.path.join(self.location, filename)
-        return os.path.exists(full)
+    def has_data(self, entry):
+        return os.path.exists(self.filename(entry))
 
     def store(self, data):
         filename, text = data
@@ -57,19 +40,16 @@ class Downloader(core.Loader):
             out.write(text)
         self.logger.info('Downloaded %s' % filename)
 
-    def data(self, filename, **kwargs):
-        pdb, extension = filename
-        destination = os.path.join(self.location, pdb + extension)
+    def data(self, name, **kwargs):
+        destination = self.filename(name)
 
         try:
-            content = self.gzip(self.file_url + pdb + extension + '.gz')
+            content = self.gzip(self.url(name))
         except:
-            if extension == '.pdb' or extension == '.cif':
-                self.logger.critical('%s could not be downloaded',
-                                     pdb + extension)
-                raise
+            self.logger.error('%s could not be downloaded', name)
+            raise core.SkipValue("Couldn't get %s" % name)
 
         if not content:
-            core.SkipValue("Downloaded empty file %s" % pdb + extension)
+            core.SkipValue("Downloaded empty file %s" % name)
 
         return destination, content
