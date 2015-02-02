4c263f08fc60bfba05ca2edd10773d619a7f0619
Craig L. Zirbel <zirbel@bgsu.edu>
2015-02-02 11:44:16 -0500

    4 space indentation

--- pymotifs/nr/connectedsets.py
+++ pymotifs/nr/connectedsets.py
@@ -6,35 +6,35 @@
 
 def findconnectedsets(connections):
 
-  considered = {}
-  for i in connections.keys():
-    considered[i] = False
-    for j in connections[i]:
-        considered[j] = False
+    considered = {}
+    for i in connections.keys():
+        considered[i] = False
+        for j in connections[i]:
+            considered[j] = False
 
-  for i in connections.keys():
-    connections[i] = connections[i] | set([i])    # make sure all connections are reflexive; i is connected to i
-    for j in connections[i]:
-        if j in connections:
-            connections[j] = connections[j] | set([i])  # make sure all connections are entered in reversed order too; symmetrize
-        else:
-            connections[j] = set([i])
+    for i in connections.keys():
+        connections[i] = connections[i] | set([i])    # make sure all connections are reflexive; i is connected to i
+        for j in connections[i]:
+            if j in connections:
+                connections[j] = connections[j] | set([i])  # make sure all connections are entered in reversed order too; symmetrize
+            else:
+                connections[j] = set([i])
 
-  linked = {}
+    linked = {}
 
-  for i in connections.keys():              # loop through all keys for connections
-    if not considered[i]:                   # if this key was not already encountered,
-        linked[i] = connections[i]            # start a new list of its links
-        considered[i] = True                  # note that it has been encountered, so it is not processed again
-        newconnections = True                 # set a flag for exploring what i is connected to
-        while newconnections:                 # keep doing this as long as a new connection was found
-            newconnections = False              # no new connections found yet
-            for j in linked[i]:                 # look through the things connected to i
-                if not considered[j]:             # that have not already been considered
-                    linked[i] = linked[i] | connections[j] # add these to the connections
-                    considered[j] = True            # don't consider these connections again
-                    newconnections = True           # a new connection was found
-        print linked[i]
+    for i in connections.keys():              # loop through all keys for connections
+        if not considered[i]:                   # if this key was not already encountered,
+            linked[i] = connections[i]            # start a new list of its links
+            considered[i] = True                  # note that it has been encountered, so it is not processed again
+            newconnections = True                 # set a flag for exploring what i is connected to
+            while newconnections:                 # keep doing this as long as a new connection was found
+                newconnections = False              # no new connections found yet
+                for j in linked[i]:                 # look through the things connected to i
+                    if not considered[j]:             # that have not already been considered
+                        linked[i] = linked[i] | connections[j] # add these to the connections
+                        considered[j] = True            # don't consider these connections again
+                        newconnections = True           # a new connection was found
+            print linked[i]
 
     return linked
 
