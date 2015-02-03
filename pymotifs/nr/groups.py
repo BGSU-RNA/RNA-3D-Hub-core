fbf5f32be47f2f42bc49c514e3ec59306b4c209c
Blake Sweeney <blakes.85@gmail.com>
2015-02-03 16:07:19 -0500

    Add some work on groups
    I've made a little progress on groups. I've outlined more needed methods as well
    as working on actually getting all the required data from the database.

--- pymotifs/nr/groups.py
+++ pymotifs/nr/groups.py
@@ -4,10 +4,12 @@ import networkx as nx
 import networkx.algorithms.components.connected as connected
 
 from pymotifs import core
-from pymotifs.models import ChainInfo
+from pymotifs import models as mod
+from pymotifs.utils.structures import BasePairQueries as BpHelper
+from pymotifs.utils.structures import LONG_RANGE_CUTOFF
 
 
-class Grouper(core.Stage):
+class Grouper(object):
     """This is a class to determine the grouping of chains in a pdb. This will
     load all non-redundant chains and then group them by similarity. It
     produces a list of chains which are non-redundant. This does not process
@@ -18,6 +20,7 @@ class Grouper(core.Stage):
     def __init__(self, config, session_maker):
         self.config = config
         self.session = core.Session(session_maker)
+        self.bp_helper = BpHelper(session_maker)
 
     def best(self, chains):
         """Compute the best chain for a list of chains
@@ -27,7 +30,15 @@ class Grouper(core.Stage):
         entry to the chains called 'equivalent', which is all other chains in
         that structure which are equivalent.
         """
-        pass
+
+        def ordering(chain):
+            return (float(chain['bp']) / float(chain['length']),
+                    float(chain['length']), chain['name'])
+
+        best = max(chains, ordering)
+        best['equivalent'] = list(chains)
+        best['equivalent'].remove(best)
+        return best
 
     def rna_chains(self, pdb):
         """Get all RNA containing chains for a given pdb.
@@ -36,9 +47,20 @@ class Grouper(core.Stage):
         :returns: A list of chain ids for that pdb.
         """
         with self.session() as session:
-            query = session.query(ChainInfo.chainId).\
+            query = session.query(mod.ChainInfo.chainId).\
                 filter_by(entityMacromoleculeType='Polyribonucleotide (RNA)')
             return [result.chainId for result in query]
+
+    def autonomous(self, chains):
+        """This method goes through all chains and detects those that are
+        autonomous. When a chain is making no interactions with other chains it
+        is kept, if the chain is making important interactions with another
+        chain then those will be taken as one autonomous unit and kept.
+
+        :chains: A list of chain dictionaries.
+        :returns: A list of merged chain objects.
+        """
+        pass
 
     def chains(self, pdb):
         """Load all chains from a given pdb. This will get the RNA chains as
@@ -49,7 +71,16 @@ class Grouper(core.Stage):
         is that which is provided by the info method.
         """
         chains = self.rna_chains(pdb)
-        return [self.info(pdb, chain) for chain in chains]
+        automous_chains = self.autonomous(chains)
+        return [self.info(pdb, chain) for chain in automous_chains]
+
+    def entity_mapping(self, pdb):
+        data = {}
+        with self.session() as session:
+            query = session.query(mod.ChainInfo).filter_by(pdb=pdb)
+            for result in query:
+                data[result.chain] = result.entity_id
+        return data
 
     def nr_chains(self, pdb):
         """Get all chains which are non-redundant from a cif file.
@@ -76,29 +107,60 @@ class Grouper(core.Stage):
         :returns: A dictionary with bp, internal, external and lr entries,
         which stores the respective data.
         """
-        pass
+        helper = self.bp_helper
+        return {
+            'bp': helper.representative(pdb, chain, count=True),
+            'lr': helper.representative(pdb, chain, count=True,
+                                        range_cutoff=LONG_RANGE_CUTOFF),
+            'internal': helper.representative(pdb, chain, count=True,
+                                              family='cWW'),
+            'external': helper.cross_chain(pdb, chain, count=True,
+                                           family='cWW')
+        }
 
     def info(self, pdb, chain):
+        """This loads all information about a chain into a dictionary. This
+        will load generic information about a chain, such as resolved, length,
+        database id, the source and information about basepairing. The
+        basepairing information is loaded from `bps`.
+
+        :pdb: The pdb to search.
+        :chain: The chain to search.
+        :returns: A dictionary with
+        """
+
         data = {
             'id': '%s|%s' % (pdb, chain),
             'name': chain,
             'pdb': pdb,
-            'source': self.source(pdb, chain),
         }
+
+        with self.session() as session:
+            query = session.query(mod.ChainInfo).\
+                filter_by(pdb_id=pdb, chainId=chain)
+
+            result = query.one()
+            data['db_id'] = result.id
+            data['length'] = result.chainLength
+            data['source'] = result.source
 
         data.update(self.bps(pdb, chain))
         return data
 
-    def is_equivalent(self, chain1, chain2):
+    def is_equivalent(self, alignments, chain1, chain2):
         """Determine if two chains are equivalent.
 
+        :alignments: A dictionary of dictionaries about the alignment between
+        chains.
         :chain1: The first chain.
         :chain2: The second chain.
         :returns: True if the two chains are equivalent, False otherwise
         """
-        pass
+        if chain1['source'] != chain2['source']:
+            return False
+        return True
 
-    def group(self, chains):
+    def group(self, chains, alignments):
         """Group all chains into connected components.
 
         :chains: A list of chains to group.
@@ -107,7 +169,7 @@ class Grouper(core.Stage):
         graph = nx.Graph()
         for chain1 in chains:
             for chain2 in chains:
-                if self.is_equivalent(chain1, chain2):
+                if self.is_equivalent(alignments, chain1, chain2):
                     graph.add_edge(chain1, chain2)
         return connected.connected_components(graph)
 
@@ -121,8 +183,9 @@ class Grouper(core.Stage):
 
     def __call__(self, pdbs):
         chains = self.nr_chains(pdbs)
+        alignments = self.alignment_info(chains)
         groups = []
-        for group in self.group(chains):
+        for group in self.group(chains, alignments):
             groups.append({
                 'members': group,
                 'representative': self.representative(group)
