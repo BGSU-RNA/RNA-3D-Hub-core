99bb1f686f1a3d0d9c6455034ff7ffc49d18c4ea
Blake Sweeney <blakes.85@gmail.com>
2015-02-05 14:42:54 -0500

    Allow the aggregate unit loader to work
    Not all unit stages are done yet but these two are tested and working so I've
    added them to the aggregate and uncommented it.

--- pymotifs/units/__init__.py
+++ pymotifs/units/__init__.py
@@ -1,10 +1,10 @@
-# from pymotifs import core
+from pymotifs import core
 
-# from pymotifs.units.info import Loader as InfoLoader
+from pymotifs.units.info import Loader as InfoLoader
 # from pymotifs.units.coordinates import Loader as CoordinateLoader
 # from pymotifs.units.distances import Loader as DistancesLoader
-# from pymotifs.units.quality import Loader as QualityLoader
+from pymotifs.units.quality import Loader as QualityLoader
 
 
-# class Loader(core.MultiLoader):
-#     stages = [InfoLoader, CoordinateLoader, DistancesLoader, QualityLoader]
+class Loader(core.MultiLoader):
+    stages = [InfoLoader, QualityLoader]
