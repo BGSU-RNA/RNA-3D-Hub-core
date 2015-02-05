ab77bfa0d28c34934138d7555ff7630a596505e5
Blake Sweeney <blakes.85@gmail.com>
2015-02-05 14:03:15 -0500

    Move utils tests around
    I've moved utils tests so that they are in the correct module and split out by
    functionality. These tests come nowhere close to covering the utils
    functionality but this is start.

---
+++ test/utils/file_finder_test.py
@@ -0,0 +1,20 @@
+import os
+import unittest
+
+from pymotifs.utils import CifFileFinder
+from pymotifs.utils import MissingFileException
+
+
+class CifFileFinderTest(unittest.TestCase):
+    def setUp(self):
+        path = os.path.normpath(".")
+        self.cif = CifFileFinder({'locations': {'fr3d_root': path}})
+
+    def test_can_find_a_cif_file(self):
+        val = self.cif("2AW7")
+        ans = os.path.abspath("./FR3D/PDBFiles/2AW7.cif")
+        self.assertEqual(ans, val)
+
+    def test_fails_for_missing_file(self):
+        self.assertRaises(MissingFileException, self.cif, "bob")
+
