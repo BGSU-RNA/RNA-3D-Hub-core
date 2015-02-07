09135cd21300956030fe61ab9e4f9fd276173c46
Blake Sweeney <blakes.85@gmail.com>
2015-02-07 18:02:10 -0500

    Add another utility class
    I've found myself wanting to load a cif file when I test stuff. It's a bunch of
    boiler plate I'm not really interested in rewriting a lot. So I've moved it into
    a class that we can inherit from.

--- test/__init__.py
+++ test/__init__.py
@@ -4,6 +4,8 @@ import unittest as ut
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from fr3d.cif.reader import Cif
+
 import pymotifs.models as models
 
 with open('conf/test.json', 'rb') as raw:
@@ -28,3 +30,19 @@ class QueryUtilTest(ut.TestCase):
     def setUp(self):
         if self.query_class:
             self.db_obj = self.query_class(Session)
+
+
+class CifStageTest(StageTest):
+    filename = None
+
+    @classmethod
+    def setUpClass(cls):
+        if cls.filename:
+            with open(cls.filename, 'rb') as raw:
+                cls.cif = Cif(raw)
+                cls.structure = cls.cif.structure()
+
+    def setUp(self):
+        super(CifStageTest, self).setUp
+        self.structure = self.__class__.structure
+        self.cif = self.__class__.cif
