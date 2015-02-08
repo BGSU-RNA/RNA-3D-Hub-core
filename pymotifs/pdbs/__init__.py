a333463a2363fda696a27e35b87d2c0c9b2965c4
Blake Sweeney <blakes.85@gmail.com>
2015-02-08 15:29:18 -0500

    Add pdb group loader

--- pymotifs/pdbs/__init__.py
+++ pymotifs/pdbs/__init__.py
@@ -1,8 +1,8 @@
-# import pymotifs.core as core
+import pymotifs.core as core
 
-# from pymotifs.pdbs.info import Loader as InfoLoader
-# from pymotifs.pdbs.obsolete import Loader as ObsoleteLoader
+from pymotifs.pdbs.info import Loader as InfoLoader
+from pymotifs.pdbs.obsolete import Loader as ObsoleteLoader
 
 
-# class Loader(core.MultiLoader):
-#     stages = [InfoLoader, ObsoleteLoader]
+class Loader(core.MultiLoader):
+    stages = [InfoLoader, ObsoleteLoader]
