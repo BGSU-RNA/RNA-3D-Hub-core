48fa1c67cdff5bc1dcd605cbc92e34d14bc3e0db
Blake Sweeney <blakes.85@gmail.com>
2015-02-08 15:56:12 -0500

    Add simple tests for the session wrapper
    This is some simple stuff to make sure session wrapper works ok. This is based
    off my tests for storing in loaders but is more specific to the wrapper.

---
+++ test/core/session_wrapper_test.py
@@ -0,0 +1,43 @@
+from test import Session
+
+import unittest as ut
+
+from pymotifs.core import Session as SessionWrapper
+from pymotifs.models import PdbInfo
+
+
+class SessionWrapperTest(ut.TestCase):
+    def setUp(self):
+        self.session = SessionWrapper(Session)
+
+    def tearDown(self):
+        with self.session() as session:
+            session.query(PdbInfo).\
+                filter(PdbInfo.id.like('0%')).\
+                delete(synchronize_session=False)
+
+    def test_it_reraises_exceptions(self):
+        def func():
+            with self.session():
+                raise AttributeError("bob")
+        self.assertRaises(AttributeError, func)
+
+    def test_it_rolls_back_on_exceptions(self):
+        def func():
+            with self.session() as session:
+                session.add(PdbInfo(id='0000'))
+                raise ValueError("Stop")
+
+        self.assertRaises(ValueError, func)
+
+        with self.session() as session:
+            count = session.query(PdbInfo).filter_by(id='0000').count()
+            self.assertEquals(0, count)
+
+    def test_it_always_commits(self):
+        with self.session() as session:
+            session.add(PdbInfo(id='0000'))
+
+        with self.session() as session:
+            count = session.query(PdbInfo).filter_by(id='0000').count()
+            self.assertEquals(1, count)
