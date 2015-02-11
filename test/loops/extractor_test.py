238ecb79792f5d55c2ba785d417ef4db83618abb
Blake Sweeney <blakes.85@gmail.com>
2015-02-11 11:05:35 -0500

    Attempt to generate loop ids correctly
    We now attempt to generate loop ids correctly. Before we had an issue where I
    followed the previous logic that relies upon saving each loop constantly so we
    can use a database query to get the count. As we no longer save until the end
    this does not work. I've fixed things to generate a mapping which can be used to
    get old ids and the pass along a count to get a new one.

--- test/loops/extractor_test.py
+++ test/loops/extractor_test.py
@@ -23,25 +23,31 @@ class DeterminingComputationTest(StageTest):
 class GettingLoopIdsTest(StageTest):
     loader_class = Loader
 
+    def setUp(self):
+        super(GettingLoopIdsTest, self).setUp()
+        self.mapping = {
+            '2AW7_AU_1_A_1076_U_,2AW7_AU_1_A_1077_G_,2AW7_AU_1_A_1078_U_,2AW7_AU_1_A_1079_G_,2AW7_AU_1_A_1080_A_,2AW7_AU_1_A_1081_A_': 'HL_2AW7_024'
+        }
+
     def test_pads_to_three_for_small_numbers(self):
-        val = self.loader.next_loop_number_string(10)
+        val = self.loader._next_loop_number_string(10)
         self.assertEquals('011', val)
 
     def test_pads_to_six_for_large_numbers(self):
-        val = self.loader.next_loop_number_string(1239)
+        val = self.loader._next_loop_number_string(1239)
         self.assertEquals('001240', val)
 
     def test_pading_jumps_at_999(self):
-        val = self.loader.next_loop_number_string(999)
+        val = self.loader._next_loop_number_string(999)
         self.assertEquals('001000', val)
 
     def test_generates_a_new_id_for_an_unknown_loop(self):
-        val = self.loader._get_loop_id('bob', '2AW7', 'IL')
+        val = self.loader._get_loop_id('bob', '2AW7', 'IL', self.mapping, 68)
         self.assertEquals('IL_2AW7_069', val)
 
     def test_uses_old_id_for_known_loop(self):
         nts = '2AW7_AU_1_A_1076_U_,2AW7_AU_1_A_1077_G_,2AW7_AU_1_A_1078_U_,2AW7_AU_1_A_1079_G_,2AW7_AU_1_A_1080_A_,2AW7_AU_1_A_1081_A_'
-        val = self.loader._get_loop_id(nts, '2AW7', 'HL')
+        val = self.loader._get_loop_id(nts, '2AW7', 'HL', self.mapping, 10)
         self.assertEquals('HL_2AW7_024', val)
 
 
@@ -50,5 +56,11 @@ class ExtractLoopsTest(StageTest):
 
     @skip_without_matlab
     def test_can_extract_correct_number_of_loops(self):
-        val = self.loader.data('2AW7', 'IL')
-        self.assertEquals(69, len(val))
+        val = self.loader.data(('2AW7', 'IL'))
+        self.assertEquals(68, len(val))
+
+    @skip_without_matlab
+    def test_can_extract_with_reasonable_ids(self):
+        data = self.loader.data(('4V4Q', 'IL'))
+        names = [data.name for name in data]
+        self.assertTrue(len(set(names)) > 69)
