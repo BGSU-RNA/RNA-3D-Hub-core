47ce49e1ef206f51fbd0313a01416f6d1097ddb6
Blake Sweeney <blakes.85@gmail.com>
2015-02-17 14:09:32 -0500

    Add the redundant units loader to units
    We will now always load the redundant units when doing units.

--- pymotifs/units/__init__.py
+++ pymotifs/units/__init__.py
@@ -4,7 +4,8 @@ from pymotifs.units.info import Loader as InfoLoader
 # from pymotifs.units.coordinates import Loader as CoordinateLoader
 # from pymotifs.units.distances import Loader as DistancesLoader
 from pymotifs.units.quality import Loader as QualityLoader
+from pymotifs.units. redundant import RedundantNucleotidesLoader
 
 
 class Loader(core.MultiStageLoader):
-    stages = [InfoLoader, QualityLoader]
+    stages = [InfoLoader, QualityLoader, RedundantNucleotidesLoader]
