f951b5d7f09003228709bf7f3fb3628a547b257b
Blake Sweeney <blakes.85@gmail.com>
2015-02-19 19:06:48 -0500

    Add species to chains mapping to chain loader
    We will now load the species information along with general chain information.

--- pymotifs/chains/__init__.py
+++ pymotifs/chains/__init__.py
@@ -2,7 +2,8 @@ from pymotifs import core
 
 from pymotifs.chains.info import Loader as InfoLoader
 from pymotifs.chains.best import BestChainsAndModelsLoader
+from pymotifs.chains.species import Loader as SpeciesLoader
 
 
 class Loader(core.MultiStageLoader):
-    stages = [InfoLoader, BestChainsAndModelsLoader]
+    stages = [InfoLoader, BestChainsAndModelsLoader, SpeciesLoader]
