897478f57093a20317014af26315acdfdee6d136
Blake Sweeney <blakes.85@gmail.com>
2015-03-02 13:22:19 -0500

    Make export ignore problems with viral structures
    We want to keep working even if we have problems processing cif files. Notably
    we should not stop if we are given a viral structure.

--- pymotifs/export/cifatom.py
+++ pymotifs/export/cifatom.py
@@ -7,6 +7,7 @@ import os
 from pymotifs import core
 
 from fr3d.cif.writer import CifAtom
+from fr3d.cif.reader import ComplexOperatorException
 
 
 class Exporter(core.Stage):
@@ -15,7 +16,15 @@ class Exporter(core.Stage):
         return os.path.join(self.config['locations']['fr3d_root'], "PDBFiles",
                             pdb + ".cifatoms")
 
+    def is_missing(self, entry, **kwargs):
+        """Will check if the file produce by filename() exists.
+        """
+        return not os.path.exists(self.filename(entry))
+
     def process(self, pdb, **kwargs):
         with open(self.filename(pdb), 'wb') as out:
             writer = CifAtom(out)
-            writer(self.structure(pdb))
+            try:
+                writer(self.structure(pdb))
+            except ComplexOperatorException:
+                self.logger.warning("Cannot export %s to cifatom", pdb)
