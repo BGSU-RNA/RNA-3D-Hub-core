3ff4e405684c25e3ca13f55e6b4948a1ce3fd24c
Blake Sweeney <blakes.85@gmail.com>
2015-03-24 21:38:16 -0400

    Add some documentation

--- pymotifs/correspondence/summary.py
+++ pymotifs/correspondence/summary.py
@@ -1,3 +1,8 @@
+"""This is a loader to summarize the correspondences. This requires that the
+positions have already been loaded, or bad things will happen. The
+summarization is key to building the nr sets correctly.
+"""
+
 from sqlalchemy.orm import aliased
 
 from pymotifs import core
@@ -26,9 +31,17 @@ class Loader(core.Loader):
             return [result.id for result in query]
 
     def remove(self, corr_id, **kwargs):
+        """We do not remove anything when summarizing as we aren't actually
+        adding any rows when we do this, to force a recompute of this data you
+        should do a recompute on the whole correspondence level information.
+        """
         pass
 
     def has_data(self, corr_id, **kwargs):
+        """Check if we have summarized this correspondence before. This only
+        looks for the length field not being null.
+        """
+
         with self.session() as session:
             query = session.query(Info).\
                 filter(Info.id == corr_id).\
@@ -36,12 +49,19 @@ class Loader(core.Loader):
             return bool(query.count())
 
     def current(self, corr_id):
+        """Get the current data for the correspondence.
+        """
+
         with self.session() as session:
             info = session.query(Info).get(corr_id)
             print(info)
             return utils.row2dict(info)
 
     def data(self, corr_id, **kwargs):
+        """Compute the summary for the given correspondence id. This will
+        update the entry with the counts of match, mismatch and such.
+        """
+
         data = self.current(corr_id)
         data.update({
             'length': 0,
