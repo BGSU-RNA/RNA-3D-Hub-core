1f3c364eef539a4005b17424bf3b9d64c6b193a0
Blake Sweeney <blakes.85@gmail.com>
2015-03-31 14:30:22 -0400

    Fix some imported names
    pdb -> pdbs and CifAtoms -> CifAtom

--- pymotifs/update.py
+++ pymotifs/update.py
@@ -7,7 +7,7 @@ from pymotifs import core
 from pymotifs import download
 from pymotifs import export
 from pymotifs import units
-from pymotifs import pdb
+from pymotifs import pdbs
 from pymotifs import loops
 from pymotifs import chains
 from pymotifs import interactions
@@ -15,8 +15,8 @@ from pymotifs import interactions
 
 class Loader(core.MultiStageLoader):
     stages = [download.Downloader,
-              export.CifAtoms,
-              pdb.Loader,
+              export.CifAtom,
+              pdbs.Loader,
               chains.Loader,
               units.Loader,
               interactions.Loader,
