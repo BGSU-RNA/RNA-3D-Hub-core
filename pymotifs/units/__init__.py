9e670972373ec23b523a8c83e8e9c220c6b65aea
Blake Sweeney <blakes.85@gmail.com>
2015-04-05 14:04:57 -0400

    Add distances to units loader
    We will now load the distances as well.

--- pymotifs/units/__init__.py
+++ pymotifs/units/__init__.py
@@ -2,10 +2,11 @@ from pymotifs import core
 
 from pymotifs.units.info import Loader as InfoLoader
 # from pymotifs.units.coordinates import Loader as CoordinateLoader
-# from pymotifs.units.distances import Loader as DistancesLoader
+from pymotifs.units.distances import Loader as DistancesLoader
 from pymotifs.units.quality import Loader as QualityLoader
 from pymotifs.units. redundant import RedundantNucleotidesLoader
 
 
 class Loader(core.MultiStageLoader):
-    stages = [InfoLoader, QualityLoader, RedundantNucleotidesLoader]
+    stages = [InfoLoader, QualityLoader, DistancesLoader,
+              RedundantNucleotidesLoader]
