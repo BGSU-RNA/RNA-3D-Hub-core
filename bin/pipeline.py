b19f89d7868d6979b305f23d44ac8ea93449bec1
Blake Sweeney <blakes.85@gmail.com>
2015-04-07 13:49:37 -0400

    Pass options to dispatcher
    The dispatcher will take a skip_dependencies option which should be passed
    along. In addition, it seems captureWarnings is not available in my python
    version, though I didn't see mention of that in the docs.

--- bin/pipeline.py
+++ bin/pipeline.py
@@ -36,7 +36,7 @@ def setup_logging(opts):
         log_args['filename'] = opts.pop('log_file')
 
     logging.basicConfig(**log_args)
-    logging.captureWarnings(True)
+    # logging.captureWarnings(True)
     base = logging.getLogger()
     pool_logger = logging.getLogger('sqlalchemy.pool')
     pool_logger.setLevel(logging.ERROR)
@@ -67,7 +67,8 @@ def run(name, config, pdbs, opts):
     models.reflect(engine)
     Session = sessionmaker(bind=engine)
 
-    dispatcher = Dispatcher(name, config, Session)
+    dispatcher = Dispatcher(name, config, Session,
+                            skip_dependencies=opts.get('skip_dependencies'))
 
     try:
         dispatcher(pdbs, **opts)
@@ -93,6 +94,8 @@ if __name__ == '__main__':
                         help="Use only downloaded pdbs")
     parser.add_argument('--dry-run', action='store_true',
                         help="Do a dry run where we store nothing")
+    parser.add_argument('--skip-dependencies', action='store_true',
+                        help='Skip running any dependencies')
 
     parser.add_argument('--log-file', dest='log_file', default='',
                         help="Log file to use")
