87fc33f1486f31cf33f0191c174d060a92ad0ec4
Blake Sweeney <blakes.85@gmail.com>
2015-04-07 13:51:39 -0400

    Handling missing species better
    If we can't get a species then we should store null, not a place holder. Plus we
    should handle the case where we can't find the taxon id at all gracefully. This
    means also storing null.

--- pymotifs/chains/species.py
+++ pymotifs/chains/species.py
@@ -2,7 +2,7 @@ from pymotifs import core
 from pymotifs.models import ChainSpecies
 from pymotifs.models import ChainInfo
 from pymotifs.utils.structures import Structure
-from pymotifs.utils.structures import SYNTHEIC
+from pymotifs.utils.structures import UnknownTaxonomyException
 
 from pymotifs.chains.info import Loader as ChainLoader
 from pymotifs.species_mapping import Loader as SpeciesLoader
@@ -33,8 +33,10 @@ class Loader(core.Loader):
         helper = Structure(self.session.maker)
         data = []
         for chain_name, chain_id in helper.rna_chains(pdb, return_id=True):
-            species = helper.source(pdb, chain_name, simplify=True)
-            if species is None:
-                species = SYNTHEIC[0]
+            species = None
+            try:
+                species = helper.source(pdb, chain_name, simplify=True)
+            except UnknownTaxonomyException as err:
+                self.logger.warning(str(err))
             data.append(ChainSpecies(chain_id=chain_id, species_id=species))
         return data
