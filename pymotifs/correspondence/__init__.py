71324727432e2ffad12573f949336231c3f1cf27
Blake Sweeney <blakes.85@gmail.com>
2015-06-23 16:14:07 -0400

    Depend on all stages of exp_seq
    This way we will make sure unit mapping and the like is always done before
    correspondence stuff as well. It would be missed otherwise and this is surprising
    if you do correspondence and then look at some views to see unit level
    correspondences.

--- pymotifs/correspondence/__init__.py
+++ pymotifs/correspondence/__init__.py
@@ -4,8 +4,10 @@ from pymotifs.correspondence.positions import Loader as PositionLoader
 from pymotifs.correspondence.info import Loader as InfoLoader
 from pymotifs.correspondence.cleanup import Loader as Cleanup
 from pymotifs.correspondence.summary import Loader as SummaryLoader
+from pymotifs.exp_seq import Loader as ExpSeqLoader
 # from pymotifs.correspondence.interactions import Loader as InterLoader
 
 
 class Loader(core.MultiStageLoader):
-    dependencies = set([InfoLoader, PositionLoader, SummaryLoader, Cleanup])
+    dependencies = set([ExpSeqLoader, InfoLoader, PositionLoader,
+                        SummaryLoader, Cleanup])
