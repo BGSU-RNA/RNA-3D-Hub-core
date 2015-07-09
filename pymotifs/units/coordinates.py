81802e67573d771537f15da6b3a2838a323e62cf
Blake Sweeney <blakes.85@gmail.com>
2015-07-09 14:02:23 -0400

    Initial attempt at using fr3d to get coordinates
    This is basically the same as the old method for creating coordinate
    information. This may not be ideal as this will still produce the errors we have
    observed on the site. However, there are not any great alternatives right now.

--- pymotifs/units/coordinates.py
+++ pymotifs/units/coordinates.py
@@ -1,3 +1,6 @@
+import os
+import csv
+
 import pymotifs.core as core
 
 from pymotifs.models import UnitInfo
@@ -13,5 +16,24 @@ class Loader(core.SimpleLoader):
             join(UnitInfo, UnitInfo.id == UnitCoordinates.id).\
             filter(UnitInfo.pdb_id == pdb)
 
+    def parse(self, filename):
+        data = []
+        with open(filename, 'rb') as raw:
+            reader = csv.reader(raw, delimiter=',', quotechar='"')
+            for row in reader:
+                data.append(UnitCoordinates(id=row[0], coordinates=row[9]))
+        return data
+
     def data(self, pdb, **kwargs):
-        pass
+        mlab = core.Matlab()
+        ifn, status, err_msg = mlab.loadCoordinates(pdb, nout=3)
+        status = status[0][0]
+        if status == 0:
+            data = self.parse(ifn)
+            os.remove(ifn)
+            return data
+        elif status == 2:
+            self.logger.warning('File %s has no nucleotides', pdb)
+        else:
+            raise core.MatlabFailed('Matlab error code %i when analyzing %s' %
+                                    (status, pdb))
