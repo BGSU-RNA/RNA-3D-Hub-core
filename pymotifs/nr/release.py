9566b67ee1ec94cabe5f2ffea77c5b48c29836db
Blake Sweeney <blakes.85@gmail.com>
2015-07-24 11:19:47 -0400

    Some updates to the release loader

--- pymotifs/nr/release.py
+++ pymotifs/nr/release.py
@@ -1,13 +1,19 @@
-import datetime
+import datetime as dt
 
 from pymotifs import core
 from pymotifs.models import NrReleases
 from pymotifs.utils.releases import Release
 
+from pymotifs.chain.info import Loader as ChainLoader
+from pymotifs.interactions import Loader as InteractionLoader
+
 
 class Loader(core.MassLoader):
+    dependencies = set([ChainLoader, InteractionLoader])
+    update_gap = dt.timedelta(7)
+
     def data(self, *args, **kwargs):
-        now = datetime.datetime.now()
+        now = dt.datetime.now()
         helper = Release(self.config, self.session.maker)
         current = helper.current('nr')
         next = helper.next(current, mode=self.config['release_mode']['nrlist'])
