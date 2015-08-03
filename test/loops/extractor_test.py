353374ed4b501535242a50b362c0f4edc62504a0
Blake Sweeney <blakes.85@gmail.com>
2015-08-02 22:31:21 -0400

    Correct failing tests
    These tests were failing due to changes in the database. Also some changes to
    how things are passed. This updates test to reflect that.

--- test/loops/extractor_test.py
+++ test/loops/extractor_test.py
@@ -8,16 +8,10 @@ class DeterminingComputationTest(StageTest):
     loader_class = Loader
 
     def test_knows_if_has_pdb_and_type(self):
-        self.assertTrue(self.loader.has_data(('1S72', 'IL')))
+        self.assertTrue(self.loader.has_data('1S72'))
 
     def test_knows_if_missing_pdb(self):
-        self.assertFalse(self.loader.has_data(('0S72', 'IL')))
-
-    def test_knows_if_missing_type(self):
-        self.assertFalse(self.loader.has_data(('1S72', '0L')))
-
-    def test_recomputes_if_missing_loops(self):
-        self.assertTrue(self.loader.should_process(('1S72', '0L')))
+        self.assertFalse(self.loader.has_data('0S72'))
 
 
 class MappingTests(StageTest):
@@ -26,7 +20,7 @@ class MappingTests(StageTest):
     def test_can_get_a_correct_mapping(self):
         mapping = self.loader._mapping('2AW7', 'HL')
         val = mapping[mapping.keys()[0]]
-        self.assertEquals('HL_2AW7_002', val)
+        self.assertEquals('HL_2AW7_019', val)
 
     def test_gets_empty_mapping_for_missing_structure(self):
         val = self.loader._mapping('0000', 'IL')
@@ -59,7 +53,7 @@ class GettingLoopIdsTest(StageTest):
 
     def test_generates_a_new_id_for_an_unknown_loop(self):
         val = self.loader._get_loop_id('bob', '2AW7', 'IL', self.mapping)
-        self.assertEquals('IL_2AW7_069', val)
+        self.assertEquals('IL_2AW7_110', val)
 
     def test_uses_old_id_for_known_loop(self):
         nts = (
