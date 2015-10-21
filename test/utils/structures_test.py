0c16079755f5456a961850bf9985daf3d856045f
Blake Sweeney <blakes.85@gmail.com>
2015-10-21 15:08:03 -0400

    Update tests for new data
    We changed some pdbs that we use so the tests must change to reflect this.

--- test/utils/structures_test.py
+++ test/utils/structures_test.py
@@ -9,7 +9,7 @@ class SourceTest(QueryUtilTest):
     query_class = st.Structure
 
     def test_can_find_a_simple_taxon_id(self):
-        self.assertEquals([562], self.db_obj.source('2AW7', 'A'))
+        self.assertEquals([562], self.db_obj.source('4V4Q', 'AA'))
 
     def test_knows_when_there_is_no_known_taxon_id(self):
         self.assertEquals([], self.db_obj.source('1ET4', 'A'))
@@ -25,11 +25,10 @@ class SourceTest(QueryUtilTest):
         self.assertEquals(None, self.db_obj.source('3T4B', 'A', simplify=True))
 
     def test_simplifies_to_just_first_id(self):
-        self.assertEquals(562, self.db_obj.source('2AW7', 'A', simplify=True))
+        self.assertEquals(562, self.db_obj.source('4V4Q', 'AA', simplify=True))
 
     def test_can_handle_annotations_that_disagree(self):
-        # TODO: Test 4L6M|*|V,4L6M|*|W
-        self.assertEquals(562, self.db_obj.source('4L6M', 'V', simplify=True))
+        self.assertEquals(2238, self.db_obj.source('3CPW', 'A', simplify=True))
 
 
 class RnaChainsTest(QueryUtilTest):
@@ -39,15 +38,12 @@ class RnaChainsTest(QueryUtilTest):
         self.assertEquals(['A'], self.db_obj.rna_chains('3T4B'))
 
     def test_will_filter_out_non_rna_chains(self):
-        self.assertEquals(['A'], self.db_obj.rna_chains('2AW7'))
+        ans = set(['AA', 'CA', 'BA', 'DA', 'BB', 'DB'])
+        self.assertEquals(ans, set(self.db_obj.rna_chains('4V4Q')))
 
     def test_can_return_ids_and_names(self):
-        val = self.db_obj.rna_chains('2AW7', return_id=True)
-        self.assertEquals([('A', 4977)], val)
-
-    def test_does_not_load_mislabeled_chain(self):
-        val = self.db_obj.rna_chains('3CPW', strict=True)
-        self.assertTrue('A' not in val)
+        val = self.db_obj.rna_chains('4V4Q', return_id=True)[0]
+        self.assertEquals(('AA', 173), val)
 
 
 class LoopLoadingTest(QueryUtilTest):
