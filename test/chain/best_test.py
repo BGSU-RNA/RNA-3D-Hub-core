d7283fec600eee0e0ed403ecf24655509ddf1577
Blake Sweeney <blakes.85@gmail.com>
2015-10-21 14:19:21 -0400

    Skip a best_chains_and_models test without matlab
    If we don't have matlab we will not have computed this. This means we will never
    be able to pass this test. We skip it without matlab.

--- test/chain/best_test.py
+++ test/chain/best_test.py
@@ -1,12 +1,14 @@
 from test import StageTest
+from test import skip_without_matlab
 from pymotifs.chains.best import BestChainsAndModelsLoader
 
 
 class QueryingTest(StageTest):
     loader_class = BestChainsAndModelsLoader
 
+    @skip_without_matlab
     def test_knows_if_known_pdb(self):
-        self.assertTrue(self.loader.has_data('1S72'))
+        self.assertTrue(self.loader.has_data('124D'))
 
     def test_knows_if_missingown_pdb(self):
         self.assertFalse(self.loader.has_data('0S72'))
