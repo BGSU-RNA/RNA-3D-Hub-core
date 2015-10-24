e694ac07417a1e1ddde611a443855c6a0f1a50d9
Blake Sweeney <blakes.85@gmail.com>
2015-10-24 10:59:02 -0400

    Add a config module to pymotifs
    I don't like having to write out paths that can be inferred in most cases. This
    adds such functionality as well as some tests for it. The merge function is
    taken from a blog post as noted in it's doc string.

---
+++ pymotifs/config.py
@@ -0,0 +1,58 @@
+import os
+import json
+import collections
+from copy import deepcopy
+
+
+def merge(a, b):
+    """recursively merges dict's. not just simple a['key'] = b['key'], if
+    both a and b have a key who's value is a dict then dict_merge is
+    called on both values and the result stored in the returned
+    dictionary.
+
+    Taken from:
+    https://www.xormedia.com/recursively-merge-dictionaries-in-python/
+
+    :a: A dictionary.
+    :b: Another dictionary.
+    """
+
+    if not isinstance(b, dict):
+        return b
+
+    result = deepcopy(a)
+    for k, v in b.iteritems():
+        if k in result and isinstance(result[k], dict):
+            result[k] = merge(result[k], v)
+        else:
+            result[k] = deepcopy(v)
+
+    return result
+
+
+def defaults():
+    """A function to create a dictionary with the default values for the
+    pipeline.
+    """
+
+    here = os.path.dirname(__file__)
+    base = os.path.abspath(os.path.join(here, '..'))
+
+    return {
+        "locations": {
+            "loops_mat_files": os.path.join(base, "MotifAtlas",
+                                            "PrecomputedData"),
+            "loops_search_dir": os.path.join(base, "MotifAtlas", "aAa"),
+            "log_dir": os.path.join(base, "MotifAtlas", "logs"),
+            "releases_dir": os.path.join(base, "MotifAtlas", "Releases"),
+            "fr3d_root": os.path.join(base, "FR3D"),
+            "2ds_destination":  os.path.join(base, "MotifAtlas", "2d"),
+        }
+    }
+
+
+def load(filename):
+    config = collections.defaultdict(dict)
+    with open(filename, 'rb') as raw:
+        config.update(merge(defaults(), json.load(raw)))
+        return config
---
+++ test/config_test.py
@@ -0,0 +1,38 @@
+import os
+from unittest import TestCase
+
+from pymotifs import config
+
+
+class ConfigTest(TestCase):
+    def setUp(self):
+        self.conf = config.load("conf/test.json")
+
+    def test_can_load_data(self):
+        self.assertTrue('db' in self.conf)
+
+    def test_uses_default_dict(self):
+        val = self.conf['locations']['log_dir']
+        ans = os.path.join(os.getcwd(), "MotifAtlas", "logs")
+        self.assertEquals(ans, val)
+
+    def test_does_not_override_nested_values(self):
+        val = self.conf['locations']['loops_mat_files']
+        ans = "/home/pipeline/hub-core/MotifAtlas/PrecomputedData"
+        self.assertEquals(ans, val)
+
+
+class MergingTest(TestCase):
+    def test_can_merge_two_dicts(self):
+        a = {'c': 3}
+        b = {'d': [1, 2, 3]}
+        val = config.merge(a, b)
+        ans = {'c': 3, 'd': [1, 2, 3]}
+        self.assertEquals(ans, val)
+
+    def test_can_recursively_merge(self):
+        a = {'a': {'a': 1}, 'c': 3}
+        b = {'a': {'b': 2}, 'd': [1, 2, 3]}
+        val = config.merge(a, b)
+        ans = {'a': {'a': 1, 'b': 2}, 'c': 3, 'd': [1, 2, 3]}
+        self.assertEquals(ans, val)
