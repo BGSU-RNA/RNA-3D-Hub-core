2b41eaff767e04d51e3f3bf426bae315cb22d758
Blake Sweeney <blakes.85@gmail.com>
2015-10-24 11:03:38 -0400

    Update main entry point
    The main entry point should use the new setup which uses stages instead of
    dependencies. Also we include the interaction export and loops importing.

--- pymotifs/export/__init__.py
+++ pymotifs/export/__init__.py
@@ -1,1 +1,2 @@
 from pymotifs.export.cifatom import Exporter as CifAtom
+from pymotifs.export.interactions import InteractionExporter
--- pymotifs/update.py
+++ pymotifs/update.py
@@ -7,25 +7,27 @@ from pymotifs import core
 from pymotifs import download
 from pymotifs import units
 from pymotifs import pdbs
-# from pymotifs import loops
+from pymotifs import loops
 from pymotifs import chains
 from pymotifs import interactions
 from pymotifs import exp_seq
 from pymotifs import correspondence
 from pymotifs import species_mapping
 from pymotifs import ife
+from pymotifs import export
 
 
 class Loader(core.MultiStageLoader):
-    dependencies = set([
+    stages = set([
         download.Downloader,
         pdbs.Loader,
         chains.Loader,
         species_mapping.Loader,
         units.Loader,
         interactions.Loader,
-        # loops.Loader,
+        loops.Loader,
         exp_seq.Loader,
         correspondence.Loader,
         ife.Loader,
+        export.InteractionExporter
     ])
