2ab999df77eb3667ff942f7d1185a76d06f86eb0
Blake Sweeney <blakes.85@gmail.com>
2015-10-26 19:58:58 -0400

    Update some pdb ids

--- test/utils/correspondence_test.py
+++ test/utils/correspondence_test.py
@@ -8,7 +8,7 @@ class AlignedChainsTest(StageTest):
 
     def setUp(self):
         super(AlignedChainsTest, self).setUp()
-        self.alignments = self.loader.aligned_chains(['4KJ2', '4KJ4'])
+        self.alignments = self.loader.aligned_chains(['4V7W', '4V42'])
 
     def test_can_load_all_alignments_from_the_pdb(self):
         self.assertEquals([24284, 24230], self.alignments.keys())
