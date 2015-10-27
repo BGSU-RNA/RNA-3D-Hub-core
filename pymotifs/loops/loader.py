e86209adcffa5fdeac63859866ab0466e603d87d
Blake Sweeney <blakes.85@gmail.com>
2015-10-26 21:36:41 -0400

    Include QA in loop loading

--- pymotifs/loops/loader.py
+++ pymotifs/loops/loader.py
@@ -2,7 +2,8 @@ from pymotifs import core
 
 from pymotifs.loops.extractor import Loader as Extractor
 from pymotifs.loops.positions import Loader as PositionLoader
+from pymotifs.loops.quality import Loader as QALoader
 
 
 class Loader(core.StageContainer):
-    stages = set([Extractor, PositionLoader])
+    stages = set([Extractor, PositionLoader, QALoader])
