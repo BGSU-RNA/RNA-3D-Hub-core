2e1a1677e3c661f4af86a6886d455d9cb67d5de9
Blake Sweeney <blakes.85@gmail.com>
2015-10-27 21:27:29 -0400

    Correct and add tests
    We add some tests to the exp_seq.info loader as well as simplify it. This is now
    much simpler as it uses SimpleLoader.

--- pymotifs/exp_seq/info.py
+++ pymotifs/exp_seq/info.py
@@ -1,16 +1,14 @@
 import hashlib
 
 from pymotifs import core
+from pymotifs import models as mod
 from pymotifs.utils import grouper
 from pymotifs.utils.structures import Structure
-from pymotifs.models import ExpSeqInfo as Info
-from pymotifs.models import ChainInfo
 from pymotifs.chains.info import Loader as ChainLoader
 
 
-class Loader(core.Loader):
+class Loader(core.SimpleLoader):
     dependencies = set([ChainLoader])
-
     mark = False
 
     def to_process(self, pdbs, **kwargs):
@@ -20,8 +18,8 @@ class Loader(core.Loader):
             chain_ids = helper.rna_chains(pdbs, return_id=True)
             chain_ids = set(p[1] for p in chain_ids)
             with self.session() as session:
-                query = session.query(ChainInfo.sequence).\
-                    filter(ChainInfo.chain_id.in_(chain_ids)).\
+                query = session.query(mod.ChainInfo.sequence).\
+                    filter(mod.ChainInfo.chain_id.in_(chain_ids)).\
                     distinct()
 
                 for result in query:
@@ -29,22 +27,12 @@ class Loader(core.Loader):
 
         return list(sequences)
 
-    def remove(self, sequence, **kwargs):
-        md5 = self.md5(sequence)
-        with self.session() as session:
-            session.query(Info).\
-                filter(Info.md5 == md5).\
-                delete(synchronize_session=False)
-
-    def has_data(self, sequence, **kwargs):
-        md5 = self.md5(sequence)
-        with self.session() as session:
-            query = session.query(Info).\
-                filter(Info.md5 == md5)
-            return bool(query.count())
+    def query(self, session, sequence):
+        return session.query(mod.ExpSeqInfo).\
+            filter(mod.ExpSeqInfo.md5 == self.md5(sequence))
 
     def md5(self, sequence):
         return hashlib.md5(sequence).hexdigest()
 
     def data(self, seq, **kwargs):
-        return Info(sequence=seq, md5=self.md5(seq), length=len(seq))
+        return mod.ExpSeqInfo(sequence=seq, md5=self.md5(seq), length=len(seq))
--- test/exp_seq/info_test.py
+++ test/exp_seq/info_test.py
@@ -7,7 +7,10 @@ class InfoTest(StageTest):
     loader_class = Loader
 
     def test_can_get_all_unique_sequences(self):
-        self.assertEquals(2, len(self.loader.sequences('1S72')))
+        self.assertEquals(2, len(self.loader.to_process(['1S72'])))
 
-    def test_can_generate_all_entries(self):
-        self.assertEquals(2, len(self.loader.data('1S72')))
+    def test_knows_if_it_has_data(self):
+        self.assertTrue(self.loader.has_data('UUUUCU'))
+
+    def test_knows_if_it_does_not_have_data(self):
+        self.assertFalse(self.loader.has_data('UUUUGU'))
