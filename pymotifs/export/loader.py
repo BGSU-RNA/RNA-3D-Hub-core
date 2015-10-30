a2b74c0ff31eb72efdf2315308cea587b79b58ab
Blake Sweeney <blakes.85@gmail.com>
2015-10-30 13:58:49 -0400

    Correct importing names

--- pymotifs/export/loader.py
+++ pymotifs/export/loader.py
@@ -1,7 +1,7 @@
 from pymotifs.core import StageContainer
 
 from pymotifs.export.cifatom import Exporter as CifAtom
-from pymotifs.export.interactions import InteractionExporter
+from pymotifs.export.interactions import Exporter as InteractionExporter
 from pymotifs.export.loops import Exporter as LoopExporter
 
 
