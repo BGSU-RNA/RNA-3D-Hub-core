2b561924438e2852d386996cc28b314a171ef310
Blake Sweeney <blakes.85@gmail.com>
2015-11-10 19:23:35 -0500

    Add normalization to exp_seq.info
    We want to store the normalized sequences ahead of time. This will prevent
    putting bad data into the database. Also we will save a little time as we will
    not constantly renormalise sequences. And best of all the there will be less
    noise in the log files.

--- pymotifs/exp_seq/info.py
+++ pymotifs/exp_seq/info.py
@@ -10,29 +10,81 @@ from pymotifs.chains.info import Loader as ChainLoader
 class Loader(core.SimpleLoader):
     dependencies = set([ChainLoader])
     mark = False
+    table = mod.ExpSeqInfo
 
     def to_process(self, pdbs, **kwargs):
         sequences = set()
         helper = Structure(self.session.maker)
-        for chunk in grouper(100, pdbs):
-            chain_ids = helper.rna_chains(pdbs, return_id=True)
-            chain_ids = set(p[1] for p in chain_ids)
+        for chunk in grouper(1000, pdbs):
+            ids = set(p[1] for p in helper.rna_chains(chunk, return_id=True))
             with self.session() as session:
                 query = session.query(mod.ChainInfo.sequence).\
-                    filter(mod.ChainInfo.chain_id.in_(chain_ids)).\
+                    filter(mod.ChainInfo.chain_id.in_(ids)).\
                     distinct()
 
-                for result in query:
-                    sequences.add(result.sequence)
+                sequences.update(result.sequence for result in query)
 
-        return list(sequences)
+        return sorted(sequences, key=lambda s: (len(s), s))
 
     def query(self, session, sequence):
-        return session.query(mod.ExpSeqInfo).\
-            filter(mod.ExpSeqInfo.md5 == self.md5(sequence))
+        """The query to find and remove all exp seq info entries for a given
+        sequence.
+        """
+        return session.query(mod.ExpSeqInfo).filter_by(md5=self.md5(sequence))
 
     def md5(self, sequence):
+        """Compute the md5 hash of a sequence.
+        """
         return hashlib.md5(sequence).hexdigest()
 
+    @property
+    def translation(self):
+        """The translation dictonary for this loader. It will translate
+        characters from modified notation to A, C, G, U only.
+        """
+
+        if hasattr(self, '_translation'):
+            return self._translation
+
+        # X is really N, but old PDB data doesn't respect that.
+        self._translation = {'X': 'N'}
+        table = mod.RnaUnitModifiedCorrespondencies
+        with self.session() as session:
+            query = session.query(
+                table.rna_unit_modified_correspondencies_id,
+                table.standard_unit,
+            )
+
+            for result in query:
+                self._translation[result[0]] = result[1]
+        return self._translation
+
+    def translate(self, character):
+        if character in set(['A', 'C', 'G', 'U', 'N']):
+            return character
+        return self.translation.get(character, None)
+
+    def normalize(self, sequence):
+        normalized = []
+        size = len(sequence) - 1
+        for index, seq in enumerate(sequence):
+            norm = self.translate(seq)
+            if not norm:
+                if index == size:
+                    self.logger.warning("Skipping final unit %s tRNA/AA", norm)
+                    continue
+                return None
+            normalized.append(norm)
+
+        return ''.join(normalized)
+
     def data(self, seq, **kwargs):
-        return mod.ExpSeqInfo(sequence=seq, md5=self.md5(seq), length=len(seq))
+        normalized = self.normalize(seq)
+        return {
+            'sequence': seq,
+            'md5': self.md5(seq),
+            'normalized': normalized,
+            'length': len(seq),
+            'normalized_length': len(normalized) if normalized else 0,
+            'was_normalized': seq is not None
+        }
--- test/exp_seq/info_test.py
+++ test/exp_seq/info_test.py
@@ -6,11 +6,39 @@ from pymotifs.exp_seq.info import Loader
 class InfoTest(StageTest):
     loader_class = Loader
 
-    def test_can_get_all_unique_sequences(self):
-        self.assertEquals(2, len(self.loader.to_process(['1S72'])))
-
     def test_knows_if_it_has_data(self):
         self.assertTrue(self.loader.has_data('UUUUCU'))
 
     def test_knows_if_it_does_not_have_data(self):
         self.assertFalse(self.loader.has_data('UUUUGU'))
+
+
+class ToProcessTest(StageTest):
+    loader_class = Loader
+
+    def test_can_get_all_unique_sequences(self):
+        self.assertEquals(2, len(self.loader.to_process(['1S72'])))
+
+    def test_loads_only_unique_sequences(self):
+        val = self.loader.to_process(['1AQ4', '1AQ3', '124D'])
+        ans = ['CAUGUGAC', 'ACAUGAGGAUUACCCAUGU']
+        self.assertEquals(ans, val)
+
+
+class NormalizationTest(StageTest):
+    loader_class = Loader
+
+    def test_can_normalize_a_sequence(self):
+        self.assertEquals('ACGUNN', self.loader.normalize('ACIUNN'))
+
+    def test_does_not_modify_a_good_sequence(self):
+        self.assertEquals('ACGUNN', self.loader.normalize('ACGUNN'))
+
+    def test_turns_X_into_N(self):
+        self.assertEquals('ACGUNN', self.loader.normalize('ACGUNX'))
+
+    def test_returns_none_if_cannot_normalize_all_characters(self):
+        self.assertEquals(None, self.loader.normalize('ACGBN'))
+
+    def test_will_remove_last_char_if_cannot_normalize(self):
+        self.assertEquals('ACGUNN', self.loader.normalize('ACGUNXT'))
