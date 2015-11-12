b53ef9095550c6eda49d05755e98940e094f2ad1
Blake Sweeney <blakes.85@gmail.com>
2015-11-12 11:44:04 -0500

    Remove most code in release utilities
    We don't need it to do so much code in this. It should do less. In particular
    the code to get previous is always unneeded and overly complicated, thus it was
    removed. In addition, we remove the code to get the current one. While this ends
    up with some cut and paste code in places I think it makes things overall much
    simpler.

--- pymotifs/utils/releases.py
+++ pymotifs/utils/releases.py
@@ -1,25 +1,3 @@
-from pymotifs import core
-from pymotifs.models import MlReleases
-from pymotifs.models import LoopReleases
-from pymotifs.models import NrReleases
-
-from sqlalchemy import desc
-
-
-CURRENT_QUERY = """
-SELECT {column}
-FROM {table}
-LIMIT 1;
-"""
-
-
-class UnknownReleaseType(Exception):
-    """This is raised when we are asked for the current release of an unknown
-    type of release.
-    """
-    pass
-
-
 class UnknownReleaseMode(Exception):
     """This is raised when the requested release mode is not known.
     """
@@ -34,132 +12,39 @@ class BadlyFormattedRelease(Exception):
     pass
 
 
-class ImpossibleRelease(Exception):
-    """This is raised if we are asking for a preivous release which is
-    impossible. For example release -1.0 would be an example which can happen
-    when getting the previous release of 0.0.
-    """
-    pass
+MODES = ['major', 'minor']
 
 
-class Release(core.Base):
+def next_id(current, mode='minor'):
+    if mode == 'none' or mode is None:
+        return str(current)
 
-    names = ['major', 'minor', 'lookup']
+    if mode not in MODES:
+        raise UnknownReleaseMode(mode)
 
-    column_mapping = {
-        'motif': 'ml_releases_id',
-        'loop': 'loop_releases_id',
-        'nr': 'nr_release_id'
-    }
+    current = parse_id(current)
+    if mode == 'minor':
+        current['minor'] += 1
 
-    table_mapping = {
-        'motif': MlReleases,
-        'loop': LoopReleases,
-        'nr': NrReleases
-    }
+    if mode == 'major':
+        current['minor'] = 0
+        current['major'] += 1
 
-    def previous(self, release, mode='minor', bad_id='wrap', name=None):
-        if mode not in self.names:
-            raise UnknownReleaseMode(mode)
+    return format_id(current)
 
-        if bad_id not in ['wrap', 'none', 'raise']:
-            raise ValueError('Invalid bad_id action %s' % bad_id)
 
-        current = self.__parse__(release)
-        if mode == 'minor':
-            current['minor'] -= 1
+def parse_id(release_id):
+    try:
+        parts = map(int, release_id.split('.'))
+    except:
+        raise BadlyFormattedRelease("Can't process release id: %s" %
+                                    release_id)
 
-        if mode == 'major':
-            current['major'] -= 1
-            current['minor'] = 0
+    if len(parts) != 2:
+        raise BadlyFormattedRelease("Release id must have major an minor")
 
-        if mode == 'lookup':
-            column_name = self.column_mapping[name]
-            table = self.table_mapping[name]
-            column = getattr(table, column_name)
-            with self.session() as session:
-                query = session.query(column).\
-                    order_by(desc(table.date)).\
-                    limit(2)
+    return dict(zip(MODES, parts))
 
-                results = [getattr(result, column_name) for result in query]
-                try:
-                    index = results.index(release)
-                    return results[index + 1]
-                except:
-                    return None
 
-        if current['major'] < 0:
-            if bad_id == 'none':
-                return None
-
-            if bad_id == 'wrap':
-                current['major'] = 0
-                current['minor'] = 1
-
-            if bad_id == 'raise':
-                raise ImpossibleRelease(release)
-
-        if current['minor'] < 0:
-            if bad_id == 'wrap':
-                current['minor'] = 0
-
-            if bad_id == 'none':
-                return None
-
-            if bad_id == 'raise':
-                raise ImpossibleRelease(release)
-
-        if current['major'] == 0 and current['minor'] == 0:
-            current['minor'] = 1
-
-        return self.__format__(current)
-
-    def current(self, name):
-        if name not in ['nr', 'loop', 'motif']:
-            raise UnknownReleaseType(name)
-
-        column_name = self.column_mapping[name]
-        table = self.table_mapping[name]
-        column = getattr(table, column_name)
-        with self.session() as session:
-            query = session.query(column).\
-                order_by(desc(table.date)).\
-                limit(1)
-
-            if query.count() == 0:
-                return '0.0'
-
-            return getattr(query.one(), column_name)
-
-    def next(self, current, mode='minor'):
-        if mode == 'none' or mode is None:
-            return str(current)
-
-        if mode not in self.names:
-            raise UnknownReleaseMode(mode)
-
-        current = self.__parse__(current)
-        if mode == 'minor':
-            current['minor'] += 1
-
-        if mode == 'major':
-            current['minor'] = 0
-            current['major'] += 1
-
-        return self.__format__(current)
-
-    def __parse__(self, release_id):
-        try:
-            parts = map(int, release_id.split('.'))
-        except:
-            raise BadlyFormattedRelease("Can't process release id: %s" %
-                                        release_id)
-
-        if len(parts) != 2:
-            raise BadlyFormattedRelease("Release id must have major an minor")
-
-        return dict(zip(self.names, parts))
-
-    def __format__(self, current):
-        return '.'.join((str(current['major']), str(current['minor'])))
+def format_id(current):
+    return '.'.join((str(current['major']), str(current['minor'])))
--- test/utils/release_test.py
+++ test/utils/release_test.py
@@ -1,127 +1,26 @@
 from test import StageTest
 
-from datetime import date
-from datetime import timedelta
-
-from pymotifs.models import NrReleases
-from pymotifs.utils.releases import Release
-from pymotifs.utils.releases import UnknownReleaseType
-from pymotifs.utils.releases import UnknownReleaseMode
-from pymotifs.utils.releases import BadlyFormattedRelease
-from pymotifs.utils.releases import ImpossibleRelease
-
-
-class GettingReleaseIdTest(StageTest):
-    loader_class = Release
-
-    def tearDown(self):
-        with self.loader.session() as session:
-            session.query(NrReleases).delete()
-
-    def test_can_get_current_loop_release(self):
-        self.assertEquals('0.0', self.loader.current('loop'))
-
-    def test_can_get_current_motif_release(self):
-        self.assertEquals('0.0', self.loader.current('motif'))
-
-    def test_can_get_current_nr_release(self):
-        self.assertEquals('0.0', self.loader.current('nr'))
-
-    def test_can_get_current_nr_release_with_existing_one(self):
-        today = date.today()
-        yesterday = today - timedelta(1)
-        with self.loader.session() as session:
-            session.add(NrReleases(nr_release_id='0.1', date=yesterday))
-            session.add(NrReleases(nr_release_id='1.1', date=today))
-
-        self.assertEquals('1.1', self.loader.current('nr'))
-
-    def test_complains_for_unknown_release_type(self):
-        self.assertRaises(UnknownReleaseType, self.loader.current, 'bob')
-
-
-class GettingWithNoCurrentReleaseTest(StageTest):
-    loader_class = Release
-
-    def setUp(self):
-        super(GettingWithNoCurrentReleaseTest, self).setUp()
-        with self.loader.session() as session:
-            session.execute('CREATE TABLE ml_releases_tmp AS (SELECT * FROM ml_releases);')
-            session.execute('delete from ml_releases')
-
-    def tearDown(self):
-        with self.loader.session() as session:
-            session.execute('insert into ml_releases select * from ml_releases_tmp;')
-            session.execute('drop table ml_releases_tmp')
-
-    def test_sets_missing_release_to_zero(self):
-        self.assertEquals('0.0', self.loader.current('motif'))
+from pymotifs.utils import releases as rel
 
 
 class ComputingNextReleaseIdTest(StageTest):
-    loader_class = Release
-
     def test_can_compute_the_next_id(self):
-        self.assertEquals('1.73', self.loader.next('1.72'))
+        self.assertEquals('1.73', rel.next_id('1.72'))
 
     def test_computes_next_major_id(self):
-        self.assertEquals('2.0', self.loader.next('1.72', mode='major'))
+        self.assertEquals('2.0', rel.next_id('1.72', mode='major'))
 
     def test_computes_next_minor_id(self):
-        self.assertEquals('1.73', self.loader.next('1.72', mode='minor'))
+        self.assertEquals('1.73', rel.next_id('1.72', mode='minor'))
 
     def test_computes_next_no_change(self):
-        self.assertEquals('1.72', self.loader.next('1.72', mode='none'))
+        self.assertEquals('1.72', rel.next_id('1.72', mode='none'))
 
     def test_complains_for_unknown_mode(self):
-        self.assertRaises(UnknownReleaseMode, self.loader.next, '1.72',
-                          mode='bob')
+        self.assertRaises(rel.UnknownReleaseMode, rel.next_id, '1.2', mode='b')
 
     def test_complains_for_badly_formatted_release_id(self):
-        self.assertRaises(BadlyFormattedRelease, self.loader.next, '1')
-        self.assertRaises(BadlyFormattedRelease, self.loader.next, '1.')
-        self.assertRaises(BadlyFormattedRelease, self.loader.next, 'a.')
-        self.assertRaises(BadlyFormattedRelease, self.loader.next, '1.a')
-
-
-class GettingPreviousReleaseId(StageTest):
-    loader_class = Release
-
-    def test_can_get_previous_release(self):
-        self.assertEquals('1.72', self.loader.previous('1.73'))
-
-    def test_can_compute_previous_major_release(self):
-        self.assertEquals('1.0', self.loader.previous('2.54', mode='major'))
-
-    def test_computes_previous_minor_id(self):
-        self.assertEquals('3.2', self.loader.previous('3.3', mode='minor'))
-
-    def test_does_not_wrap_major_below_zero(self):
-        self.assertEquals('0.1', self.loader.previous('0.54', mode='major'))
-
-    def test_does_not_wrap_minor_below_zero(self):
-        self.assertEquals('1.0', self.loader.previous('1.0', mode='minor'))
-
-    def test_will_give_none_if_requested_for_minor(self):
-        self.assertEquals(None, self.loader.previous('1.0', mode='minor',
-                                                     bad_id='none'))
-
-    def test_will_give_none_if_requested_for_major(self):
-        self.assertEquals(None, self.loader.previous('0.0', mode='major',
-                                                     bad_id='none'))
-
-    def test_will_raise_if_requested_for_bad_minor(self):
-        self.assertRaises(ImpossibleRelease, self.loader.previous,
-                          '1.0', mode='minor', bad_id='raise')
-
-    def test_will_raise_if_requested_for_bad_major(self):
-        self.assertRaises(ImpossibleRelease, self.loader.previous,
-                          '0.0', mode='major', bad_id='raise')
-
-    # def test_can_get_previous_from_database(self):
-    #     self.assertEquals('0.110', self.loader.previous('1.0', mode='lookup',
-    #                                                     name='nr'))
-
-    def test_it_gives_none_if_asked_for_previous_of_unknown(self):
-        self.assertEquals(None, self.loader.previous('3.0', mode='lookup',
-                                                     name='nr'))
+        self.assertRaises(rel.BadlyFormattedRelease, rel.next_id, '1')
+        self.assertRaises(rel.BadlyFormattedRelease, rel.next_id, '1.')
+        self.assertRaises(rel.BadlyFormattedRelease, rel.next_id, 'a.')
+        self.assertRaises(rel.BadlyFormattedRelease, rel.next_id, '1.a')
