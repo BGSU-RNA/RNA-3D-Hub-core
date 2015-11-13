f36dfc5d652da4a999c8f45dfeaa7bf86d44bd4a
Blake Sweeney <blakes.85@gmail.com>
2015-11-13 11:11:53 -0500

    Change some imports
    I prefer to import the whole module now instead of just the parts I want.

--- pymotifs/ife/info.py
+++ pymotifs/ife/info.py
@@ -1,5 +1,5 @@
-from pymotifs.core import SimpleLoader
-from pymotifs.models import IfeInfo
+from pymotifs import core
+from pymotifs import models as mod
 
 from pymotifs.ife.grouper import Grouper
 
@@ -7,19 +7,19 @@ from pymotifs.chains.loader import Loader as ChainLoader
 from pymotifs.interactions.loader import Loader as InteractionLoader
 
 
-class Loader(SimpleLoader):
+class Loader(core.SimpleLoader):
     dependencies = set([ChainLoader, InteractionLoader])
     merge_data = True
 
     def query(self, session, pdb_id):
-        return session.query(IfeInfo).filter_by(pdb_id=pdb_id)
+        return session.query(mod.IfeInfo).filter_by(pdb_id=pdb_id)
 
     def as_group(self, group):
         integral_count = None
         accompanying_count = len(group['chains'] - 1)
         structured_count = sum(g['autonomous'] for g in group['chains'])
 
-        return IfeInfo(
+        return mod.IfeInfo(
             ife_id=group['id'],
             pdb_id=group['pdb'],
             has_integral=bool(integral_count),
