ca0e4914801e27201a5922b435f77b5637b95743
Blake Sweeney <blakes.85@gmail.com>
2015-11-15 14:38:09 -0500

    Begin rewrite of loop quality
    The loop quality logic has to be reworked. It relied upon reading PDB files. As
    these do not exist for all files anymore this cannot be used. We are going to
    rewrite it in python to read cif files. It shouldn't be too bad, I hope. Most of
    the data is easily computed or already stored in our database.

--- pymotifs/loops/quality.py
+++ pymotifs/loops/quality.py
@@ -1,72 +1,154 @@
-"""
-Program for importing loop quality assurance data into the RNA 3D Hub database.
+"""Program for importing loop quality assurance data into the RNA 3D Hub
+database.
 
 Some loops should be disqualified because they have unresolved or missing
-nucleotides. See matlab code for disqualification codes.
+nucleotides.
 """
 
-import os
-import csv
+import itertools as it
+
+# from sqlalchemy.orm import aliased
 
 from pymotifs import core
+from pymotifs import utils as ut
 from pymotifs import models as mod
-from pymotifs.utils.releases import Release
 
 from pymotifs.loops.extractor import Loader as InfoLoader
+from pymotifs.loops.positions import Loader as PositionLoader
+from pymotifs.exp_seq.positions import Loader as ExpSeqPositionLoader
+from pymotifs.exp_seq.mapping import Loader as ExpSeqUnitLoader
+from pymotifs.units.info import Loader as UnitLoader
 
 
 class Loader(core.SimpleLoader):
-    dependencies = set([InfoLoader])
+    dependencies = set([InfoLoader, ExpSeqPositionLoader, ExpSeqUnitLoader,
+                        UnitLoader, PositionLoader])
+    table = mod.LoopQa
+
+    complementary = {
+        'A': 'U',
+        'C': 'G',
+        'G': set(['C', 'U']),
+        'U': set(['A', 'G']),
+    }
+
+    loop_types = ['HL', 'IL']
+
+    def current_id(self):
+        with self.session() as session:
+            query = session.query(mod.LoopReleases).\
+                order_by(mod.LoopReleases.date.desc()).\
+                limit(1)
+            return query.one().loop_releases_id
 
     def query(self, session, pdb):
-        helper = Release(self.config, self.session.maker)
-        release_id = helper.current('loop')
-        return session.query(mod.LoopQa).\
+        release_id = self.current_id()
+        return session.query(self.table).\
             join(mod.LoopInfo, mod.LoopInfo.loop_id == mod.LoopQa.loop_id).\
             filter(mod.LoopInfo.pdb_id == pdb).\
             filter(mod.LoopQa.release_id == release_id)
 
-    def parse(self, raw, release_id):
-        """Reads the csv file, imports all distances, deletes the file when done
-           to avoid stale data and free up disk space
-        """
+    def loops(self, pdb):
+        with self.session() as session:
+            lp = mod.LoopPositions
+            query = session.query(mod.LoopInfo.loop_id.label('id'),
+                                  mod.LoopInfo.type,
+                                  mod.LoopInfo.seq,
+                                  mod.UnitInfo.unit_id,
+                                  mod.UnitInfo.chain).\
+                join(lp, lp.loop_id == mod.LoopInfo.loop_id).\
+                join(mod.UnitInfo, mod.UnitInfo == lp.unit_id).\
+                filter(mod.LoopInfo.pdb_id == pdb).\
+                filter(mod.LoopInfo.type.in_(self.loop_types)).\
+                order_by(mod.LoopInfo.loop_id)
 
-        self.logger.info('Importing qa')
-        reader = csv.reader(raw, delimiter=',', quotechar='"')
-        data = []
-        for i, row in enumerate(reader):
-            modres = row[2]
-            if modres == '':
-                modres = None
+            loops = []
+            results = it.imap(ut.row2dict, query)
+            for loop_id, group in it.groupby(results, lambda r: r['id']):
+                positions = list(group)
+                parts = []
+                loops.append({
+                    'id': loop_id,
+                    'sequence': positions[0]['sequence'].replace('*', ','),
+                    'parts': parts,
+                    'type': positions[0]['type'],
+                    'chains': set(p['chain'] for p in positions),
+                    'unit_ids': [p['unit_id'] for p in positions]
+                })
+            return loops
 
-            compl = row[4]
-            if compl == '':
-                compl = None
+    def is_complementary(self, loop):
+        if loop['type'] != 'IL':
+            return False
 
-            data.append({
-                'loop_id': row[0],
-                'status': row[1],
-                'modifications': modres,
-                'nt_signature': row[3],
-                'complementary': compl,
-                'release_id': release_id
-            })
+        seq = loop['sequence'].replace(',', '')
+        for index, char in enumerate(seq):
+            rev = -1 * (index + 1)
+            # print(index, char, self.complementary[char], rev, seq[rev])
+            if seq[rev] not in self.complementary[char]:
+                return False
+        return loop['sequence']
 
-        return data
+    def has_incomplete_units(self, loop):
+        with self.session() as session:
+            query = session.query(mod.UnitInfo).\
+                filter(mod.UnitInfo.unit_id.in_(loop['unit_ids'])).\
+                filter(mod.UnitInfo.is_complete == False).\
+                limit(1)
+            return bool(query.count())
+
+    def too_many_chains(self, loop):
+        if loop['type'] == 'HL':
+            return len(loop['chains']) == 1
+        return len(loop['chains']) == 2
+
+    def has_missing(self, loop):
+        pass
+
+    def modifications(self, loop):
+        with self.session() as session:
+            pos = mod.ExpSeqPosition
+            umap = mod.ExpSeqUnitMapping
+            query = session.query(pos.unit).\
+                join(umap,
+                     umap.exp_seq_position_id == pos.exp_seq_position_id).\
+                filter(~pos.unit.in_(['A', 'C', 'G', 'U']))
+
+            return [r.unit for r in query]
+
+    def quality(self, release_id, loop):
+        status = 1
+        nts = ', '.join(self.get_nts(loop))
+        mods = None
+        compl = None
+
+        if self.has_missing(loop):
+            status = 2
+
+        mods = self.modifications(loop)
+        if mods:
+            mods = ', '.join(mods)
+            status = 3
+
+        if self.too_many_chains(loop):
+            status = 4
+
+        if self.has_incomplete_units(loop):
+            status = 5
+
+        if self.is_complementary(loop):
+            status = 6
+
+        return {
+            'loop_id': loop['id'],
+            'status': status,
+            'modifications': mods,
+            'nt_signature': nts,
+            'complementary': compl,
+            'loop_release_id': release_id
+        }
 
     def data(self, pdb, **kwargs):
-
-        helper = Release(self.config, self.session.maker)
-        release_id = helper.current('loop')
-
-        mlab = core.Matlab(self.config['locations']['fr3d_root'])
-        [ifn, err_msg] = mlab.aLoopQualityAssurance(pdb, nout=2)
-
-        if err_msg != '':
-            raise core.MatlabFailed('Error %s in pdb %s' % (err_msg, pdb))
-
-        with open(ifn, 'rb') as raw:
-            data = [mod.LoopQa(**loop) for loop in self.parse(raw, release_id)]
-
-        os.remove(ifn)
-        return data
+        raise core.Skip("NYI")
+        release_id = self.current_id()
+        return [self.quality(release_id, loop) for loop in self.loops(pdb)]
--- test/loops/quality_test.py
+++ test/loops/quality_test.py
@@ -1,69 +1,122 @@
-import os
-
 from test import StageTest
-from test import skip_without_matlab
 from nose import SkipTest
 
-from pymotifs.loops.quality import LoopQualityLoader
-from pymotifs.models import LoopQa
-
-
-class ParsingTest(StageTest):
-    loader_class = LoopQualityLoader
-
-    def setUp(self):
-        super(ParsingTest, self).setUp()
-        with open('test/files/loop-quality.csv', 'rb') as raw:
-            self.data = self.loader.parse(raw, '1.9')
-
-    def test_can_parse_all_of_a_file(self):
-        self.assertEquals(20, len(self.data))
-
-    def test_creates_correct_data(self):
-        ans = {
-            'loop_id': "HL_1GID_001",
-            'status': "1",
-            'modifications': None,
-            'nt_signature': "149, 150, 151, 152, 153, 154",
-            'complementary': None,
-            'release_id': '1.9'
-        }
-        self.assertEquals(ans, self.data[0])
+from pymotifs.loops.quality import Loader
 
 
 class QueryingTest(StageTest):
-    loader_class = LoopQualityLoader
+    loader_class = Loader
 
     def test_can_detect_if_has_data(self):
-        raise SkipTest("Waiting for data import")
         self.assertTrue(self.loader.has_data('1GID'))
 
     def test_can_detect_if_has_no_data(self):
         self.assertFalse(self.loader.has_data('0GID'))
 
+    def test_can_get_current_release(self):
+        self.assertEquals('0.1', self.loader.current_id())
 
-class MatlabTests(StageTest):
-    loader_class = LoopQualityLoader
 
-    @skip_without_matlab
+class LoopsTest(StageTest):
+    loader_class = Loader
+
     def setUp(self):
-        super(MatlabTests, self).setUp()
-        self.data = self.loader.data('1GID')
+        super(LoopsTest, self).setUp()
+        self.loops = self.loader.loops('4V9Q')
 
-    @skip_without_matlab
-    def test_gets_all_loop_quality(self):
-        self.assertEquals(22, len(self.data))
+    def test_can_load_all_loops(self):
+        raise SkipTest()
 
-    @skip_without_matlab
-    def test_creates_correct_data(self):
-        ans = LoopQa(loop_id="HL_1GID_001",
-                     status="1",
-                     modifications=None,
-                     nt_signature="149, 150, 151, 152, 153, 154",
-                     complementary=None,
-                     release_id='2.0')
-        self.assertEquals(ans, self.data[0])
+    def test_builds_the_correct_loop(self):
+        raise SkipTest()
 
-    @skip_without_matlab
-    def test_removes_data_file(self):
-        self.assertFalse(os.path.exists('./FR3D/LoopQA.csv'))
+
+class ComplementaryTest(StageTest):
+    loader_class = Loader
+
+    def test_knows_HL_not_complementary(self):
+        loop = {'type': 'HL'}
+        self.assertFalse(self.loader.is_complementary(loop))
+
+    def test_knows_J3_not_complementary(self):
+        loop = {'type': 'J3'}
+        self.assertFalse(self.loader.is_complementary(loop))
+
+    def test_knows_non_complemntary_IL(self):
+        loop = {'type': 'IL', 'sequence': 'ACCAG,CGUGU'}
+        self.assertFalse(self.loader.is_complementary(loop))
+
+    def test_knows_an_even_length_is_complementary(self):
+        loop = {'type': 'IL', 'sequence': 'ACGGUU,AACCGU'}
+        self.assertTrue(self.loader.is_complementary(loop))
+
+    def test_knows_an_odd_length_is_complementary(self):
+        loop = {'type': 'IL', 'sequence': 'ACGUU,AACGU'}
+        self.assertTrue(self.loader.is_complementary(loop))
+
+    def test_knows_1_letter_not_complementary(self):
+        loop = {'type': 'IL', 'sequence': 'A'}
+        self.assertFalse(self.loader.is_complementary(loop))
+
+    def test_knows_2_letter_not_complementary(self):
+        loop = {'type': 'IL', 'sequence': 'AC'}
+        self.assertFalse(self.loader.is_complementary(loop))
+
+    def test_knows_2_letter_is_complementary(self):
+        loop = {'type': 'IL', 'sequence': 'AU'}
+        self.assertTrue(self.loader.is_complementary(loop))
+
+    def test_knows_observed_are_complementary(self):
+        known = ['CGU,AUG', 'CUA,UAG', 'GAGC,GCUC', 'GGC,GCC', 'AGC,GCU',
+                 'GUG,CGC', 'UCA,UGG', 'CAG,CUG', 'AGA,UCU', 'UCC,GGA',
+                 'GAC,GUC', 'GUG,UGC', 'GAU,AUC', 'GGA,UCC', 'GUG,CAC',
+                 'CUG,CAG', 'CUGUC,GACAG', 'GGU,ACC', 'GUC,GAC', 'CCG,CGG',
+                 'GAA,UUC', 'ACC,GGU', 'CUC,GGG', 'GUC,GGC', 'CGCG,CGUG',
+                 'GGG,CUC', 'CGC,GUG', 'UGG,CUG', 'GCC,GGC', 'AUG,CGU',
+                 'CUGG,CUGG', 'CGGU,AUCG', 'CGG,CUG', 'CUCG,CGAG', 'CUG,CGG',
+                 'GUAC,GUGC', 'GUA,UGC', 'CAC,GUG', 'GGG,CCC', 'UGC,GCA',
+                 'CCC,GGG', 'UAG,CUA', 'CUC,GAG', 'GCU,GGC', 'GGU,AUC',
+                 'UUA,UAA', 'CGC,GCG', 'CGG,CCG', 'GCG,CGU', 'CGGA,UCCG',
+                 'GAG,CUC', 'GGC,GCU', 'UGG,CCA', 'GGG,CCU', 'CAU,GUG',
+                 'CCA,UGG', 'CGGG,CCUG', 'GCG,CGC', 'UCU,AGG', 'UGU,GCG',
+                 'UAC,GUG', 'GGG,UCC', 'UGU,AUG', 'UCG,CGA', 'UCGC,GUGA',
+                 'CGA,UUG', 'AUC,GGU', 'UUC,GGA', 'GACG,CGUC', 'GUG,CAU',
+                 'GUAG,UUGC', 'UAG,UUG', 'AGA,UUU', 'GUAGA,UUUGC', 'UUG,CAA',
+                 'GUG,CGU', 'GGU,GUC', 'GGC,GUC', 'GGG,CUU', 'CUG,UGG',
+                 'AUCG,CGGU', 'GGA,UUU', 'GGCCGCG,CGUGGUC', 'CUCC,GGAG',
+                 'CCCG,CGGG', 'CGCC,GGCG', 'CGGGC,GCCCG', 'GGCCGC,GUGGUC',
+                 'GGGU,GCCU', 'CCCA,UGGG', 'CAA,UUG', 'UUCA,UGGA',
+                 'AGU,GCU', 'UGG,UUA', 'AGC,GUU', 'AUG,UGU', 'GUAU,GUAC',
+                 'CGGG,CUUG', 'GUUU,AAAC', 'UUG,UGA']
+        fn = self.loader.is_complementary
+        for seq in known:
+            self.assertEquals(seq, fn({'type': 'IL', 'sequence': seq}))
+
+
+class ModificationsTest(StageTest):
+    loader_class = Loader
+
+    def setUp(self):
+        super(ModificationsTest, self).setUp()
+        self.loops = {}
+        for loop in self.loader.loops('1C2W'):
+            self.loops[loop['id']] = loop
+
+    def test_can_detect_a_modified_loop(self):
+        raise SkipTest()
+        val = self.loader.modifications(self.loops['HL_1C2W_038'])
+        self.assertEquals(['PHI'], val)
+
+    def test_can_detect_multiple_modifications(self):
+        raise SkipTest()
+        val = self.loader.modifications(self.loops['HL_1C2W_045'])
+        self.assertEquals(['PHI'], val)
+
+    def test_knows_unmodified_loop(self):
+        raise SkipTest()
+        val = self.loader.modifications(self.loops['HL_1C2W_044'])
+        self.assertEquals([], val)
+
+
+class IncompleteNucleotidesTest(StageTest):
+    pass
