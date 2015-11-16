41ff2627a7958fd573673eade6592f77f4934af4
Blake Sweeney <blakes.85@gmail.com>
2015-11-16 15:55:00 -0500

    Add logic for querying by date
    We can now get pdbs given a date range. This is needed for the pipeline since we
    will be rerunning the nr set many times. Each time will be for structures before
    a date. This will help me run these structures easily and concisely. Also this
    comes with tests about things so I know that the structures are gotten are
    actually correct.

--- pymotifs/utils/pdb.py
+++ pymotifs/utils/pdb.py
@@ -2,6 +2,7 @@ import re
 import csv
 import logging
 import datetime
+import xml.etree.cElementTree as ET
 
 from pymotifs import utils
 
@@ -20,34 +21,100 @@ class RnaPdbsHelper(object):
     url = 'http://www.rcsb.org/pdb/rest/search'
 
     def __init__(self):
-        self.helper = utils.WebRequestHelper(method='post', parser=self.parse)
+        parser = lambda r: filter(lambda x: len(x) == 4, r.text.split("\n"))
+        self.helper = utils.WebRequestHelper(method='post', parser=parser,
+                                             allow_empty=True)
 
-    def parse(self, response):
-        return filter(lambda x: len(x) == 4, response.text.split("\n"))
+    def __date_refinement__(self, start, stop):
+        refinement = {
+            'queryType': 'org.pdb.query.simple.ReleaseDateQuery',
+            'database_PDB_rev.date.comparator': 'between',
+        }
 
-    def __call__(self):
+        if start:
+            refinement['database_PDB_rev.date.min'] = start.isoformat()
+
+        if stop:
+            refinement['database_PDB_rev.date.max'] = stop.isoformat()
+
+        return refinement
+
+    def __type_refinement__(self, rna='Y', dna='?', protein='?', hybrid='?'):
+        return {
+            'queryType': 'org.pdb.query.simple.ChainTypeQuery',
+            'containsProtein': protein,
+            'containsDna': dna,
+            'containsRna': rna,
+            'containsHybrid': hybrid
+        }
+
+    def xml(self, **kwargs):
+        """Create the XML text to send to PDB for some search. This defaults to
+        all RNA containing PDB files for any date. It does not limit if any
+        other molecules are present. Given more than 1 of the other options it
+        will and them all together.
+
+        :contains: A dictonary containing any of rna, dna, protein and hybrid,
+        where a value of True means the must contain that type of molecule,
+        False meaning it should not and None (or not present) meaning ignore.
+        Also if strings are given as values those will be used directly.
+        :dates: A tuple of the start and end dates to limit the search to. If
+        no start or end date is desired None, or some other falsey value should
+        be used.
+        :returns: A string of the xml data to send to PDB.
+        """
+
+        contains = {}
+        for name, value in kwargs.get('contains', {}):
+            if value is True:
+                contains[name] = 'Y'
+            elif value is False:
+                contains[name] = 'N'
+            elif value is None:
+                contains[name] = '?'
+            else:
+                contains[name] = value
+
+        refinements = [self.__type_refinement__(**contains)]
+        if 'dates' in kwargs:
+            start, stop = kwargs.get('dates')
+            refinements.append(self.__date_refinement__(start, stop))
+
+        root = ET.Element("orgPdbCompositeQuery", version='1.0')
+        for index, refinement in enumerate(refinements):
+            refine = ET.SubElement(root, "queryRefinement")
+            level = ET.SubElement(refine, "queryRefinementLevel")
+            level.text = str(index)
+            if index != 0:
+                ET.SubElement(refine, 'conjunctionType').text = 'and'
+
+            refine_query = ET.SubElement(refine, "orgPdbQuery")
+            for name, value in refinement.items():
+                ET.SubElement(refine_query, name).text = value
+
+        return ET.tostring(root)
+
+    def __call__(self, **kwargs):
         """Get a list of all rna-containing pdb files, including hybrids. Raise
-           a specific error if it fails.
+        a specific error if it fails.
+
+        :kwargs: Keyword arguments to define additional properties, such as
+        date, to search by. See `.xml` for details.
+        :returns: The response body if this succeeds.
         """
 
         logger.debug('Getting a list of all rna-containing pdbs')
-        query_text = """
-        <orgPdbQuery>
-        <queryType>org.pdb.query.simple.ChainTypeQuery</queryType>
-        <containsProtein>I</containsProtein>
-        <containsDna>I</containsDna>
-        <containsRna>Y</containsRna>
-        <containsHybrid>I</containsHybrid>
-        </orgPdbQuery>
-        """
+        query_text = self.xml(**kwargs)
+
         headers = {
             'content-type': 'application/x-www-form-urlencoded'
         }
 
         try:
             return self.helper(self.url, headers=headers, data=query_text)
-        except:
+        except Exception as err:
             logger.error("Could not get all rna containing pdbs")
+            logger.exception(err)
             raise GetAllRnaPdbsError("Failed to get list of RNA PDBs")
 
 
---
+++ test/utils/pdb_test.py
@@ -0,0 +1,34 @@
+from datetime import date
+from unittest import TestCase
+
+from pymotifs.utils.pdb import RnaPdbsHelper
+
+
+class RnaPdbsHelperTest(TestCase):
+
+    def setUp(self):
+        self.helper = RnaPdbsHelper()
+
+    def test_can_find_all_pdbs(self):
+        val = self.helper()
+        self.assertTrue(len(val) >= 3000)
+
+    def test_can_find_all_before_date(self):
+        val = self.helper(dates=(None, date(2014, 12, 11)))
+        self.assertEquals(2708, len(val))
+
+    def test_can_find_all_after_date(self):
+        val = self.helper(dates=(date(2014, 12, 11), None))
+        self.assertTrue(len(val) > 300)
+
+    def test_can_find_all_between_dates(self):
+        val = self.helper(dates=(date(2014, 12, 11), date(2014, 12, 20)))
+        self.assertEquals(['4WAL', '4WAN'], val)
+
+    def test_can_find_between_dates_with_nothing(self):
+        val = self.helper(dates=(date(2014, 12, 11), date(2014, 12, 16)))
+        self.assertEquals([], val)
+
+    def test_fails_given_invalid_dates(self):
+        self.assertRaises(Exception, self.helper,
+                          dates=('bob', date(2014, 12, 15)))
