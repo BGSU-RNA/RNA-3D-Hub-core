491753d05425b36f09f6805362aa05248c30d73f
Blake Sweeney <blakes.85@gmail.com>
2015-11-20 12:42:02 -0500

    Simplify best chains loader

--- pymotifs/chains/best.py
+++ pymotifs/chains/best.py
@@ -1,26 +1,22 @@
 from pymotifs import core
-from pymotifs.models import PdbBestChainsAndModels
+from pymotifs import models as mod
 from pymotifs.mat_files import Loader as MatLoader
 
 
 class BestChainsAndModelsLoader(core.SimpleLoader):
     dependencies = set([MatLoader])
-
-    def __init__(self, *args, **kwargs):
-        super(BestChainsAndModelsLoader, self).__init__(*args, **kwargs)
-        self.matlab = core.Matlab(self.config['locations']['fr3d_root'])
+    table = mod.PdbBestChainsAndModels
 
     def query(self, session, pdb, **kwargs):
-        return session.query(PdbBestChainsAndModels). \
-            filter(PdbBestChainsAndModels.pdb_id == pdb)
+        return session.query(mod.PdbBestChainsAndModels).filter_by(pdb_id=pdb)
 
     def data(self, pdb, **kwargs):
         # 'A,B,C', '1,2', ''
-        best_chains, best_models, err_msg = \
-            self.matlab.loadBestChainsAndModels(str(pdb), nout=3)
+        mlab = core.Matlab(self.config['locations']['fr3d_root'])
+        chains, models, err = mlab.loadBestChainsAndModels(pdb, nout=3)
 
-        if err_msg != '':
-            raise core.MatlabFailed(err_msg)
+        if err != '':
+            print(chains, models, err)
+            raise core.MatlabFailed(err)
 
-        return PdbBestChainsAndModels(pdb_id=pdb, best_chains=best_chains,
-                                      best_models=best_models)
+        return {'pdb_id': pdb, 'best_chains': chains, 'best_models': models}
