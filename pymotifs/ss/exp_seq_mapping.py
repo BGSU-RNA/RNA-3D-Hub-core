4d0f32751109c75164b4a635cd81fb26f1c43fa2
Blake Sweeney <blakes.85@gmail.com>
2015-12-05 19:15:31 -0500

    More untested work on import ss data
    This is work to allow the import of aligned ss data. This is the main point of
    import secondary structure data, aligning it to 3D structure annotations. This
    is untested but outlines what I want to do.

---
+++ pymotifs/ss/exp_seq_mapping.py
@@ -0,0 +1,95 @@
+from sqlalchmey.orm import aliased
+
+from pymotifs import core
+from pymotifs import utils as ut
+from pymotifs import models as mod
+
+from pymotifs.ss.info import Loader as SsInfoLoader
+from pymotifs.nr.chains import Loader as NrChainsLoader
+from pymotifs.exp_seq.chain_mapping import Loader as ExpSeqChainLoader
+from pymotifs.ife.info import Loader as IfeLoader
+
+
+class Loader(core.SimpleLoader):
+    dependencies = set([SsInfoLoader, NrChainsLoader, IfeLoader,
+                        ExpSeqChainLoader])
+    table = mod.SsExpSeqMapping
+    mark = False
+
+    def to_process(self, pdbs, **kwargs):
+        seqs = set()
+        for chunk in ut.grouper(100, pdbs):
+            with self.session() as session:
+                query = session.query(mod.ExpSeqPdb).\
+                    filter(mod.ExpSeqPdb.pdb_id.in_(chunk)).\
+                    distinct()
+                seqs.update(result.exp_seq_id for result in query)
+        return sorted(seqs)
+
+    def query(self, session, exp_seq):
+        return session.query(self.table).\
+            filter_by(exp_seq_id=exp_seq)
+
+    def ss_id(self, filename):
+        with self.session() as session:
+            return session.query(mod.SsInfo).\
+                filter_by(md5=self.md5(filename)).\
+                one().\
+                ss_id
+
+    def mapped_id(self, exp_seq):
+        if not self.has_data(exp_seq):
+            return None
+
+        with self.session() as session:
+            return self.query(session, exp_seq).\
+                one().\
+                ss_id
+
+    def nr_seqs(self, exp_seq):
+        exp = aliased(mod.ExpSeqInfo)
+        exp_map = aliased(mod.ExpSeqChainMapping)
+        ife = aliased(mod.IfeChains)
+        nr = aliased(mod.NrChains)
+        mem = aliased(mod.NrChains)
+        mem_ife = aliased(mod.IfeChains)
+        mem_map = aliased(mod.ExpSeqChainMapping)
+        with self.session() as session:
+            query = session.query(mem_map.exp_seq_id).\
+                join(mem_ife, mem_ife.chain_id == mem_map.chain_id).\
+                join(mem, mem.ife_id == mem_ife.ife_id).\
+                join(nr, nr.nr_class_id == mem.nr_class_id).\
+                join(ife, ife.ife_id == nr.ife_id).\
+                join(exp_map, exp_map.chain_id == ife.chain_id).\
+                join(exp, exp.exp_seq_id == exp_map.exp_seq_id).\
+                filter(exp.exp_seq_id == exp_seq).\
+                distinct()
+            return set(result.exp_seq_id for result in query)
+
+    def matching_sequence(self, exp_seq):
+        aligned = self.nr_seqs(exp_seq)
+        mapped = set(self.mapped_id(seq_id) for seq_id in aligned)
+        mapped.discard(None)
+
+        if len(mapped) > 1:
+            self.logger.error("Sequence %s has multiple possiblities %s %s:",
+                              exp_seq, aligned, mapped)
+
+        if len(mapped) != 1:
+            return None
+        return mapped.pop()
+
+    def match(self, exp_seq, **kwargs):
+        if 'filename' in kwargs:
+            return self.ss_id(kwargs['filename'])
+        return self.matching_sequence(exp_seq)
+
+    def data(self, exp_seq, **kwargs):
+        ss_id = self.match(exp_seq, **kwargs)
+        if ss_id is None:
+            raise core.Skip("Nothing to align %s to", exp_seq)
+
+        return {
+            'ss_id': ss_id,
+            'exp_seq_id': exp_seq
+        }
---
+++ pymotifs/ss/position_mapping.py
@@ -0,0 +1,73 @@
+from pymotifs import core
+from pymotifs import utils as ut
+from pymotifs import models as mod
+
+from pymotifs.utils.alignment import align
+
+from pymotifs.ss.exp_seq_mapping import Loader as SsMappingLoader
+from pymotifs.ss.positions import Loader as SsPositionLoader
+from pymotifs.exp_seq.positions import Loader as ExpPositionLoader
+
+
+class Loader(core.SimpleLoader):
+    dependencies = set([SsMappingLoader, SsPositionLoader, ExpPositionLoader])
+    table = mod.SsExpSeqPositionMapping
+
+    def to_process(self, pdbs, **kwargs):
+        ss_map = mod.SsExpSeqMapping
+        exp_pdbs = mod.ExpSeqPdb
+        seqs = set()
+        for chunk in ut.grouper(1000, pdbs):
+            with self.session() as session:
+                query = session.query(ss_map.ss_exp_seq_mapping_id).\
+                    join(exp_pdbs, ss_map.exp_seq_id == exp_pdbs.exp_seq_id).\
+                    filter(exp_pdbs.pdb_id.in_(chunk)).\
+                    distinct()
+                seqs.update(r.ss_exp_seq_mapping_id for r in query)
+        return sorted(seqs)
+
+    def query(self, session, map_id):
+        return session.query(self.table).\
+            filter_by(ss_exp_seq_mapping_id=map_id)
+
+    def pair(self, map_id):
+        with self.session() as session:
+            result = session.query(mod.SsExpSeqMapping).\
+                filter_by(ss_exp_seq_mapping_id=map_id).\
+                one()
+            return (result.exp_seq_id, result.ss_id)
+
+    def exp_info(self, exp_id):
+        loader = ExpPositionLoader(self.config, self.session)
+        return loader.sequence(exp_id)
+
+    def ss_info(self, ss_id):
+        with self.session() as session:
+            query = session.query(mod.SsPositions).\
+                filter_by(ss_id=ss_id).\
+                order_by(mod.SsPositions.index)
+
+            ids = []
+            sequence = []
+            for result in query:
+                ids.append(result.ss_position_id)
+                sequence.append(result.unid)
+        return {'ids': ids, 'sequence': ''.join(sequence)}
+
+    def align(self, exp_info, ss_info):
+        alignment = align([exp_info, ss_info])
+        return [{'ss': ss_id, 'exp': exp_id} for exp_id, ss_id in alignment]
+
+    def data(self, map_id, **kwargs):
+        exp_id, ss_id = self.pair(map_id)
+        exp = self.exp_info(exp_id)
+        ss = self.ss_info(ss_id)
+        alignment = self.align(exp, ss)
+        data = []
+        for position in alignment:
+            data.append({
+                'ss_position_id': position['ss'],
+                'exp_seq_position_id': position['exp'],
+                'ss_exp_seq_mapping_id': map_id
+            })
+        return data
