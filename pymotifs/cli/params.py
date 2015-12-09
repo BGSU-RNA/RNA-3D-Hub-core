47507f2bb51e67d170181de12317904853413cc8
Blake Sweeney <blakes.85@gmail.com>
2015-12-08 22:23:56 -0500

    Remove unneeded code
    I don't think I will be able to automatic multigroups anymore. Plus I can get a
    similar effect by using list or having error messages that say we should list.

--- pymotifs/cli/params.py
+++ pymotifs/cli/params.py
@@ -1,11 +1,7 @@
-import os
 import re
-import inspect
 from datetime import datetime
 
 import click
-
-from pymotifs.core import Stage
 
 
 class DateParamType(click.ParamType):
@@ -25,48 +21,6 @@ class PdbParamType(click.ParamType):
         if re.match('^[1-9][0-9a-z]{3}$', value, re.IGNORECASE):
             return value
         self.fail("Invalid PDB id")
-
-
-class StageCommand(click.Command):
-    def __init__(self, name, **kwargs):
-        kwargs['callback'] = self.run_stage
-        super(name, **kwargs)
-        import_name = 'pymotifs.' + name
-        from_list = import_name.split('.')
-        module = __import__(import_name, fromlist=from_list)
-        pairs = inspect.getmembers(module, self.is_loader(import_name))
-        if not pairs:
-            raise ImportError("Could not find a class to use from: %s" % name)
-
-        self.stage = pairs[0][1]
-
-    def run_stage(self, **kwargs):
-        pass
-
-
-class StageGroup(click.MultiCommand):
-
-    def get_loaders(self, base):
-        loaders = []
-        ignore = set(['utils', 'cli', 'core', '__init__.py', 'dispatcher.py'])
-        for path in os.listdir(base):
-            if path.endswith('.py') and path not in ignore:
-                name = path[0:-3]
-                module = __import__(base + '.' + path[0:-3])
-                if bool(inspect.getmembers(module, self.__stage_checker__)):
-                    loaders.append(name)
-        return loaders
-
-    def __stage_checker__(self, obj):
-        return inspect.isclass(obj) and issubclass(obj, Stage) and obj != Stage
-
-    def list_commands(self, ctx):
-        return self.get_loaders('pymotifs')
-
-    def get_command(self, ctx, name):
-        command = StageCommand(name)
-        dec = click.argument('ids', type=PDB)
-        return dec(command)
 
 
 PDB = PdbParamType()
