40d63cb10708c43b25f2e48f08453fc006dcc668
Blake Sweeney <blakes.85@gmail.com>
2015-12-08 19:45:06 -0500

    Add list of bootstrapping pdbs
    These are a constant so we might as well keep it with the rest of the constants.

--- pymotifs/constants.py
+++ pymotifs/constants.py
@@ -2,3 +2,44 @@ IFE_SEPERATOR = '+'
 STRUCTURED_BP_COUNT = 5
 STRUCTURED_BP_PER_NT = 0.5
 IFE_EXTERNAL_INTERNAL_FRACTION = 1
+
+"""PDBS used when bootstrapping the database"""
+BOOTSTRAPPING_PDBS = (
+    "124D",
+    "157D",
+    "1DUH",
+    "1E4P",
+    "1EIY",
+    "1EKD",
+    "1ET4",
+    "1F5H",
+    "1F5U",
+    "1FCW",
+    "1FEU",
+    "1FG0",
+    "1FJG",
+    "1G59",
+    "1GID",
+    "1VY4",
+    "4V42",
+    "1J5E",
+    "1KOG",
+    "1S72",
+    "2HOJ",
+    "2HOK",
+    "2HOL",
+    "2HOM",
+    "2HOO",
+    "3CPW",
+    "4V6M",
+    "4V6R",
+    "3T4B",
+    "4V88",
+    "4V8G",
+    "4V8I",
+    "4V9O",
+    "4V9Q",
+    "4V4Q",
+    "4V7W",
+    "4V9K"
+)
