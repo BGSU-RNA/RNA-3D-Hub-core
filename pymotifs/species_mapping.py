76543a047703c0c6192b3a5633ff22575389a389
Blake Sweeney <blakes.85@gmail.com>
2015-12-11 15:03:51 -0500

    Rewrite species mapping logic
    We are having troubles with the xml modules that biopython uses on rnatest.
    Because of this I have rewritten this to not use biopython. It is also now
    pointed at a European resource since they make no claims on how often and how
    much data you can request from them. I doubt it will be an issue since we will
    generally only need a few new species ids each time we get new structures. We
    also now handle getting genus or above level data by inserting NULL to the
    columns to indicate no known species for it.

--- pymotifs/species_mapping.py
+++ pymotifs/species_mapping.py
@@ -6,10 +6,7 @@ are not. This will determine the assigned species. A few chains have
 assignments to the genus level, which this complains about.
 """
 
-try:
-    import lxml
-except ImportError:
-    pass
+import xml.etree.ElementTree as ET
 
 from pymotifs import core
 from pymotifs import models as mod
@@ -18,8 +15,29 @@ from pymotifs.chains.info import Loader as ChainLoader
 
 
 class Parser(object):
+    def get_species(self, taxon):
+        if taxon.attrib.get('rank') == 'species':
+            return int(taxon.attrib['taxId']), taxon.attrib['scientificName']
+
+        lineage = taxon.find('lineage')
+        for level in lineage.findall('taxon'):
+            if level.attrib.get('rank') == 'species':
+                return int(level.attrib['taxId']), level.attrib['scientificName']
+
+        return None, None
+
+    def parse(self, text):
+        root = ET.fromstring(text)
+        taxon = root.find('taxon')
+        species_id, species_name = self.get_species(taxon)
+        return {
+            'species_mapping_id': int(taxon.attrib['taxId']),
+            'species_id': species_id,
+            'species_name': species_name
+        }
+
     def __call__(self, response):
-        pass
+        return self.parse(response.text)
 
 
 class Loader(core.SimpleLoader):
@@ -32,14 +50,16 @@ class Loader(core.SimpleLoader):
         with self.session() as session:
             query = session.query(mod.ChainInfo.taxonomy_id).\
                 filter(mod.ChainInfo.pdb_id.in_(pdbs)).\
-                distinct()
-            return [result.taxonomy_id for result in query]
+                filter(mod.ChainInfo.taxonomy_id != None).\
+                distinct().\
+                order_by(mod.ChainInfo.taxonomy_id)
+            return [int(result.taxonomy_id) for result in query]
 
     def query(self, session, taxonomy_id):
         return session.query(mod.SpeciesMapping).\
-            filter_by(mod.SpeciesMapping.species_mapping_id == taxonomy_id)
+            filter_by(species_mapping_id=taxonomy_id)
 
     def data(self, taxonomy_id, **kwargs):
         helper = WebRequestHelper(parser=Parser())
         result = helper(self.url % taxonomy_id)
-        return [mod.SpeciesMapping(**d) for d in result]
+        return mod.SpeciesMapping(**result)
--- test/species_mapping_test.py
+++ test/species_mapping_test.py
@@ -1,81 +1,61 @@
+from unittest import TestCase
 from nose import SkipTest
 
 from test import StageTest
 
+from pymotifs.species_mapping import Parser
 from pymotifs.species_mapping import Loader
 
 
-class ProcessResultsTest(StageTest):
-    loader_class = Loader
+class ParsingTest(TestCase):
 
-    def setUp(self):
-        super(ProcessResultsTest, self).setUp()
-        self.record = [
-            {u'Lineage': 'other sequences; artificial sequences',
-             u'Division': 'Synthetic',
-             u'ParentTaxId': '81077',
-             u'PubDate': '1993/04/27 01:00:00',
-             u'LineageEx': [
-                 {u'ScientificName': 'other sequences',
-                  u'TaxId': '28384', u'Rank': 'no rank'},
-                 {u'ScientificName': 'artificial sequences',
-                  u'TaxId': '81077', u'Rank': 'no rank'}
-             ],
-             u'CreateDate': '1995/02/27 09:24:00',
-             u'TaxId': '32630',
-             u'Rank': 'species',
-             u'ScientificName': 'synthetic construct'
-             },
-            {u'Lineage': 'Viruses; ssRNA viruses; ssRNA positive-strand viruses, no DNA stage; Flaviviridae; Hepacivirus; Hepatitis C virus; Hepatitis C virus genotype 4; Hepatitis C virus subtype 4a',
-             u'Division': 'Viruses',
-             u'ParentTaxId': '31653',
-             u'PubDate': '2005/11/12 18:00:14',
-             u'LineageEx': [
-                 {u'ScientificName': 'Viruses', u'TaxId': '10239', u'Rank': 'superkingdom'},
-                 {u'ScientificName': 'ssRNA viruses', u'TaxId': '439488', u'Rank': 'no rank'},
-                 {u'ScientificName': 'ssRNA positive-strand viruses, no DNA stage', u'TaxId': '35278', u'Rank': 'no rank'},
-                 {u'ScientificName': 'Flaviviridae', u'TaxId': '11050', u'Rank': 'family'},
-                 {u'ScientificName': 'Hepacivirus', u'TaxId': '11102', u'Rank': 'genus'},
-                 {u'ScientificName': 'Hepatitis C virus', u'TaxId': '11103', u'Rank': 'species'},
-                 {u'ScientificName': 'Hepatitis C virus genotype 4', u'TaxId': '33745', u'Rank': 'no rank'},
-                 {u'ScientificName': 'Hepatitis C virus subtype 4a', u'TaxId': '31653', u'Rank': 'no rank'}],
-             u'TaxId': '356418',
-             u'Rank': 'no rank',
-             u'ScientificName': 'Hepatitis C virus ED43'
-             }
-        ]
+    def parse(self, filename):
+        parser = Parser()
+        with open(filename, 'rb') as raw:
+            return parser.parse(raw.read())
 
-    def test_can_process_a_record(self):
-        val = self.loader.get_species(self.record[0])
+    def test_can_get_correct_data_given_species(self):
+        val = self.parse('test/files/species-mapping/species.xml')
         ans = {
-            'species_mapping_id': 32630,
-            'species_id': 32630,
-            'species_name': 'synthetic construct'
+            'species_mapping_id': 562,
+            'species_id': 562,
+            'species_name': 'Escherichia coli'
         }
         self.assertEquals(ans, val)
 
-    def test_can_determine_species(self):
-        val = self.loader.get_species(self.record[1])
+    def test_can_get_correct_data_given_subspecies(self):
+        val = self.parse('test/files/species-mapping/subspecies.xml')
         ans = {
-            'species_mapping_id': 356418,
-            'species_id': 11103,
-            'species_name': 'Hepatitis C virus'
+            'species_mapping_id': 656435,
+            'species_id': 562,
+            'species_name': 'Escherichia coli'
         }
-        self.assertEquals(ans, val)
+
+    def test_can_get_correct_data_given_genus(self):
+        val = self.parse('test/files/species-mapping/genus.xml')
+        ans = {
+            'species_mapping_id': 4930,
+            'species_id': None,
+            'species_name': None
+        }
 
 
 class SpeciesMappingTest(StageTest):
     loader_class = Loader
 
-    def test_downloads_a_context(self):
-        context = self.loader.context([32630, 356418])
-        val = self.loader.taxon_entries(context)
-        val.sort(key=lambda v: v['species_mapping_id'])
-        ans = [
-            {'species_mapping_id': 32630, 'species_id': 32630, 'species_name': 'synthetic construct'},
-            {'species_mapping_id': 356418, 'species_id': 11103, 'species_name': 'Hepatitis C virus'}
-        ]
+    def test_can_transform_to_taxon_ids(self):
+        val = self.loader.to_process(['1FJ0', '1S72', '4V4Q'])
+        ans = [2238, 562]
         self.assertEquals(ans, val)
 
-    def test_can_find_all_missing_ids(self):
-        raise SkipTest()
+    def test_knows_if_has_data(self):
+        self.assertTrue(self.loader.has_data(562))
+
+    def test_knows_if_has_no_data(self):
+        self.assertFalse(self.loader.has_data(-1))
+
+    def test_can_fetch_for_one_species(self):
+        val = self.loader.data(1590)
+        self.assertEquals(1590, val.species_id)
+        self.assertEquals('Lactobacillus plantarum', val.species_name)
+        self.assertEquals(1590, val.species_mapping_id)
