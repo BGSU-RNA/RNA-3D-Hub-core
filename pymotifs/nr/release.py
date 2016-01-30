1c706bc7dbf90cf267c175782b6ebc6f83e5c22c
Blake Sweeney <blakes.85@gmail.com>
2016-01-29 21:10:23 -0500

    List chain chain as a dependency as nr

--- pymotifs/nr/release.py
+++ pymotifs/nr/release.py
@@ -13,11 +13,12 @@ from pymotifs.chains.species import Loader as ChainSpeciesLoader
 from pymotifs.interactions.loader import Loader as InteractionLoader
 from pymotifs.ife.loader import Loader as IfeLoader
 from pymotifs.correspondence.loader import Loader as CorrespondenceLoader
+from pymotifs.chain_chain.loader import Loader as ChainChainLoader
 
 
 class Loader(core.MassLoader):
     dependencies = set([ChainLoader, ChainSpeciesLoader, InteractionLoader,
-                        IfeLoader, CorrespondenceLoader])
+                        IfeLoader, CorrespondenceLoader, ChainChainLoader])
     update_gap = dt.timedelta(7)
 
     def has_data(self, *args, **kwargs):
