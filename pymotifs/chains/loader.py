8fe44421a43bf35f55810aaefff3d0d36dcb637a
Blake Sweeney <blakes.85@gmail.com>
2016-03-23 16:05:48 -0400

    Remove best stage
    This isn't needed with the current setup I think. I'm keeping the code
    around until we know for certain that it is not needed.

--- pymotifs/chains/loader.py
+++ pymotifs/chains/loader.py
@@ -4,9 +4,9 @@
 from pymotifs import core
 
 from pymotifs.chains.info import Loader as InfoLoader
-from pymotifs.chains.best import BestChainsAndModelsLoader
+# from pymotifs.chains.best import BestChainsAndModelsLoader
 from pymotifs.chains.species import Loader as SpeciesLoader
 
 
 class Loader(core.StageContainer):
-    stages = set([InfoLoader, BestChainsAndModelsLoader, SpeciesLoader])
+    stages = set([InfoLoader, SpeciesLoader])
