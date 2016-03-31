12a39594cf505d238c819824fb8e061d3e7f5313
Blake Sweeney <blakes.85@gmail.com>
2016-03-30 21:40:02 -0400

    Update center loading
    It seems this will be needed for NR computations so I'm updating it.
    This needs to be tested out before we can use it.

--- pymotifs/units/centers.py
+++ pymotifs/units/centers.py
@@ -1,23 +1,32 @@
+"""Load all unit centers into the database.
+"""
+
 import pymotifs.core as core
 
 from pymotifs.models import UnitInfo
 from pymotifs.models import UnitCenters
 
+from pymotifs.units.info import Loader as InfoLoader
+
 
 class Loader(core.SimpleLoader):
+    """A class to load all base centers into the database.
+    """
+
+    dependencies = set([InfoLoader])
+    allow_no_data = False
 
     def query(self, session, pdb):
         return session.query(UnitCenters).\
-            join(UnitInfo, UnitInfo.id == UnitCenters.id).\
-            filter(UnitInfo.pdb == pdb)
+            join(UnitInfo, UnitInfo.unit_id == UnitCenters.unit_id).\
+            filter(UnitInfo.pdb_id == pdb)
 
     def data(self, pdb, **kwargs):
         structure = self.structure(pdb)
         for residue in structure.residues():
-            if residue.type == 'rna':
-                center = residue.centers['base']
-                yield UnitCenters(id=residue.unit_id(),
-                                  name='base',
+            for name, center in residue.centers.items():
+                yield UnitCenters(unit_id=residue.unit_id(),
+                                  name=name,
                                   x=center[0],
                                   y=center[1],
                                   z=center[2])
