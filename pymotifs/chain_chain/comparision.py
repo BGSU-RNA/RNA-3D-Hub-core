Correct computation of chain_chain comparison
We can now compare all ife's as part of the pipeline. This adds tests
for the stage as well as correcting the loading so that we load
everything at once and do the sorting in the database.

pymotifs/chain_chain/comparision.py
@@ -3,7 +3,8 @@ This will look at good correspondences for a given structure and extract all
 the aligned chains and then compute the geometric discrepancy between them and
 then place them in the database.
 
-This will only compare chains which are an integral part of an IFE.
+This will only compare chains which are an integral part of an IFE. Each time
+it is run it will only add 10 new connections to the database.
 """
 
 import itertools as it
@@ -16,7 +17,6 @@ from pymotifs import core
 from pymotifs import models as mod
 import pymotifs.utils as ut
 from pymotifs.constants import NR_DISCREPANCY_CUTOFF
-from pymotifs.utils import correspondence as corr
 
 from pymotifs.correspondence.summary import Loader as CorrespondenceLoader
 from pymotifs.exp_seq.mapping import Loader as ExpSeqUnitMappingLoader
@@ -25,6 +25,29 @@ from pymotifs.units.centers import Loader as CenterLoader
 from pymotifs.units.rotation import Loader as RotationLoader
 
 from fr3d.geometry.discrepancy import matrix_discrepancy
+
+
+def label_center(table, number):
+    return [
+        getattr(table, 'unit_id').label('unit%s' % number),
+        getattr(table, 'x').label('x%s' % number),
+        getattr(table, 'y').label('y%s' % number),
+        getattr(table, 'z').label('z%s' % number),
+    ]
+
+
+def label_rotation(table, number):
+    return [
+        getattr(table, 'cell_0_0').label('cell_00_%s' % number),
+        getattr(table, 'cell_0_1').label('cell_01_%s' % number),
+        getattr(table, 'cell_0_2').label('cell_02_%s' % number),
+        getattr(table, 'cell_1_0').label('cell_10_%s' % number),
+        getattr(table, 'cell_1_1').label('cell_11_%s' % number),
+        getattr(table, 'cell_1_2').label('cell_12_%s' % number),
+        getattr(table, 'cell_2_0').label('cell_20_%s' % number),
+        getattr(table, 'cell_2_1').label('cell_21_%s' % number),
+        getattr(table, 'cell_2_2').label('cell_22_%s' % number),
+    ]
 
 
 class Loader(core.SimpleLoader):
@@ -52,6 +75,13 @@ class Loader(core.SimpleLoader):
             return [ut.row2dict(result) for result in query]
 
     def find_correspondence(self, pair):
+        """Find the correspondence id for the given pair, using chain ids.
+
+        :param tuple pair: The pair of dictonaries.
+        :returns: A tuple of the given chain ids in the pair and a dicontary of
+        containing the correspondence_id and good_alignment.
+        """
+
         with self.session() as session:
             pdbs = mod.CorrespondencePdbs
             info = mod.CorrespondenceInfo
@@ -81,9 +111,16 @@ class Loader(core.SimpleLoader):
         pairs = it.ifilter(lambda p: p[2], pairs)
         pairs = it.ifilter(lambda p: p[2]['good_alignment'], pairs)
         pairs = it.imap(as_tuple, pairs)
-        return sorted(pairs, key=lambda p: (p[0], p[1]))
+        return sorted(set(pairs), key=lambda p: (p[0], p[1]))
 
     def has_data(self, entry, **kwargs):
+        """Check if the data has been computed for the given entry. If we have
+        enough new connections we will say we have data, even if it is missing.
+
+        :param tuple entry: The tuple to process.
+        :returns: A bool indicating if we have data or not.
+        """
+
         if super(Loader, self).has_data(entry, **kwargs):
             return True
         done_enough = self.new_updates[entry[0]] > self.max_new_connections
@@ -107,88 +144,42 @@ class Loader(core.SimpleLoader):
             filter(ife2.index == 0).\
             filter(cc.correspondence_id == entry[2])
 
-    def centers(self, corr_id, info1, info2, ordering, name='base'):
+    def matrices(self, corr_id, info1, info2, name='base'):
+        """Load the matrices used to compute discrepancies.
+
+        :param int corr_id: The correspondence id.
+        :param dict info1: The result of info for the first chain to compare.
+        :param dict info2: The result of info for the second chain to compare.
+        :param str name: The type of base center to use.
+        :returns: This returns 4 lists, which are in order, centers1, centers2,
+        rotation1, rotation2.
+        """
+
         with self.session() as session:
             centers1 = aliased(mod.UnitCenters)
             centers2 = aliased(mod.UnitCenters)
             units1 = aliased(mod.UnitInfo)
             units2 = aliased(mod.UnitInfo)
-            corr = mod.CorrespondenceUnits
-            results = session.query(centers1.unit_id.label('unit1'),
-                                    centers1.x.label('x1'),
-                                    centers1.y.label('y1'),
-                                    centers1.z.label('z1'),
-                                    centers2.unit_id.label('unit2'),
-                                    centers2.x.label('x2'),
-                                    centers2.y.label('y2'),
-                                    centers2.z.label('z2')).\
-                join(corr, corr.unit_id_1 == centers1.unit_id).\
-                join(centers2, corr.unit_id_2 == centers2.unit_id).\
-                join(units1, units1.unit_id == corr.unit_id_1).\
-                join(units2, units2.unit_id == corr.unit_id_2).\
-                filter(centers1.name == centers2.name).\
-                filter(centers1.name == name).\
-                filter(corr.correspondence_id == corr_id).\
-                filter(units1.pdb_id == info1['pdb']).\
-                filter(units1.sym_op == info1['sym_op']).\
-                filter(units1.model == info1['model']).\
-                filter(units1.chain == info1['chain_name']).\
-                filter(units2.pdb_id == info2['pdb']).\
-                filter(units2.sym_op == info2['sym_op']).\
-                filter(units2.model == info2['model']).\
-                filter(units2.chain == info2['chain_name']).\
-                order_by(corr.correspondence_index).\
-                all()
-
-            results.sort(key=lambda r: ordering[r.unit1])
-
-            first = []
-            second = []
-            seen = set()
-            for result in results:
-                if result.unit1 in seen:
-                    raise core.InvalidState("Got duplicate unit %s" % unit1)
-                if result.unit2 in seen:
-                    raise core.InvalidState("Got duplicate unit %s" % unit2)
-
-                seen.add(result.unit1)
-                seen.add(result.unit2)
-
-                first.append(np.array([result.x1, result.y1, result.z1]))
-                second.append(np.array([result.x2, result.y2, result.z2]))
-            return first, second
-
-    def rotations(self, corr_id, info1, info2, ordering):
-        with self.session() as session:
             rot1 = aliased(mod.UnitRotations)
             rot2 = aliased(mod.UnitRotations)
-            units1 = aliased(mod.UnitInfo)
-            units2 = aliased(mod.UnitInfo)
-            corr = mod.CorrespondenceUnits
-            results = session.query(rot1.unit_id.label('unit1'),
-                                    rot1.cell_0_0.label('f_cell_0_0'),
-                                    rot1.cell_0_1.label('f_cell_0_1'),
-                                    rot1.cell_0_2.label('f_cell_0_2'),
-                                    rot1.cell_1_0.label('f_cell_1_0'),
-                                    rot1.cell_1_1.label('f_cell_1_1'),
-                                    rot1.cell_1_2.label('f_cell_1_2'),
-                                    rot1.cell_2_0.label('f_cell_2_0'),
-                                    rot1.cell_2_1.label('f_cell_2_1'),
-                                    rot1.cell_2_2.label('f_cell_2_2'),
-                                    rot2.unit_id.label('unit2'),
-                                    rot2.cell_0_0.label('s_cell_0_0'),
-                                    rot2.cell_0_1.label('s_cell_0_1'),
-                                    rot2.cell_0_2.label('s_cell_0_2'),
-                                    rot2.cell_1_0.label('s_cell_1_0'),
-                                    rot2.cell_1_1.label('s_cell_1_1'),
-                                    rot2.cell_1_2.label('s_cell_1_2'),
-                                    rot2.cell_2_0.label('s_cell_2_0'),
-                                    rot2.cell_2_1.label('s_cell_2_1'),
-                                    rot2.cell_2_2.label('s_cell_2_2')).\
-                join(corr, corr.unit_id_1 == rot1.unit_id).\
-                join(rot2, corr.unit_id_2 == rot2.unit_id).\
-                join(units1, rot1.unit_id == units1.unit_id).\
-                join(units2, rot2.unit_id == units2.unit_id).\
+            corr_units = mod.CorrespondenceUnits
+
+            columns = []
+            columns.extend(label_center(centers1, 1))
+            columns.extend(label_center(centers2, 2))
+            columns.extend(label_rotation(rot1, 1))
+            columns.extend(label_rotation(rot2, 2))
+
+            results = session.query(*columns).\
+                join(corr_units, corr_units.unit_id_1 == centers1.unit_id).\
+                join(centers2, corr_units.unit_id_2 == centers2.unit_id).\
+                join(units1, units1.unit_id == corr_units.unit_id_1).\
+                join(units2, units2.unit_id == corr_units.unit_id_2).\
+                join(rot1, rot1.unit_id == units1.unit_id).\
+                join(rot2, rot2.unit_id == units2.unit_id).\
+                filter(centers1.name == centers2.name).\
+                filter(centers1.name == name).\
+                filter(corr_units.correspondence_id == corr_id).\
                 filter(units1.pdb_id == info1['pdb']).\
                 filter(units1.sym_op == info1['sym_op']).\
                 filter(units1.model == info1['model']).\
@@ -197,43 +188,48 @@ class Loader(core.SimpleLoader):
                 filter(units2.sym_op == info2['sym_op']).\
                 filter(units2.model == info2['model']).\
                 filter(units2.chain == info2['chain_name']).\
-                filter(corr.correspondence_id == corr_id).\
-                order_by(corr.correspondence_index).\
-                all()
+                filter(rot1.unit_id == centers1.unit_id).\
+                filter(rot2.unit_id == centers2.unit_id).\
+                filter((units1.alt_id == None) | (units1.alt_id == 'A')).\
+                filter((units2.alt_id == None) | (units2.alt_id == 'A')).\
+                order_by(corr_units.correspondence_index)
 
-            # results.sort(key=lambda r: ordering[r.unit1])
-
+            c1 = []
+            c2 = []
             r1 = []
             r2 = []
             seen = set()
             for r in results:
                 if r.unit1 in seen:
                     raise core.InvalidState("Got duplicate unit %s" % r.unit1)
+                seen.add(r.unit1)
+
                 if r.unit2 in seen:
                     raise core.InvalidState("Got duplicate unit %s" % r.unit2)
-
-                seen.add(r.unit1)
                 seen.add(r.unit2)
 
-                r1.append(np.array([[r.f_cell_0_0, r.f_cell_0_1, r.f_cell_0_2],
-                                    [r.f_cell_1_0, r.f_cell_1_1, r.f_cell_1_2],
-                                    [r.f_cell_2_0, r.f_cell_2_1, r.f_cell_2_2]]))
-                r2.append(np.array([[r.s_cell_0_0, r.s_cell_0_1, r.s_cell_0_2],
-                                    [r.s_cell_1_0, r.s_cell_1_1, r.s_cell_1_2],
-                                    [r.s_cell_2_0, r.s_cell_2_1, r.s_cell_2_2]]))
-            return r1, r2
+                r1.append(np.array([[r.cell_00_1, r.cell_01_1, r.cell_02_1],
+                                    [r.cell_10_1, r.cell_11_1, r.cell_12_1],
+                                    [r.cell_20_1, r.cell_21_1, r.cell_22_1]]))
+                r2.append(np.array([[r.cell_00_2, r.cell_01_2, r.cell_02_2],
+                                    [r.cell_10_2, r.cell_11_2, r.cell_12_2],
+                                    [r.cell_20_2, r.cell_21_2, r.cell_22_2]]))
 
+                c1.append(np.array([r.x1, r.y1, r.z1]))
+                c2.append(np.array([r.x2, r.y2, r.z2]))
+
+            return np.array(c1), np.array(c2), np.array(r1), np.array(r2)
 
     def discrepancy(self, corr_id, centers1, centers2, rot1, rot2):
         """Compare the chains. This will filter out all residues in the chain
         that do not have a base center computed.
         """
 
-        if not centers1 or not rot1:
+        if not centers1.all() or not rot1.all():
             self.logger.error("No residues with base centers/rot for first")
             return -1, 0
 
-        if not centers2 or not rot2:
+        if not centers2.all() or not rot2.all():
             self.logger.error("No residues with base centers/rot for second")
             return -1, 0
 
@@ -246,6 +242,12 @@ class Loader(core.SimpleLoader):
         return disc, len(centers1)
 
     def info(self, ife_id):
+        """Load the needed data for an ife.
+
+        :param str ife_id: The ife id.
+        :returns: A dictionary with pdb, model, chain name, chain_id, and
+        symmetry operator to use.
+        """
         with self.session() as session:
             result = session.query(mod.IfeInfo.pdb_id.label('pdb'),
                                    mod.ChainInfo.chain_name,
@@ -271,14 +273,6 @@ class Loader(core.SimpleLoader):
 
         return info
 
-    def ordering(self, corr_id, info1, info2):
-        util = corr.Helper(self.config, self.session.maker)
-        ordering = util.ordering(corr_id, info1['chain_id'], info2['chain_id'])
-        if not ordering:
-            dat = (corr_id, info1['chain_id'], info2['chain_id'])
-            raise core.InvalidState("No ordering for %i, %s, %s", dat)
-        return ordering
-
     def data(self, entry, **kwargs):
         """Compute all chain to chain similarity data. This will get all
         corresponding chains to chain alignment for all chains in this pdb and
@@ -290,11 +284,8 @@ class Loader(core.SimpleLoader):
         info2 = self.info(entry[1])
         corr_id = entry[2]
 
-        ordering = self.ordering(corr_id, info1, info2)
-        centers1, centers2 = self.centers(corr_id, info1, info2, ordering)
-        rot1, rot2 = self.rotations(corr_id, info1, info2, ordering)
-        disc, length = self.discrepancy(corr_id, centers1, centers2,
-                                        rot1, rot2)
+        matrices = self.matrices(corr_id, info1, info2)
+        disc, length = self.discrepancy(corr_id, *matrices)
 
         compare = {
             'chain_id_1': info1['chain_id'],

test/chain_chain/comparision_test.py
@@ -1,32 +1,54 @@
-import pytest
+import numpy as np
 from numpy.testing import assert_array_almost_equal
+from numpy.testing import assert_almost_equal
 
 from test import StageTest
 
-from pymotifs import core
+from pymotifs import models as mod
 from pymotifs.chain_chain.comparision import Loader
 
 
 class QueryingTest(StageTest):
     loader_class = Loader
 
+    def corr_id(self, info1, info2):
+        with self.loader.session() as session:
+            pdbs = mod.CorrespondencePdbs
+            return session.query(pdbs.correspondence_id).\
+                filter(pdbs.pdb_id_1 == info1['pdb']).\
+                filter(pdbs.pdb_id_2 == info2['pdb']).\
+                filter(pdbs.chain_name_1 == info1['chain_name']).\
+                filter(pdbs.chain_name_2 == info2['chain_name']).\
+                one().\
+                correspondence_id
+
     def test_can_find_all_chains_to_process(self):
-        val = self.loader.to_process(['1X8W', '1GRZ'])
-        ans = [('1X8W|1|A', '1GRZ|1|A', 248L),
-               ('1X8W|1|A', '1GRZ|1|B', 248L),
-               ('1X8W|1|B', '1GRZ|1|A', 248L),
-               ('1X8W|1|B', '1GRZ|1|B', 248L),
-               ('1X8W|1|C', '1GRZ|1|A', 248L),
-               ('1X8W|1|C', '1GRZ|1|B', 248L),
-               ('1X8W|1|D', '1GRZ|1|A', 248L),
-               ('1X8W|1|D', '1GRZ|1|B', 248L)]
+        pairs = self.loader.to_process(['1X8W', '1GRZ'])
+        val = [(p[0], p[1]) for p in pairs]
+        ans = [('1GRZ|1|A', '1GRZ|1|B'),
+               ('1X8W|1|A', '1GRZ|1|A'),
+               ('1X8W|1|A', '1GRZ|1|B'),
+               ('1X8W|1|A', '1X8W|1|B'),
+               ('1X8W|1|A', '1X8W|1|C'),
+               ('1X8W|1|A', '1X8W|1|D'),
+               ('1X8W|1|B', '1GRZ|1|A'),
+               ('1X8W|1|B', '1GRZ|1|B'),
+               ('1X8W|1|B', '1X8W|1|C'),
+               ('1X8W|1|B', '1X8W|1|D'),
+               ('1X8W|1|C', '1GRZ|1|A'),
+               ('1X8W|1|C', '1GRZ|1|B'),
+               ('1X8W|1|C', '1X8W|1|D'),
+               ('1X8W|1|D', '1GRZ|1|A'),
+               ('1X8W|1|D', '1GRZ|1|B')]
         self.assertEquals(val, ans)
 
     def test_knows_if_a_pair_has_been_done(self):
-        assert self.loader.has_data(('1X8W|1|D', '1GRZ|1|B', 248L)) is True
+        corr_id = self.corr_id({'pdb': '1X8W', 'chain_name': 'D'},
+                               {'pdb': '1GRZ', 'chain_name': 'B'})
+        assert self.loader.has_data(('1X8W|1|D', '1GRZ|1|B', corr_id)) is True
 
     def test_knows_if_a_pair_is_not_done(self):
-        assert self.loader.has_data(('1X8W|1|D', '1GRZ|1|B', -1)) is False
+        assert self.loader.has_data(('4V7R|1|A1', '4V88|1|A2', -1)) is False
 
     def test_it_will_have_data_if_done_many_new(self):
         assert self.loader.has_data(('1X8W|1|D', '1GRZ|1|B', 0)) is False
@@ -57,57 +79,70 @@ class LoadingResiduesTest(StageTest):
         self.info1 = self.loader.info('1X8W|1|D')
         self.info2 = self.loader.info('1GRZ|1|B')
 
-    def test_loads_an_ordering(self):
-        val = self.loader.ordering(248, self.info1, self.info2)
-        assert len(val) == 242 * 2
-        assert val['1X8W|1|D|G|96'] == 0
-        assert val['1GRZ|1|B|G|96'] == 0
-        assert val['1X8W|1|D|G|414'] == 246
-        assert val['1GRZ|1|B|G|414'] == 246
+    def corr_id(self, info1, info2):
+        with self.loader.session() as session:
+            pdbs = mod.CorrespondencePdbs
+            return session.query(pdbs.correspondence_id).\
+                filter(pdbs.pdb_id_1 == info1['pdb']).\
+                filter(pdbs.pdb_id_2 == info2['pdb']).\
+                filter(pdbs.chain_name_1 == info1['chain_name']).\
+                filter(pdbs.chain_name_2 == info2['chain_name']).\
+                one().\
+                correspondence_id
 
-    def test_complains_if_unknown_corr_id(self):
-        with pytest.raises(core.InvalidState):
-            self.loader.ordering(-1, self.info1, self.info2)
-
-    def test_it_loads_all_centers(self):
-        ordering = self.loader.ordering(248, self.info1, self.info2)
-        center1, center2 = self.loader.centers(248, self.info1, self.info2,
-                                               ordering)
-        assert len(center1) == 242
-        assert len(center2) == 242
-        assert_array_almost_equal(center1[0], [42.3341, 96.7131, 29.8361])
-        assert_array_almost_equal(center2[0], [42.7088, 91.8778, 54.455])
-
-    def test_loads_all_rotations(self):
-        ordering = self.loader.ordering(248, self.info1, self.info2)
-        rot1, rot2 = self.loader.rotations(248, self.info1, self.info2,
-                                           ordering)
-        assert len(rot1) == 242
-        assert len(rot2) == 242
-        assert_array_almost_equal(rot1[0], [[0.712491, -0.507095, 0.484986],
-                                            [0.00272507, -0.689172, -0.724592],
-                                            [0.701676, 0.517587, -0.489647]])
-        assert_array_almost_equal(rot2[0], [[-0.0857475, -0.5474, 0.832466],
-                                            [0.140197, -0.83386, -0.533876],
-                                            [0.986404, 0.0709304, 0.148245]])
+    def test_it_loads_all_data(self):
+        corr_id = self.corr_id(self.info1, self.info2)
+        c1, c2, r1, r2 = self.loader.matrices(corr_id, self.info1, self.info2)
+        assert len(c1) == 242
+        assert len(c2) == 242
+        assert len(r1) == 242
+        assert len(r2) == 242
+        assert_array_almost_equal(c1[0], [42.3341, 96.7131, 29.8361])
+        assert_array_almost_equal(c2[0], [42.7088, 91.8778, 54.455])
+        assert_array_almost_equal(r1[0], [[0.712491, -0.507095, 0.484986],
+                                          [0.00272507, -0.689172, -0.724592],
+                                          [0.701676, 0.517587, -0.489647]])
+        assert_array_almost_equal(r2[0], [[-0.0857475, -0.5474, 0.832466],
+                                          [0.140197, -0.83386, -0.533876],
+                                          [0.986404, 0.0709304, 0.148245]])
 
     def test_will_give_empty_for_unknown(self):
-        ordering = self.loader.ordering(248, self.info1, self.info2)
-        centers = self.loader.centers(None, self.info1, self.info2, ordering)
-        rotations = self.loader.rotations(None, self.info1, self.info2,
-                                          ordering)
-        assert centers == ([], [])
-        assert rotations == ([], [])
+        c1, c2, r1, r2 = self.loader.matrices(None, self.info1, self.info2)
+        assert_array_almost_equal(c1, np.array([]))
+        assert_array_almost_equal(c2, np.array([]))
+        assert_array_almost_equal(r1, np.array([]))
+        assert_array_almost_equal(r2, np.array([]))
 
 
 class ComputingDataTest(StageTest):
     loader_class = Loader
 
+    def corr_id(self, info1, info2):
+        with self.loader.session() as session:
+            pdbs = mod.CorrespondencePdbs
+            return session.query(pdbs.correspondence_id).\
+                filter(pdbs.pdb_id_1 == info1['pdb']).\
+                filter(pdbs.pdb_id_2 == info2['pdb']).\
+                filter(pdbs.chain_name_1 == info1['chain_name']).\
+                filter(pdbs.chain_name_2 == info2['chain_name']).\
+                one().\
+                correspondence_id
+
     def test_computes_both_discrepancies(self):
-        val = self.loader.data(('1X8W|1|D', '1GRZ|1|B', 248L))
+        corr_id = self.corr_id({'pdb': '1X8W', 'chain_name': 'D'},
+                               {'pdb': '1GRZ', 'chain_name': 'B'})
+        val = self.loader.data(('1X8W|1|D', '1GRZ|1|B', corr_id))
         self.assertTrue(len(val), 2)
 
     def test_it_increments_new_counts(self):
+        corr_id = self.corr_id({'pdb': '1X8W', 'chain_name': 'D'},
+                               {'pdb': '1GRZ', 'chain_name': 'B'})
         assert self.loader.new_updates['1X8W|1|D'] == 0
-        self.loader.data(('1X8W|1|D', '1GRZ|1|B', 248L))
+        self.loader.data(('1X8W|1|D', '1GRZ|1|B', corr_id))
         assert self.loader.new_updates['1X8W|1|D'] == 1
+
+    def test_it_can_handle_nan_discrepancy(self):
+        corr_id = self.corr_id({'pdb': '1FCW', 'chain_name': 'A'},
+                               {'pdb': '1FCW', 'chain_name': 'B'})
+        val = self.loader.data(('1FCW|1|A', '1FCW|1|B', corr_id))
+        assert_almost_equal(7.266e-05, val[0].discrepancy)