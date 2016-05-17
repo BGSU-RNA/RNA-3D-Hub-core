2fcc40ae43a9a080d3784f5ac1b4fb134c9b3323
Blake Sweeney <blakes.85@gmail.com>
2016-05-17 16:10:21 -0400

    Initial pass at storing coordinates in cif format
    We use the cifatom writer in fr3d to write cif formatted text to the
    database. We cleanup the data a little bit by removing lines that aren't
    wanted on a per residue basis. We also do not write waters as we do not
    generally view them and thus reduce the space used a little bit.

--- pymotifs/units/coordinates.py
+++ pymotifs/units/coordinates.py
@@ -1,8 +1,16 @@
-import os
-import csv
+"""A loader to store the coordinates for each resideu in the database. This
+will write one row per non water residue into the unit_coordinates table in the
+database. Data is written in the CIF format. This will write only the ATOM
+level entries (atom_site in cif) but will not includ the header lines like
+'loop_' or '_atom_site.group_PDB'.
+"""
+
+from cStringIO import StringIO
 
 import pymotifs.core as core
-from pymotifs.utils import matlab
+
+from fr3d.cif.writer import CifAtom
+from fr3d.data import Structure
 
 from pymotifs.models import UnitInfo
 from pymotifs.models import UnitCoordinates
@@ -17,24 +25,30 @@ class Loader(core.SimpleLoader):
             join(UnitInfo, UnitInfo.unit_id == UnitCoordinates.unit_id).\
             filter(UnitInfo.pdb_id == pdb)
 
-    def parse(self, filename):
-        data = []
-        with open(filename, 'rb') as raw:
-            reader = csv.reader(raw, delimiter=',', quotechar='"')
-            for row in reader:
-                data.append(UnitCoordinates(id=row[0], coordinates=row[9]))
-        return data
+    def coordinates(self, pdb, residue):
+        structure = Structure([residue], pdb=pdb)
+        sio = StringIO()
+        writer = CifAtom(sio)
+        writer(structure)
+        raw = sio.getvalue()
+        # Skip the first 24 lines which consist of all the header information
+        # for cif atoms.
+        coords = []
+        for line in raw.split('\n'):
+            if not line or \
+                    line.startswith('data_') or \
+                    line.startswith('loop_') or \
+                    line[0] in set('_#'):
+                continue
+            coords.append(line)
+        return '\n'.join(coords)
 
     def data(self, pdb, **kwargs):
-        mlab = matlab.Matlab()
-        ifn, status, err_msg = mlab.loadCoordinates(pdb, nout=3)
-        status = status[0][0]
-        if status == 0:
-            data = self.parse(ifn)
-            os.remove(ifn)
-            return data
-        elif status == 2:
-            self.logger.warning('File %s has no nucleotides', pdb)
-        else:
-            raise matlab.MatlabFailed('Matlab error %i when analyzing %s' %
-                                      (status, pdb))
+        structure = self.structure(pdb)
+        for unit in structure.residues():
+            if unit.sequence == 'HOH':
+                continue
+            yield {
+                'unit_id': unit.unit_id,
+                'coordinates': self.coordinates(pdb, unit)
+            }
---
+++ test/units/coordinates_test.py
@@ -0,0 +1,33 @@
+from test import StageTest
+
+from pymotifs.units.coordinates import Loader
+
+
+class QueryingTest(StageTest):
+    loader_class = Loader
+
+    def test_it_knows_if_it_has_data(self):
+        assert self.loader.has_data('1GID') is True
+
+    def test_it_knows_if_has_no_data(self):
+        assert self.loader.has_data('0GID') is False
+
+
+class CoordianteTest(StageTest):
+    loader_class = Loader
+
+    def setUp(self):
+        super(CoordianteTest, self).setUp()
+        self.data = list(self.loader.data('157D'))
+
+    def test_it_creates_only_atom_level_entries(self):
+        for residue in self.data:
+            for line in residue.split('\n'):
+                assert line.startswith('ATOM')
+
+    def test_it_creates_all_atom_level_entries(self):
+        val = self.data[0].split('\n')
+        assert len(val) == 22
+
+    def test_it_creates_entries_for_each_residue(self):
+        assert len(self.data) == 24
