6c6a9b1b8b9eb500ccbe4dc57ada0221fa7f966b
Blake Sweeney <blakes.85@gmail.com>
2016-05-18 10:52:35 -0400

    First pass at some simple quality steps
    I've added some simple tests for the easy to test parts of quality
    assurance in loops. This doesn't cover all the logic but at least the
    easy bits seem to work. More testing will wait for more bootstrapping.

--- pymotifs/loops/quality.py
+++ pymotifs/loops/quality.py
@@ -2,27 +2,49 @@
 Program for importing loop quality assurance data into the RNA 3D Hub database.
 
 Some loops should be disqualified because they have unresolved or missing
-nucleotides. See matlab code for disqualification codes.
+nucleotides. The disqualification codes are:
+
+1 - valid loop
+2 - missing nucleotides
+3 - modified nucleotides
+4 - abnormal chain number
+5 - incomplete nucleotides
+6 - self-complementary internal loop
+7 - cyro-em structure
 """
 
-import os
-import csv
+import collections as coll
 
+from sqlalchemy import asc
 from sqlalchemy import desc
 
 from pymotifs import core
-from pymotifs.utils import matlab
 from pymotifs import models as mod
 
+from pymotifs.loops.release import Loader as ReleaseLoader
 from pymotifs.loops.extractor import Loader as InfoLoader
+from pymotifs.loops.positions import Loader as PositionLoader
+from pymotifs.exp_seq.mapping import Loader as ExpSeqMappingLoader
+
+COMPLEMENTARY = {
+    'G': 'C',
+    'C': 'G',
+    'A': 'U',
+    'U': 'A'
+}
 
 
 class Loader(core.SimpleLoader):
-    dependencies = set([InfoLoader])
+    dependencies = set([ReleaseLoader, InfoLoader, PositionLoader,
+                        ExpSeqMappingLoader])
 
     def current_id(self):
+        """Compute the current loop release id.
+
+        :returns: The current loop release id string, eg 1.2.
+        """
         with self.session() as session:
-            query = session.query(mod.LoopReleases.loop_releases_id).\
+            query = session.query(mod.LoopReleases.loop_release_id).\
                 order_by(desc(mod.LoopReleases.date)).\
                 limit(1)
 
@@ -32,58 +54,193 @@ class Loader(core.SimpleLoader):
             return query.one().loop_releases_id
 
     def query(self, session, pdb):
+        """Create a query to find all loop qa entries for the given pdb.
+
+        :param session: The session to query with.
+        :param str pdb: The PDB id to look up.
+        :returns: The query to use.
+        """
+
         release_id = self.current_id()
         return session.query(mod.LoopQa).\
             join(mod.LoopInfo, mod.LoopInfo.loop_id == mod.LoopQa.loop_id).\
             filter(mod.LoopInfo.pdb_id == pdb).\
-            filter(mod.LoopQa.release_id == release_id)
+            filter(mod.LoopQa.loop_release_id == release_id)
 
-    def is_complementary(self, loop):
-        pass
-
-    def has_modified(self, loop):
-        pass
-
-    def parse(self, raw, release_id):
-        """Reads the csv file, imports all distances, deletes the file when done
-           to avoid stale data and free up disk space
+    def loops(self, pdb):
+        """Load all the loops in the pdb file.
         """
 
-        self.logger.info('Importing qa')
-        reader = csv.reader(raw, delimiter=',', quotechar='"')
+        def empty_loop():
+            return {
+                'nts': [],
+                'positions': [],
+                'chains': set(),
+                'signature': [],
+                'endpoints': []
+            }
+
+        with self.session() as session:
+            query = session.query(mod.LoopInfo.loop_id.label('id'),
+                                  mod.LoopInfo.type,
+                                  mod.LoopInfo.pdb_id.label('pdb'),
+                                  mod.UnitInfo.unit_id.label('uid'),
+                                  mod.UnitInfo.unit.label('unit'),
+                                  mod.LoopInfo.seq.label('seq'),
+                                  mod.UnitInfo.chain_name.label('chain'),
+                                  mod.UnitInfo.number.label('number'),
+                                  mod.LoopPositions.flanking,
+                                  ).\
+                join(mod.LoopPositions,
+                     mod.LoopPositions.loop_id == mod.LoopInfo.loop_id).\
+                join(mod.UnitInfo,
+                     mod.UnitInfo.unit_id == mod.LoopPositions.unit_id).\
+                filter(mod.LoopInfo.pdb_id == pdb).\
+                order_by(asc(mod.LoopPositions.position))
+
+            loops = coll.defaultdict(empty_loop)
+            for result in query:
+                current = loops[result.id]
+                current['id'] = result.id
+                current['type'] = result.type
+                current['nts'].append(result.uid)
+                current['units'].append(result.unit)
+                current['chains'].add(result.chain)
+                current['signature'].append(result.number)
+                current['pdb'] = result.pdb
+
+                if result.flanking:
+                    current['endpoints'].append('uid')
+
+                loops[result.id] = current
+            return loops.values()
+
+    def complementary_sequence(self, loop):
+        """Detect if a sequence is complementary.
+        """
+
+        sequence = loop['units']
+
+        midpoint = None
+        if len(loop['units']) % 2 != 0:
+            midpoint = int(len(loop['units']) / 2)
+
+        pair = zip(sequence, reversed(sequence))
+        for index, (first, second) in enumerate(pair):
+            if midpoint is not None and index == midpoint:
+                continue
+            if first != COMPLEMENTARY[second]:
+                return False
+
+        return True
+
+    def has_no_non_cWW(self, loop):
+        """Check if there are non-cWW interactions within the loop.
+        """
+
+        with self.session() as session:
+            inters = mod.UnitPairInteractions
+            bps = mod.BasePairFamilyInfo
+            query = session.query(inters).\
+                join(bps, bps.bp_family_id == inters.f_lwbp).\
+                filter(inters.unit_id_1.in_(loop['nts'])).\
+                filter(inters.unit_id_2.in_(loop['nts'])).\
+                filter(bps.is_near == 0).\
+                filter(bps.bp_family_id != 'cWW')
+
+            return bool(query.count())
+
+    def is_complementary(self, loop):
+        """Check if a loop has a complementary sequence. This requires that the
+        loop have no non-cWW basepairs (though near are allowed) between them.
+        If so then we consider it as a complemenatry loop since it is likely
+        just a poorly modeled helix.
+        """
+
+        return self.has_no_non_cWW(loop) and self.complementary_sequence(loop)
+
+    def modified_bases(self, loop):
+        """Get a list of all modified bases, if any in this loop.
+        """
+        def is_modified(unit):
+            return unit not in set(['A', 'C', 'G', 'U'])
+
+        return [unit for unit in loop['units'] if is_modified(unit)]
+
+    def has_modified(self, loop):
+        """Check if there are any modified nucleotides in the loops. These
+        cannot yet be processed.
+        """
+        return bool(self.modified_bases(loop))
+
+    def has_breaks(self, loop):
+        """Check if there are any chain breaks within the loop.
+        """
+        pass
+
+    def has_incomplete_nucleotides(self, cif, loop):
+        """Check if any of the nucleotides in the loop are incomplete, that is
+        are missing atoms that they should not be.
+        """
+        pass
+
+    def bad_chain_number(self, loop):
+        """Check if there are the correct number of chains for each loop.
+        """
+
+        if not loop['chains']:
+            return True
+
+        if loop['type'] == 'HL':
+            return len(loop['chains']) != 1
+        if loop['type'] == 'IL':
+            return len(loop['chains']) > 2
+        if loop['type'] == 'J3':
+            return len(loop['chains']) > 3
+        raise core.InvalidState("Unknown type of loop")
+
+    def is_cyro(self, loop):
+        pass
+
+    def status(self, cif, loop):
+        """Compute the status code. The status code is defined above.
+
+        :param dict loop: A loop as returned from loops.
+        :returns: The status code.
+        """
+
+        if self.has_breaks(loop):
+            return 2
+        if self.has_modified(loop):
+            return 3
+        if self.bad_chain_number(loop):
+            return 4
+        if self.has_incomplete_nucleotides(cif, loop):
+            return 5
+        if self.is_complementary(loop):
+            return 6
+        if self.is_cyro(loop):
+            return 7
+        return 1
+
+    def data(self, pdb):
+        """Compute the qa status of each loop in the structure.
+
+        :param str pdb: The pdb id to use.
+        :returns: A list of the status entries for all loops in the structure.
+        """
+
         data = []
-        for i, row in enumerate(reader):
-            modres = row[2]
-            if modres == '':
-                modres = None
-
-            compl = row[4]
-            if compl == '':
-                compl = None
-
+        cif = self.cif(pdb)
+        release_id = self.current_id()
+        for loop in self.loops(pdb):
             data.append({
-                'loop_id': row[0],
-                'status': row[1],
-                'modifications': modres,
-                'nt_signature': row[3],
-                'complementary': compl,
-                'release_id': release_id
+                'loop_id': loop['id'],
+                'status': self.status(cif, loop),
+                'modifications': ', '.join(self.modified_bases(loop)),
+                'nt_signature': ', '.join(loop['signature']),
+                'complementary': self.is_complementary(loop),
+                'loop_release_id': release_id,
             })
 
-        return data
-
-    def data(self, pdb, **kwargs):
-
-        release_id = self.current_id()
-
-        mlab = matlab.Matlab(self.config['locations']['fr3d_root'])
-        [ifn, err_msg] = mlab.aLoopQualityAssurance(pdb, nout=2)
-
-        if err_msg != '':
-            raise matlab.MatlabFailed('Error %s in pdb %s' % (err_msg, pdb))
-
-        with open(ifn, 'rb') as raw:
-            data = [mod.LoopQa(**loop) for loop in self.parse(raw, release_id)]
-
-        os.remove(ifn)
         return data
--- test/loops/quality_test.py
+++ test/loops/quality_test.py
@@ -1,37 +1,14 @@
-import os
+import pytest
+
+from pymotifs import core
 
 from test import StageTest
-from test import skip_without_matlab
 
-from pymotifs.loops.quality import LoopQualityLoader
-from pymotifs.models import LoopQa
-
-
-class ParsingTest(StageTest):
-    loader_class = LoopQualityLoader
-
-    def setUp(self):
-        super(ParsingTest, self).setUp()
-        with open('test/files/loop-quality.csv', 'rb') as raw:
-            self.data = self.loader.parse(raw, '1.9')
-
-    def test_can_parse_all_of_a_file(self):
-        self.assertEquals(20, len(self.data))
-
-    def test_creates_correct_data(self):
-        ans = {
-            'loop_id': "HL_1GID_001",
-            'status': "1",
-            'modifications': None,
-            'nt_signature': "149, 150, 151, 152, 153, 154",
-            'complementary': None,
-            'release_id': '1.9'
-        }
-        self.assertEquals(ans, self.data[0])
+from pymotifs.loops.quality import Loader
 
 
 class QueryingTest(StageTest):
-    loader_class = LoopQualityLoader
+    loader_class = Loader
 
     def test_can_detect_if_has_data(self):
         self.assertTrue(self.loader.has_data('1GID'))
@@ -40,28 +17,119 @@ class QueryingTest(StageTest):
         self.assertFalse(self.loader.has_data('0GID'))
 
 
-class MatlabTests(StageTest):
-    loader_class = LoopQualityLoader
+class ModifiedBasesTest(StageTest):
+    loader_class = Loader
 
-    @skip_without_matlab
-    def setUp(self):
-        super(MatlabTests, self).setUp()
-        self.data = self.loader.data('1GID')
+    def test_knows_if_unmodified_bases(self):
+        valid = {'units': ['A', 'C', 'G', 'U']}
+        assert [] == self.loader.modified_bases(valid)
+        assert False is self.loader.has_modified(valid)
 
-    @skip_without_matlab
-    def test_gets_all_loop_quality(self):
-        self.assertEquals(22, len(self.data))
+    def test_knows_if_has_modified_bases(self):
+        invalid = {'units': ['A', 'C', 'G', 'I']}
+        assert ['I'] == self.loader.modified_bases(invalid)
+        assert True is self.loader.has_modified(invalid)
 
-    @skip_without_matlab
-    def test_creates_correct_data(self):
-        ans = LoopQa(loop_id="HL_1GID_001",
-                     status="1",
-                     modifications=None,
-                     nt_signature="149, 150, 151, 152, 153, 154",
-                     complementary=None,
-                     release_id='2.0')
-        self.assertEquals(ans, self.data[0])
 
-    @skip_without_matlab
-    def test_removes_data_file(self):
-        self.assertFalse(os.path.exists('./FR3D/LoopQA.csv'))
+class ComplementarySequenceTest(StageTest):
+    loader_class = Loader
+
+    @pytest.mark.skip()
+    def test_can_detect_if_has_non_cWW_pairs(self):
+        pass
+
+    @pytest.mark.skip()
+    def test_can_detect_if_is_complementary(self):
+        pass
+
+    def test_can_find_if_complemenatry_sequence(self):
+        valid = {'units': ['A', 'C', 'C', 'G']}
+        invalid1 = {'units': ['A', 'C', 'G', 'U']}
+        invalid2 = {'units': ['A', 'C', 'A', 'G', 'U']}
+        assert self.loader.complementary_sequence(valid) is False
+        assert self.loader.complementary_sequence(invalid1) is True
+        assert self.loader.complementary_sequence(invalid2) is True
+
+
+class ChainNumberValidationTests(StageTest):
+    loader_class = Loader
+
+    def bad_chain_number(self, *args, **kwargs):
+        return self.loader.bad_chain_number(*args, **kwargs)
+
+    def test_knows_if_good_IL(self):
+        loop1 = {'type': 'IL', 'chains': set('A')}
+        loop2 = {'type': 'IL', 'chains': set('AB')}
+        assert self.bad_chain_number(loop1) is False
+        assert self.bad_chain_number(loop2) is False
+
+    def test_knows_if_has_bad_IL(self):
+        loop1 = {'type': 'IL', 'chains': set('ABC')}
+        loop2 = {'type': 'IL', 'chains': set('')}
+        assert self.bad_chain_number(loop1) is True
+        assert self.bad_chain_number(loop2) is True
+
+    def test_knows_if_good_HL(self):
+        loop1 = {'type': 'HL', 'chains': set('A')}
+        assert self.bad_chain_number(loop1) is False
+
+    def test_knows_if_bad_HL(self):
+        loop1 = {'type': 'HL', 'chains': set('ABC')}
+        loop2 = {'type': 'HL', 'chains': set('BC')}
+        loop3 = {'type': 'HL', 'chains': set('')}
+        assert self.bad_chain_number(loop1) is True
+        assert self.bad_chain_number(loop2) is True
+        assert self.bad_chain_number(loop3) is True
+
+    def test_knows_if_good_J3(self):
+        loop1 = {'type': 'J3', 'chains': set('A')}
+        loop2 = {'type': 'J3', 'chains': set('AB')}
+        loop3 = {'type': 'J3', 'chains': set('ABC')}
+        assert self.bad_chain_number(loop1) is False
+        assert self.bad_chain_number(loop2) is False
+        assert self.bad_chain_number(loop3) is False
+
+    def test_knows_if_bad_J4(self):
+        loop1 = {'type': 'J3', 'chains': set('')}
+        loop2 = {'type': 'J3', 'chains': set('ABCD')}
+        assert self.bad_chain_number(loop1) is True
+        assert self.bad_chain_number(loop2) is True
+
+    def test_complains_invalid_loop(self):
+        loop1 = {'type': 'J4', 'chains': set('ABCD')}
+        with pytest.raises(core.InvalidState):
+            self.bad_chain_number(loop1)
+
+
+class RealDataValidationTests(StageTest):
+    loader_class = Loader
+
+    @pytest.mark.skip()
+    def test_can_detect_if_has_breaks(self):
+        loop = self.loop('HL_1FG0_002')
+        assert self.loader.has_breaks(loop) is True
+        assert self.loader.status(loop) == 2
+
+    @pytest.mark.skip()
+    def test_can_detect_if_has_modifications(self):
+        loop = self.loop('HL_1FCW_015')
+        assert self.loader.has_modified(loop) == '5MU, PSU, 1MA'
+        assert self.loader.status(loop) == 3
+
+    @pytest.mark.skip()
+    def test_can_detect_if_has_bad_chain_number(self):
+        loop = self.loop('HL_1FEU_001')
+        assert self.loader.has_bad_chain_number(loop) is True
+        assert self.loader.status(loop) == 4
+
+    @pytest.mark.skip()
+    def test_can_detect_if_has_incomplete_nts(self):
+        loop = self.loop('IL_2HOM_001')
+        assert self.loader.has_incomplete_nucleotides(loop) is True
+        assert self.loader.status(loop) == 5
+
+    @pytest.mark.skip()
+    def test_can_detect_if_is_complemenatry(self):
+        loop = self.loop('IL_1GRZ_007')
+        assert self.loader.is_complementary(loop) == 'CAG,CUG'
+        assert self.loader.status(loop) == 6
