3c913bdd7d8c0e65542c24dc78aae176750045cd
Blake Sweeney <blakes.85@gmail.com>
2016-06-14 12:27:04 -0400

    Speed up distance storing
    This uses the pairwise iterator and it's distance logic to make the
    lookups faster.

--- pymotifs/units/distances.py
+++ pymotifs/units/distances.py
@@ -8,30 +8,15 @@ from pymotifs.models import UnitPairsDistances
 from pymotifs.units.info import Loader as InfoLoader
 
 
-class Loader(core.Loader):
+class Loader(core.SimpleLoader):
     max_distance = 10.0
     dependencies = set([InfoLoader])
 
-    def remove(self, pdb, **kwargs):
+    def query(self, session, pdb):
         with self.session() as session:
-            query = session.query(UnitPairsDistances.unit_id_1.label('unit_id')).\
+            return session.query(UnitPairsDistances).\
                 join(UnitInfo, UnitInfo.unit_id == UnitPairsDistances.unit_id_1).\
                 filter(UnitInfo.pdb_id == pdb)
-            ids = [result.unit_id for result in query]
-
-        with self.session() as session:
-            query = session.query(UnitPairsDistances).\
-                filter(UnitPairsDistances.unit_id_1.in_(ids)).\
-                delete(synchronize_session=True)
-
-    def has_data(self, pdb, **kwargs):
-        with self.session() as session:
-            query = session.query(UnitPairsDistances).\
-                join(UnitInfo, UnitInfo.unit_id == UnitPairsDistances.unit_id_1).\
-                filter(UnitInfo.pdb_id == pdb).\
-                limit(1)
-
-            return bool(query.count())
 
     def center(self, residue):
         if residue.sequence == 'HOH':
@@ -54,12 +39,11 @@ class Loader(core.Loader):
         structure = self.structure(pdb)
         not_water = lambda p: p[0].sequence != 'HOH' and p[1].sequence != 'HOH'
 
-        pairs = structure.pairs()
+        pairs = structure.pairs(distance={'cutoff': self.max_distance})
         pairs = it.ifilter(not_water, pairs)
 
         for residue1, residue2 in pairs:
             distance = self.distance(residue1, residue2)
-            if distance is not None and distance < self.max_distance:
-                yield UnitPairsDistances(unit_id_1=residue1.unit_id(),
-                                         unit_id_2=residue2.unit_id(),
-                                         distance=distance)
+            yield UnitPairsDistances(unit_id_1=residue1.unit_id(),
+                                     unit_id_2=residue2.unit_id(),
+                                     distance=distance)
