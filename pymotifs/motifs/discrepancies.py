3eedf43c956c1e602e529094aced89239c2c244d
Blake Sweeney <blakes.85@gmail.com>
2016-07-15 13:31:11 -0400

    Add some tests to discrepancy loading
    For motif releases we need to load some discrepancies. This adds a few
    simple tests for this stage. It also adds some small test data.

--- pymotifs/motifs/discrepancies.py
+++ pymotifs/motifs/discrepancies.py
@@ -12,14 +12,27 @@ class Loader(core.SimpleLoader):
     dependencies = set([ReleaseLoader])
     table = mod.MlMutualDiscrepancy
 
-    def query(self, session, *args, **kwargs):
-        current, _ = ReleaseLoader(self.config, self.session).current_id()
-        return session.query(mod.MlMutualDiscrepancy).\
-            filter_by(release_id=current)
+    def to_process(self, *args, **kwargs):
+        return ReleaseLoader.types
 
-    def data(self):
+    def query(self, session, loop_type, **kwargs):
+        data = self.cached(loop_type)
+        return session.query(mod.MlMutualDiscrepancy).\
+            join(mod.LoopInfo,
+                 mod.LoopInfo.loop_id == mod.MlMutualDiscrepancy.loop_id_1).\
+            filter(mod.LoopInfo.type == loop_type).\
+            filter(mod.MlMutualDiscrepancy.ml_release_id == data['release'])
+
+    def discrepancies(self, cached):
         data = []
-        for loop_type in ReleaseLoader.types:
-            cached = self.cached(loop_type)
-            data.extend(cached['discrepancies'])
+        for disc in cached['discrepancies']:
+            entry = dict(disc)
+            entry['ml_release_id'] = cached['release']
+            data.append(entry)
         return data
+
+    def data(self, loop_type, **kwargs):
+        cached = self.cached(loop_type)
+        if not cached:
+            raise core.InvalidState("No data cached")
+        return self.discrepancies(cached)
