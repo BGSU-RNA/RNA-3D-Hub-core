ec4dc0f9e984114d20757d0bf295d69eadd6eaae
Blake Sweeney <blakes.85@gmail.com>
2016-07-19 12:57:33 -0400

    Add a lightly tested loop order stage
    First pass at a working stage to store the loop ordering for motif
    stages.

--- pymotifs/motifs/loop_order.py
+++ pymotifs/motifs/loop_order.py
@@ -12,15 +12,21 @@ class Loader(BaseLoader):
     dependencies = set([ReleaseLoader, InfoLoader])
     table = mod.MlLoopOrder
 
-    def as_order(self, entry):
-        return {
-            'motif_id': entry['motif_id'],
-            'loop_id': entry['ordering']['loop_id'],
-            'ml_release_id': entry['release_id'],
-            'orginal_order': entry['ordering']['orginal_order'],
-            'similarity_order': entry['ordering']['similarity_order'],
-        }
+    def ordering(self, cached):
+        data = []
+        for entry in cached['motifs']:
+            for loop in entry['ordering']:
+                data.append({
+                    'motif_id': entry['motif_id'],
+                    'loop_id': loop['loop_id'],
+                    'ml_release_id': cached['release'],
+                    'original_order': loop['original_order'],
+                    'similarity_order': loop['similarity_order'],
+                })
+        return data
 
-    def data(self, pair, **kwargs):
-        cached = self.cached(pair[0])
-        return [self.as_order(entry) for entry in cached]
+    def data(self, release, **kwargs):
+        data = self.cached(NR_CACHE_NAME)
+        if not data:
+            raise core.InvalidState("Missing cached data")
+        return self.ordering(cached)
---
+++ test/motifs/loop_order_test.py
@@ -0,0 +1,29 @@
+import pickle
+
+import pytest
+
+from test import StageTest
+
+from pymotifs.motifs.loop_order import Loader
+
+
+class ComputingDataTest(StageTest):
+    loader_class = Loader
+
+    def setUp(self):
+        super(ComputingDataTest, self).setUp()
+        with open('test/files/motifs/IL.pickle') as raw:
+            self.cached = pickle.load(raw)
+            self.ordering = self.loader.ordering(self.cached)
+
+    def test_it_loads_all_data(self):
+        assert len(self.ordering) == 81
+
+    def test_it_creates_valid_data(self):
+        assert self.ordering[0] == {
+            'motif_id': 'IL_85752.1',
+            'loop_id': 'IL_1ET4_003',
+            'ml_release_id': '0.1',
+            'original_order': 1,
+            'similarity_order': 1,
+        }
