dffe28455629d6f556dc2a5d601d29c9e4ad8582
Blake Sweeney <blakes.85@gmail.com>
2016-09-28 19:45:06 -0400

    Remove units.redundant
    Not sure what this is used for or needed. So skipping it now.

--- pymotifs/units/loader.py
+++ pymotifs/units/loader.py
@@ -5,7 +5,6 @@
 - units.distances
 - units.info
 - units.quality
-- units.redundant
 - units.rotation
 """
 
@@ -15,7 +14,7 @@ from pymotifs.units.info import Loader as InfoLoader
 from pymotifs.units.coordinates import Loader as CoordinateLoader
 from pymotifs.units.distances import Loader as DistancesLoader
 from pymotifs.units.quality import Loader as QualityLoader
-from pymotifs.units.redundant import RedundantNucleotidesLoader
+# from pymotifs.units.redundant import RedundantNucleotidesLoader
 from pymotifs.units.centers import Loader as CenterLoader
 from pymotifs.units.rotation import Loader as RotationLoader
 
@@ -26,5 +25,4 @@ class Loader(core.StageContainer):
 
     """The `Stages` this will run"""
     stages = set([InfoLoader, QualityLoader, DistancesLoader, CenterLoader,
-                  RotationLoader, RedundantNucleotidesLoader,
-                  CoordinateLoader])
+                  RotationLoader, CoordinateLoader])
