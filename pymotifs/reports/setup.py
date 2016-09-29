41a64793dbc6468fe0ad7f02a328dba1d9cf5f40
Blake Sweeney <blakes.85@gmail.com>
2016-09-28 22:55:34 -0400

    Cleanup unused code

--- pymotifs/reports/setup.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import sessionmaker
-
-from pymotifs.core import Session
-from pymotifs import models as mod
-
-
-def setup(engine):
-    mod.reflect(engine)
-    return Session(sessionmaker(engine))
