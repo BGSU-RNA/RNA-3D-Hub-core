7b03a45edffab3772fbbfb7ceb45ac992c156ca1
Blake Sweeney <blakes.85@gmail.com>
2016-10-07 15:29:39 -0400

    Attempt to add option to limit max sizes compared
    A lot time is spent on computing comparisons with very large loops.
    This adds an option to require that things that are too large (20) have
    nearly exact sizes. This should speed up some clusterings when we are
    testing.

--- FR3DMotifs/aAaSearches.m
+++ FR3DMotifs/aAaSearches.m
@@ -1,10 +1,14 @@
-function [] = aAaSearches(loop_ids, start, stop)
+function [] = aAaSearches(loop_ids, start, stop, exactSizeLimit)
+
+    if nargin < 4
+        exactSizeLimit = 0;
+    end
 
     if ischar(loop_ids) % assume file
         fid = fopen(loop_ids, 'r');
         line = fgetl(fid);
         loop_ids = regexp(line, ',','split');
-        fclose(fid);        
+        fclose(fid);
     elseif ~iscell(loop_ids)
         error('Incorrect input');
     end
@@ -18,30 +22,30 @@ function [] = aAaSearches(loop_ids, start, stop)
     if stop > N
         stop = N;
     end
-    
-    for i = start:stop    
-        
+
+    for i = start:stop
+
         disp(i);
         destination = [getSearchFolder filesep loop_ids{i}];
         if ~exist(destination,'dir'), mkdir(destination); end
 
         % read log file with negative results
-        log = [destination filesep 'No_candidates.txt'];                        
+        log = [destination filesep 'No_candidates.txt'];
         if exist(log,'file')
             done = textread(log,'%s');
         else
             done = {};
         end
-        
+
         % load only once
-        load(getPrecomputedDataAddress(loop_ids{i}));        
-        
+        load(getPrecomputedDataAddress(loop_ids{i}));
+
         for j = 1:N
-            if isempty(intersect(done, loop_ids{j}))            
-                pairwiseSearch(File, loop_ids{j});
+            if isempty(intersect(done, loop_ids{j}))
+                pairwiseSearch(File, loop_ids{j}, exactSizeLimit);
             end
         end
-        
+
     end
-    
+
 end
--- FR3DMotifs/pairwiseSearch.m
+++ FR3DMotifs/pairwiseSearch.m
@@ -1,25 +1,29 @@
-function [disc] = pairwiseSearch(file1, file2)
+function [disc] = pairwiseSearch(file1, file2, exactSizeLimit)
+
+    if nargin < 3
+        exactSizeLimit = 0
+    end
 
     % defer loading loop Precomputed data if not necessary
-    if ~ischar(file1)        
+    if ~ischar(file1)
         [File1, file1] = getNameAndData(file1);
     end
     if ~ischar(file2)
-        [File2, file2] = getNameAndData(file2);        
+        [File2, file2] = getNameAndData(file2);
     end
-    
+
     result = getSearchAddress(file1, file2);
 
     % Parameters structure
     P.Discrepancy   = 1;
-    P.Subdir        = fullfile(getSearchFolder, file1, '');    
+    P.Subdir        = fullfile(getSearchFolder, file1, '');
     P.no_candidates = [P.Subdir filesep 'No_candidates.txt'];
     P.maxNtToSearch = 25;
     if ~exist(P.Subdir,'dir'), mkdir(P.Subdir); end
-    
+
     % look up existing results
     if exist(result,'file')
-        try 
+        try
             load(result);
             disc = Search.Discrepancy(1);
             return;
@@ -35,18 +39,18 @@ function [disc] = pairwiseSearch(file1, file2)
             if ~isempty(find(ismember(no_candidates{1}, file2), 1))
                 disc = Inf;
                 return;
-            end            
+            end
         end
     end
-    
+
     % if file1 and file2 are never compared before loop ids
     if ~exist('File1', 'var')
-        [File1, file1] = getNameAndData(file1);        
+        [File1, file1] = getNameAndData(file1);
     end
     if ~exist('File2', 'var')
-        [File2, file2] = getNameAndData(file2);        
+        [File2, file2] = getNameAndData(file2);
     end
-    
+
     % don't analyze huge spurious loops
     if File1.NumNT > P.maxNtToSearch || File2.NumNT > P.maxNtToSearch
         fprintf('Large loop: %s vs %s, %i vs %i\n', file1, file2, File1.NumNT, File2.NumNT);
@@ -58,30 +62,30 @@ function [disc] = pairwiseSearch(file1, file2)
             return;
         end
     end
-    
+
     % don't search large in small
     if ~isSizeCompatible(File1, File2)
         fprintf('Query %s is larger than target %s\n', file1, file2);
         addToNoCandidatesFile(file2, P);
         disc = Inf;
         return;
-    end    
-    
+    end
+
     % try searching
     Search = struct;
-    
-    if aSearchFlankingBases(File1,File2,P) == 1   
-    
+
+    if aSearchFlankingBases(File1,File2,P) == 1
+
         [Query,S] = aConstructPairwiseSearch(File1, File2, P);
 
-        % S.File(1) - query file, S.File(2) - target file.        
+        % S.File(1) - query file, S.File(2) - target file.
         if Query.NumNT <= S.File(2).NumNT
             Search = aFR3DSearch(Query,S);
         end
     end
 
-    if ~isfield(Search,'Candidates') || isempty(Search.Candidates)        
-        addToNoCandidatesFile(file2, P);        
+    if ~isfield(Search,'Candidates') || isempty(Search.Candidates)
+        addToNoCandidatesFile(file2, P);
         disc = Inf;
     else
         disc = min(Search.Discrepancy);
@@ -102,10 +106,16 @@ function [searchPossible] = isSizeCompatible(File1, File2)
 
     % check if the query without bulges is <= than the entire target loop
     if queryEffectiveLength <= File2.NumNT
-        searchPossible = 1;
+        if exactSizeLimit and queryEffectiveLength >= 20
+            if File2.NumNT - queryEffectiveLength <= 1
+                searchPossible = 1;
+            end
+        else
+            searchPossible = 1;
+        end
     else
         searchPossible = 0;
-    end    
+    end
 
 end
 
@@ -113,7 +123,7 @@ function [] = addToNoCandidatesFile(loop_id, P)
 
     fid = fopen(P.no_candidates, 'a');
     fprintf(fid,'%s\n', loop_id);
-    fclose(fid); 
+    fclose(fid);
 
 end
 
@@ -137,68 +147,68 @@ function [matched] = aSearchFlankingBases(File1, File2, P)
     % skip hairpins
     if strcmp(File1.Filename(1:2),'HL')
         matched = 1;
-        return;                
+        return;
     end
-    
+
     % don't search in the same file
     if File1.Filename == File2.Filename
         matched = 1;
         return;
-    end        
+    end
 
     F = File1;
-    [S.File(1), Indices] = leaveOnlyFlankingBases(File1);            
+    [S.File(1), Indices] = leaveOnlyFlankingBases(File1);
     S.File(2)            = leaveOnlyFlankingBases(File2);
     S.QIndex = [1 2];
-    
+
     Query.Geometric      = 1;
-    Query.ExcludeOverlap = 1;    
+    Query.ExcludeOverlap = 1;
     Query.SaveDir        = pwd;
     Query.SearchFiles    = File2.Filename;
-    Query.Filename       = F.Filename;                
-    Query.ChainList      = {F.NT(Indices).Chain}; 
-    Query.Name           = [File1.Filename '_' File2.Filename];        
+    Query.Filename       = F.Filename;
+    Query.ChainList      = {F.NT(Indices).Chain};
+    Query.Name           = [File1.Filename '_' File2.Filename];
     Query.NumNT          = length(Indices);
     Query.NTList         = {F.NT(Indices).Number};
     Query.NT             = F.NT(Indices);
-    
+
     Query.Diagonal(1:Query.NumNT) = {'N'};
     Query.Edges = cell(Query.NumNT, Query.NumNT);
     Query.Edges{1,4} = 'cWW';
     Query.Edges{2,3} = 'cWW';
-    
-    Query.DiscCutoff = P.Discrepancy;                
-    Query.RelCutoff  = Query.DiscCutoff;           
-    
+
+    Query.DiscCutoff = P.Discrepancy;
+    Query.RelCutoff  = Query.DiscCutoff;
+
     Search = aFR3DSearch(Query,S);
-    
+
     if isfield(Search,'Candidates') && ~isempty(Search.Candidates)
         matched = 1;
         if ismac
             unix(sprintf('rm %s', fullfile(pwd, [Query.Name '.mat'])));
         else
-            delete([Query.Name '.mat']); % delete small search        
+            delete([Query.Name '.mat']); % delete small search
         end
-    end            
-    
+    end
+
 end
 
 function [File, Indices] = leaveOnlyFlankingBases(File)
     chbr = File.chain_breaks;
-    Indices = [1 chbr chbr+1 length(File.NT)];    
+    Indices = [1 chbr chbr+1 length(File.NT)];
     fn = fieldnames(File);
     for j = 1:length(fn)
         [r,c] = size(File.(fn{j}));
         if r == c && r == File.NumNT
             File.(fn{j}) = File.(fn{j})(Indices, Indices);
-        end            
+        end
     end
     File.NT = File.NT(Indices);
-    File.NumNT = 4;        
+    File.NumNT = 4;
 end
 
 function [Query,S] = aConstructPairwiseSearch(File1, File2, P)
-    
+
     F = File1;
     S.File = [File1 File2];
     S.QIndex = [1 2];
@@ -209,36 +219,36 @@ function [Query,S] = aConstructPairwiseSearch(File1, File2, P)
     else
         Indices = 1:length(F.NT);
     end
-                   
+
     Query.Geometric      = 1;
-    Query.ExcludeOverlap = 1;    
+    Query.ExcludeOverlap = 1;
     Query.SaveDir        = P.Subdir;
     Query.SearchFiles    = File2.Filename;
-    Query.Filename       = F.Filename;    
-    Query.ChainList      = {F.NT(Indices).Chain}; 
-    Query.Name           = [File1.Filename '_' File2.Filename];        
+    Query.Filename       = F.Filename;
+    Query.ChainList      = {F.NT(Indices).Chain};
+    Query.Name           = [File1.Filename '_' File2.Filename];
     Query.Number         = 1;
     Query.NumNT          = length(Indices);
     Query.NTList         = {F.NT(Indices).Number};
     Query.NT             = F.NT(Indices);
     Query.IndicesManual  = Indices;
-    
+
     Query.Diagonal(1:Query.NumNT) = {'N'};
-    Query.Edges = cell(Query.NumNT,Query.NumNT);    
-    
-    Query.DiscCutoff = P.Discrepancy;                
-    Query.RelCutoff = Query.DiscCutoff;           
-    
+    Query.Edges = cell(Query.NumNT,Query.NumNT);
+
+    Query.DiscCutoff = P.Discrepancy;
+    Query.RelCutoff = Query.DiscCutoff;
+
     for i=1:Query.NumNT-1
-        Query.Diff{i+1,i} = '>';       
-    end                   
+        Query.Diff{i+1,i} = '>';
+    end
 
     if strcmp(File1.Filename(1:2), 'IL')
-        Query.Edges{1,Query.NumNT} = 'cWW';        
-        chainbreak = find(Indices==File1.chain_breaks);        
-        Query.Diff{chainbreak+1,chainbreak} = '';    
+        Query.Edges{1,Query.NumNT} = 'cWW';
+        chainbreak = find(Indices==File1.chain_breaks);
+        Query.Diff{chainbreak+1,chainbreak} = '';
         Query.Edges{chainbreak,chainbreak+1} = 'cWW';
-        
+
         if File1.Flank(1,File1.chain_breaks) == 1
             Query.Edges{1, find(Indices==File1.chain_breaks)} = 'flankSS';
         end
