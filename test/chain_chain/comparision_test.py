752ac96cfddb208e75b594caefcb8ac36fe1f287
Blake Sweeney <blakes.85@gmail.com>
2016-11-28 22:15:50 -0500

    More test fixing
    Reorganize them a bit as well as fix some id issues.

--- test/chain_chain/comparision_test.py
+++ test/chain_chain/comparision_test.py
@@ -1,32 +1,45 @@
 import pytest
 
-import numpy as np
 from numpy.testing import assert_array_almost_equal
 from numpy.testing import assert_almost_equal
 
 from test import StageTest
 
 from pymotifs import models as mod
+from pymotifs.core import Skip
+from pymotifs.utils import row2dict
 from pymotifs.core import InvalidState
 from pymotifs.chain_chain.comparision import Loader
 
 
-class QueryTest(StageTest):
+class BaseTest(StageTest):
     loader_class = Loader
 
+    def chain_id(self, pdb, chain):
+        with self.loader.session() as session:
+            return session.query(mod.ChainInfo).\
+                filter_by(pdb_id=pdb, chain_name=chain).\
+                one().\
+                chain_id
+
+    def pair(self, pdb1, chain1, pdb2, chain2):
+        return (self.chain_id(pdb1, chain1), self.chain_id(pdb2, chain2))
+
+
+class QueryTest(BaseTest):
     def test_it_knows_if_data_exists(self):
-        assert self.loader.has_data((240, 1671)) is True
+        val = self.pair('1ET4', 'A', '1ET4', 'B')
+        assert self.loader.has_data(val) is True
 
     def test_it_knows_if_no_data_exists(self):
         assert self.loader.has_data((-1, 1)) is False
 
     def test_it_knows_if_reversed_data_exists(self):
-        assert self.loader.has_data((1671, 240)) is True
+        val = self.pair('1ET4', 'B', '1ET4', 'A')
+        assert self.loader.has_data(val) is True
 
 
-class ToProcessTest(StageTest):
-    loader_class = Loader
-
+class ToProcessTest(BaseTest):
     @pytest.mark.skip()
     def test_it_creates_all_groups(self):
         pass
@@ -40,20 +53,18 @@ class ToProcessTest(StageTest):
         pass
 
 
-class LoadingInfoTest(StageTest):
-    loader_class = Loader
-
+class LoadingInfoTest(BaseTest):
     def test_it_loads_correct_info(self):
         val = self.loader.info(4)
-        assert 'chain_id' in val
-        val.pop('chain_id')  # This is a DB only id so we don't test it
         assert val == {
             'pdb': '1CGM',
+            'chain_id': self.chain_id('1CGM', 'I'),
             'chain_name': 'I',
             'model': 1,
             'sym_op': 'P_25',
             'ife_id': '1CGM|1|I',
             'name': '1CGM|1|I+P_25',
+            'alt_id': None,
         }
 
     def test_it_complains_given_unknown_id(self):
@@ -65,16 +76,7 @@ class LoadingInfoTest(StageTest):
             self.loader.info(3)
 
 
-class LoadingCorrespondneceTest(StageTest):
-    loader_class = Loader
-
-    def chain_id(self, pdb, name):
-        with self.loader.session() as session:
-            return session.query(mod.ChainInfo).\
-                filter_by(pdb_id=pdb, chain_name=name).\
-                one().\
-                chain_id
-
+class LoadingCorrespondneceTest(BaseTest):
     def test_can_load_a_correspondence(self):
         c1 = self.chain_id('4A3J', 'P')
         c2 = self.chain_id('3J9M', 'u')
@@ -86,22 +88,13 @@ class LoadingCorrespondneceTest(StageTest):
         assert self.loader.corr_id(c2, c1) == 1
 
     def test_it_complains_given_invalid_pair(self):
-        with pytest.raises(InvalidState):
+        with pytest.raises(Skip):
             self.loader.corr_id(-1, 2)
 
 
-class LoadingResiduesTest(StageTest):
-    loader_class = Loader
-
+class LoadingResiduesTest(BaseTest):
     def corr_id(self, info1, info2):
         return self.loader.corr_id(info1['chain_id'], info2['chain_id'])
-
-    def chain_id(self, pdb, name):
-        with self.loader.session() as session:
-            return session.query(mod.ChainInfo).\
-                filter_by(pdb_id=pdb, chain_name=name).\
-                one().\
-                chain_id
 
     def setUp(self):
         super(LoadingResiduesTest, self).setUp()
@@ -111,6 +104,7 @@ class LoadingResiduesTest(StageTest):
             'chain_id': self.chain_id('1X8W', 'D'),
             'model': 1,
             'sym_op': '1_555',
+            'alt_id': None,
         }
         self.info2 = {
             'pdb': '1GRZ',
@@ -118,6 +112,7 @@ class LoadingResiduesTest(StageTest):
             'chain_id': self.chain_id('1GRZ', 'B'),
             'model': 1,
             'sym_op': '1_555',
+            'alt_id': None,
         }
 
     def test_it_loads_all_data(self):
@@ -136,16 +131,8 @@ class LoadingResiduesTest(StageTest):
                                           [0.140197, -0.83386, -0.533876],
                                           [0.986404, 0.0709304, 0.148245]])
 
-class ComputingDataTest(StageTest):
-    loader_class = Loader
 
-    def chain_id(self, pdb, name):
-        with self.loader.session() as session:
-            return session.query(mod.ChainInfo).\
-                filter_by(pdb_id=pdb, chain_name=name).\
-                one().\
-                chain_id
-
+class ComputingDataTest(BaseTest):
     def corr_id(self, chain1, chain2):
         return self.loader.corr_id(chain1, chain2)
 
@@ -153,11 +140,13 @@ class ComputingDataTest(StageTest):
         c1 = self.chain_id('1X8W', 'D')
         c2 = self.chain_id('1GRZ', 'B')
         corr_id = self.corr_id(c1, c2)
-        val = self.loader.data((c1, c2))
+        val = [row2dict(d) for d in self.loader.data((c1, c2))]
         assert len(val) == 2
         # Remove discrepancy since it needs a different method
         assert_almost_equal(val[0].pop('discrepancy'), 0.227388, decimal=6)
         assert_almost_equal(val[1].pop('discrepancy'), 0.227388, decimal=6)
+        del val[0]['chain_chain_similarity_id']
+        del val[1]['chain_chain_similarity_id']
         assert val[0] == {
             'chain_id_1': c1,
             'chain_id_2': c2,
