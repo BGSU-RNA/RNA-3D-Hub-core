658376c1b0ffc28beb685c2e940146fa61390027
Blake Sweeney <blakes.85@gmail.com>
2016-12-01 15:01:42 -0500

    Remove old quality importers

--- pymotifs/units/loader.py
+++ pymotifs/units/loader.py
@@ -13,7 +13,6 @@ from pymotifs import core
 from pymotifs.units.info import Loader as InfoLoader
 from pymotifs.units.coordinates import Loader as CoordinateLoader
 from pymotifs.units.distances import Loader as DistancesLoader
-from pymotifs.units.quality import Loader as QualityLoader
 # from pymotifs.units.redundant import RedundantNucleotidesLoader
 from pymotifs.units.centers import Loader as CenterLoader
 from pymotifs.units.rotation import Loader as RotationLoader
@@ -25,5 +24,5 @@ class Loader(core.StageContainer):
     """
 
     """The `Stages` this will run"""
-    stages = set([InfoLoader, QualityLoader, DistancesLoader, CenterLoader,
-                  RotationLoader, CoordinateLoader, IncompleteLoader])
+    stages = set([InfoLoader, DistancesLoader, CenterLoader, RotationLoader,
+                  CoordinateLoader, IncompleteLoader])
