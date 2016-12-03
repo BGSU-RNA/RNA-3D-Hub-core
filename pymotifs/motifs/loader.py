0e4657d3e5793b4a75bb21d8240f3e6493c5788d
Blake Sweeney <blakes.85@gmail.com>
2016-12-03 14:30:45 -0500

    Add stage to populate ml_loops
    This adds a stage to populate the ml_loops table. It is pretty basic but
    may work.

--- pymotifs/motifs/loader.py
+++ pymotifs/motifs/loader.py
@@ -10,6 +10,7 @@ from pymotifs.motifs.info import Loader as MotifLoader
 # from pymotifs.motifs.parents import Loader as ParentLoader
 from pymotifs.motifs.release import Loader as ReleaseLoader
 from pymotifs.motifs.loop_order import Loader as LoopOrderLoader
+from pymotifs.motifs.assignments import Loader as AssignmentLoader
 from pymotifs.motifs.discrepancies import Loader as DiscrepancyLoader
 from pymotifs.motifs.loop_positions import Loader as LoopPositionLoader
 # from pymotifs.motifs.parent_counts import Loader as ParentCountsLoader
@@ -17,4 +18,4 @@ from pymotifs.motifs.loop_positions import Loader as LoopPositionLoader
 
 class Loader(core.StageContainer):
     stages = [ReleaseLoader, DiscrepancyLoader, MotifLoader, LoopOrderLoader,
-              LoopPositionLoader]
+              LoopPositionLoader, AssignmentLoader]
