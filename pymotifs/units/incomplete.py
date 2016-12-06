310e713f816894d5adf982a968b2e0b00c0a1620
Blake Sweeney <blakes.85@gmail.com>
2016-12-06 11:01:05 -0500

    Fixes to incomplete unit detection
    First, lots more docs.
    
    Secondly, I'm set merge = True because this sets the pk and so we can
    safely merge this data.
    
    Thirdly, I've added detection of already seen unit ids to the data
    method. This should prevent duplicate writing, which we do want to
    avoid.

--- pymotifs/units/incomplete.py
+++ pymotifs/units/incomplete.py
@@ -14,19 +14,65 @@ from pymotifs.utils import row2dict
 
 from pymotifs.units.info import Loader as InfoLoader
 
-Entry = coll.namedtuple('Entry', ['model', 'chain', 'number', 'sequence',
-                                  'alt_id', 'insertion'])
+
+class Entry(coll.namedtuple('Entry', ['model', 'chain', 'number', 'sequence',
+                                      'alt_id', 'insertion'])):
+    """A useful tuple for creating mappings and handling incomplete/missing
+    units. It does not include symmetry operators to allow for mapping from
+    these entries to all unit ids which will include symmetry operators.
+
+    Attributes
+    ----------
+    model : int
+        The model number
+    chain : str
+        The chain name
+    number : int
+        The unit number
+    sequence : str
+        The sequence of the unit
+    alt_id : str or None
+        The alt id
+    insertion : str or None
+        The insertation code
+    """
+    pass
 
 
 class Loader(core.SimpleLoader):
+    merge = True
     allow_no_data = True
     dependencies = set([InfoLoader])
 
     def query(self, session, pdb):
+        """Build a query to
+
+        Parameters
+        ----------
+        session : pymotifs.core.Session
+            The session to use.
+
+        pdb : str
+            The pdb to query for.
+        """
         return session.query(mod.UnitIncomplete).\
             filter(mod.UnitIncomplete.pdb_id == pdb)
 
     def mapping(self, pdb):
+        """Create a mapping from the parts of a unit id to a full unit id for
+        some pdb id. This is needed since the missing_keys method provides part
+        of the unit ids to indicate what parts are unobserved/missing.
+
+        Parameters
+        ----------
+        pdb : str
+            The PDB id
+
+        Returns
+        -------
+        mapping : dict
+            A mapping from Entry keys to unit id.
+        """
         with self.session() as session:
             query = session.query(mod.UnitInfo.model,
                                   mod.UnitInfo.chain,
@@ -46,6 +92,21 @@ class Loader(core.SimpleLoader):
         return mapping
 
     def missing_keys(self, pdb):
+        """Determine the unit ids in a file that are missing/unobserved. This
+        parses the mmCIF file for the given PDB and examines the
+        'pdbx_unobs_or_zero_occ_residues' and 'pdbx_unobs_or_zero_occ_atoms'
+        data blocks for this information.
+
+        Parameters
+        ----------
+        pdb : str
+            The PDB id.
+
+        Returns
+        -------
+        missing : set
+            A set of Entry tuples
+        """
         cif = self.cif(pdb)
         total = it.chain(getattr(cif, 'pdbx_unobs_or_zero_occ_residues', []),
                          getattr(cif, 'pdbx_unobs_or_zero_occ_atoms', []))
@@ -68,12 +129,34 @@ class Loader(core.SimpleLoader):
         return missing
 
     def data(self, pdb, **kwargs):
+        """Determine the unit ids from a given structure that are incomplete
+        and create data for writing to the database. This will only create
+        entries for things that are incomplete, not missing because of database
+        constraints. The unit_incomplete table requries that the unit_id
+        exist in unit_info as well, and missing ones will not (by definition).
+
+
+        Parameters
+        ----------
+        pdb : str
+            The PDB id.
+
+        Returns
+        -------
+        data : list
+            A list of pymotifs.model.UnitIncomplete entries for all incomplete
+            unit ids.
+        """
         data = []
+        seen = set()
         mapping = self.mapping(pdb)
         for key in self.missing_keys(pdb):
             if key not in mapping:
                 self.logger.info("Skipping unobserved %s", str(key))
             for uid in mapping[key]:
+                if uid in seen:
+                    continue
+                seen.add(uid)
                 data.append(mod.UnitIncomplete(unit_id=uid,
                                                pdb_id=pdb,
                                                is_incomplete=True))
