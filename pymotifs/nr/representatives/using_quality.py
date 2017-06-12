1e5e5c9497bad14d970c873af8ae69f93076d4b5
Blake Sweeney <blakes.85@gmail.com>
2017-06-12 16:54:30 +0100

    Reorganize and implement new representative method
    This moves the representative selection code around. It was getting very
    large so I figured multiple files would be easier for me to work with.
    At the moment this breaks the functionality of representative selection.
    Basically I separated out the methods by how they work. I suspect we
    will delete the methods that use structure data and keep only the
    quality ones, but until that is tested I won't do so.
    
    In addition, I've added a first pass at implementing the compscore
    method for selecting the representative. This isn't yet tested. I've
    also built a small base class for the quality methods so I can easily
    have some common behaviors.

---
+++ pymotifs/nr/representatives/using_quality.py
@@ -0,0 +1,260 @@
+import abc
+
+import numpy as np
+
+from pymotifs import core
+from pymotifs import models as mod
+from pymotifs.utils import row2dict
+
+from pymotifs.constants import MANUAL_IFE_REPRESENTATIVES
+from pymotifs.constants import WORSE_THAN_MANUAL_IFE_REPRESENTATIVES
+
+from pymotifs.representatives.core import Representative
+
+
+class QualityBase(Representative):
+    """
+    Find representatives using quality metrics provided by PDB.
+    """
+    hardcoded = MANUAL_IFE_REPRESENTATIVES
+    worse = WORSE_THAN_MANUAL_IFE_REPRESENTATIVES
+
+    __metaclass__ = abc.ABCMeta()
+
+    @abc.abstractmethod
+    def has_quality(self, member):
+        pass
+
+    @abc.abstractmethod
+    def load_quality(self, members):
+        """
+        Load all required quality data for the given members.
+        """
+        pass
+
+    @abc.abstractmethod
+    def sort_by_quality(self, members):
+        """
+        This must provide a linear ordering to all members in the group. The
+        one which is listed first will be used as the representative. In
+        addition it should be tolerant of missing data and the like as it will
+        be called twice. First with X-ray structures with all quality data and
+        then later with non-xray structures that may be missing data.
+        """
+        pass
+
+    @abc.abstractmethod
+    def select_candidates(self, members):
+        return members
+
+    def find_hardcoded(self, members):
+        ids = {m['id'] for m in members}
+
+        found = ids.intersection(self.hardcoded)
+        if len(found) > 1:
+            self.logger.error("More than one hardcoded, using quality")
+            return None
+
+        if not found:
+            self.logger.debug("No hardcoded representative")
+            return None
+
+        selected = next(m for m in members if m['id']in found)
+        self.logger.info("Found hardcoded representative %s", selected)
+        return selected
+
+    def filter_by_method(self, members):
+        def is_good_xray(member):
+            return member['method'] == 'X-RAY DIFFRACTION' and \
+                member['resolution'] <= 4.0 and \
+                self.has_quality(member)
+
+        if any(is_good_xray(m) for m in members):
+            return [m for m in members if is_good_xray(m)]
+        return list(members)
+
+    def filter_by_nts(self, members):
+        best = max(m['length'] for m in members)
+        return [m for m in members if m['length'] >= 0.75 * best]
+
+    def filter_by_resolution(self, members):
+        best = min(m['resolution'] for m in members)
+        return [m for m in members if abs(m['resolution'] - best) <= 0.2]
+
+    def use_hardcoded(self, members):
+        current = members[0]
+        hardcoded = self.find_hardcoded(members)
+
+        if not hardcoded:
+            self.logger.debug("No hardcoded representative to use")
+            return list(members)
+
+        if current == hardcoded:
+            self.logger.info("Hardcoded and chosen agree")
+            return list(members)
+
+        if current['id'] not in self.worse:
+            self.logger.warning("Automatically selected %s not in"
+                                " WORSE_THAN_MANUAL_IFE_REPRESENTATIVES",
+                                current)
+
+        return self.insert_as_representative(hardcoded, members)
+
+    def final_ordering(self, ordered, given):
+        already_ordered = {m['id'] for m in ordered}
+        rest = [m for m in given if m['id'] not in already_ordered]
+        return ordered + self.sort_by_quality(rest)
+
+    def __call__(self, given):
+        self.logger.info("Selecting representative for %s", given)
+        with_quailty = self.load_quality(given['members'])
+        candidates = self.select_candidates(with_quailty)
+        ordered_by_quality = self.sort_by_quality(candidates)
+        with_representative = self.use_hardcoded(ordered_by_quality)
+        return self.final_ordering(with_representative, given['members'])
+
+
+class QualityMetrics(QualityBase):
+    method = 'quality-metrics'
+
+    def has_quality(self, member):
+        required = set(['rsrz', 'backbone', 'clashscore'])
+        return required.issubset(member['quality']['has'])
+
+    def load_quality(self, members):
+        def as_quality(data):
+            return {
+                'has': {key for key, value in data.items() if value},
+                'rsrz': data.get('rsrz') or 100,
+                'backbone': data.get('backbone') or 100,
+                'clashscore': data.get('clashscore') or 500,
+            }
+
+        known = {m['pdb'] for m in members}
+        with self.session() as session:
+            query = session.query(mod.PdbQuality.pdb_id,
+                                  mod.PdbQuality.percent_rsrz_outliers.
+                                  label('rsrz'),
+                                  mod.PdbQuality.clashscore,
+                                  mod.PdbQuality.percent_rota_outliers.
+                                  label('backbone'),
+                                  ).\
+                filter(mod.PdbQuality.pdb_id.in_(known))
+
+            measures = {}
+            for result in query:
+                result = row2dict(result)
+                pdb_id = result.pop('pdb_id')
+                measures[pdb_id] = as_quality(result)
+
+        for member in members:
+            pdb_id = member['pdb']
+            member['quality'] = measures.get(pdb_id, as_quality({}))
+
+        return members
+
+    def sort_by_quality(self, members):
+        def key(member):
+            quality = member['quality']
+            quality_factor = quality['rsrz'] * \
+                quality['clashscore'] * \
+                quality['backbone'] * \
+                pow(member['resolution'], 4)
+            size_factor = member['length'] + member['bp']
+            return quality_factor / size_factor
+
+        return sorted(members, key=key)
+
+    def select_candidates(self, members):
+        best_method = self.filter_by_method(members)
+        best_nts = self.filter_by_nts(best_method)
+        best_resolution = self.filter_by_resolution(best_nts)
+        if not best_resolution:
+            raise core.InvalidState("Nothing with good resolution")
+        return best_resolution
+
+
+class CompScore(QualityBase):
+    """
+    This implements a composite scoring metric that should provide a good
+    linear ordering of IFE's.
+    """
+    name = 'compscore'
+
+    def has_quality(self, member):
+        return 'real_space_r' in member['has'] and \
+            'rscc' in member['has'] and \
+            'pdb_rfree' in member['has'] and \
+            'resolution' in member['has']
+
+    def as_quality(self, entries):
+        def avg_of(name):
+            return sum(e[name] for e in entries if e[name]) / len(entries)
+
+        def has_entry(name):
+            return any(e[name] for e in entries)
+
+        def percent_of(name):
+            length = len(entries)
+            return 100 * sum(1.0 for e in entries if e[name]) / length
+
+        def first_value(name):
+            return entries[0][name]
+
+        def assign(name, default, function, tracking):
+            if has_entry(name):
+                tracking.add(name)
+                return (function(name), tracking)
+            return (default, tracking)
+
+        resolution, has = assign('resolution', 100, first_value, set())
+        rfree, has = assign('pdb_rfree', 2, first_value, has)
+        percent_clash, has = assign('clash_score', 0, percent_of, has)
+        average_rsr, has = assign('real_space_r', 2, avg_of, has)
+        average_rscc, has = assign('rscc', 10, avg_of, has)
+
+        return {
+            'resolution': resolution,
+            'percent_clash': percent_clash,
+            'average_rsr': average_rsr,
+            'average_rscc': average_rscc,
+            'rfree': rfree,
+            'has': has,
+        }
+
+    def load_quality(self, members):
+        for member in members:
+            chains = [mem['chain'] for mem in members if members['structured']]
+            if not chains:
+                chains = [mem['chain'] for mem in members]
+
+            with self.session() as session:
+                query = session.query(
+                    mod.UnitQuality.real_space_r,
+                    mod.UnitQuality.clash_score,
+                    mod.UnitQuality.rscc,
+                    mod.PdbQuality.rfree,
+                ).join(mod.UnitQuality,
+                       mod.UnitQuality.unit_id == mod.UnitInfo.unit_id).\
+                    filter(mod.UnitInfo.pdb_id == member['pdb']).\
+                    filter(mod.UnitInfo.model == member['model']).\
+                    filter(mod.UnitInfo.sym_op == member['sym_op']).\
+                    filter(mod.UnitInfo.chain.in_(chains)).\
+                    filter(mod.UnitInfo.unit.in_(['A', 'C', 'G', 'U']))
+
+            member['quality'] = self.as_quality([row2dict(r) for r in query])
+        return member
+
+    def sort_by_quality(self, members):
+        def key(member):
+            average = np.mean(member['resolution'],
+                              member['percent_clash'],
+                              10 * member['average_rsr'],
+                              10 * (1 - member['average_rscc']),
+                              10 * member['rfree'])
+
+            return 100 * average
+            # eqn = 100*AVERAGE(resolution, percent clash, 10*average rsr,
+            #                   10*(1-average rscc), 10*rfree)
+
+        return sorted(members, key=key, reverse=True)
---
+++ pymotifs/nr/representatives/using_structure.py
@@ -0,0 +1,239 @@
+import operator as op
+import itertools as it
+
+from pymotifs import core
+
+from pymotifs.constants import NR_BP_PERCENT_INCREASE
+from pymotifs.constants import NR_LENGTH_PERCENT_INCREASE
+
+from pymotifs.representatives.core import Representative
+
+
+def bp_per_nt(chain):
+    """Function to use for sorting by bps/nt. Deals with things where there
+    are 0 bps or nts. It must have bps, length and id entry.
+
+    :param dict chain: Chain to compute a sorting key for.
+    :returns: A tuple that can be used to sort the chains.
+    """
+    ratio = 0
+    if chain['bp'] and chain['length']:
+        ratio = float(chain['bp']) / float(chain['length'])
+    resolution = chain.get('resolution')
+    if resolution:
+        resolution = resolution * -1
+    return (ratio, resolution, chain['id'])
+
+
+class Naive(Representative):
+    """
+    This will select the naive representative. This just selects the chain
+    with the most bp/nt and uses that as the representative.
+    """
+    method = 'naive'
+
+    def __call__(self, group):
+        return sorted(group['members'], key=bp_per_nt, reverse=True)
+
+
+class Increase(Representative):
+    """
+    A class to find the representative for a group of ifes. This will find
+    the best in terms of bp/nt and attempt to find all those with more bp's and
+    nts in the set.
+    """
+    method = 'percent-increase'
+
+    def initial_representative(self, group):
+        """
+        Find the best chain terms of bps/nts. This is the starting point for
+        finding the representative in a set of ifes. This method is naive
+        because it does not favor more complete structures. In addition, it is
+        very sensitive to minor changes in number of basepairs and nts.
+
+        Parameters
+        ----------
+        members : dict
+            A group dict to find the representative of.
+
+        Returns
+        --------
+            The initial representative.
+        """
+        naive = Naive(self.config, self.session)
+        return naive(group)[0]
+
+    def candidates(self, best, members):
+        """
+        Find all possible candidates for a representative within the group,
+        given a current best ife. This finds all chains that have at least as
+        many basepairs and nucleotides as the best chain. The chains will be
+        returned in sorted order.
+
+        :param dict best: The current best.
+        :param list group: The list of dicts to search.
+        :returns: The list of candidates for the representative.
+        """
+
+        len = op.itemgetter('length')
+        bp = op.itemgetter('bp')
+
+        def same(chain):
+            return bp(chain) == bp(best) and len(chain) == len(best)
+
+        possible = it.ifilter(lambda c: len(c) >= len(best), members)
+        possible = it.ifilter(lambda c: bp(c) >= bp(best), possible)
+        possible = it.ifilterfalse(same, possible)
+        return sorted(possible, key=bp_per_nt, reverse=True)
+
+    def increase(self, first, second, key):
+        """
+        Compute the percent increase for the given set of dictionaries and
+        with the given key. If the second one is 0 then we return 100 for 100%
+        increase.
+
+        :param dict first: Dictionary to get the increase to.
+        :param dict second: Dictionary to get the increase from.
+        :param str key: Key to use
+        :returns: The percent increase.
+        """
+
+        if not second[key]:
+            if not first[key]:
+                return 0
+            return 100
+        return (float(first[key]) / float(second[key]) - 1) * 100
+
+    def best_above_cutoffs(self, representative, candidates, length_increase,
+                           bp_increase):
+        """
+        This will find the true representative given a current one and a
+        list of candidates. This will attempt to maximize the number of bps and
+        nts in the candidate as compared to the current representative. In
+        addition, it will only change representatives if we have have enough of
+        an increase. This adds stability to the process so minor improvements
+        are ignored, while large ones will lead to large changes.
+
+        :param dict representative: The current representative.
+        :param list candidates: A list of candidates to examine.
+        :returns: The new representative.
+        """
+
+        for candidate in candidates:
+            length_change = self.increase(candidate, representative, 'length')
+            bp_change = self.increase(candidate, representative, 'bp')
+            if length_change >= length_increase and bp_change >= bp_increase:
+                return candidate
+        return representative
+
+    def __call__(self, initial, length_increase=NR_LENGTH_PERCENT_INCREASE,
+                 bp_increase=NR_BP_PERCENT_INCREASE):
+        """
+        Find the representative for the group.
+
+        Parameters
+        ----------
+        group : list
+            List of IFE's to find the representative of.
+        length_increase : float
+            The fraction increase in resolved nucleotides that an IFE must have
+            to be selected as representative.
+        bp_increase : float
+            The fraction increase of basepairs that an IFE must have to be
+            selected as representative.
+
+        Returns
+        -------
+            representative : dict
+        The ife which should be the representative.
+        """
+
+        group = self.filter_group_by_method(initial)
+        best = self.initial_representative(group)
+        if not best:
+            raise core.InvalidState("No current representative")
+        self.logger.debug("Naive representative: %s", best['id'])
+
+        rep = best
+        while True:
+            candidates = self.candidates(rep, group['members'])
+            self.logger.debug("Found %i representative candidates",
+                              len(candidates))
+            new_rep = self.best_above_cutoffs(rep, candidates,
+                                              length_increase,
+                                              bp_increase)
+            if new_rep == rep:
+                break
+            self.logger.info("Changed representative from %s to %s",
+                             rep['id'], new_rep['id'])
+            rep = new_rep
+
+        if not rep:
+            raise core.InvalidState("No representative found")
+
+        self.logger.debug("Computed representative: %s", rep['id'])
+        return self.insert_as_representative(rep,
+                                             initial['members'],
+                                             sort=bp_per_nt)
+
+
+class AnyIncrease(Increase):
+    """
+    A modification of the percent-increase representative method that will
+    instead select the representative if it has any increase in length and bp.
+    """
+    method = 'any-increase'
+
+    def __call__(self, group):
+        return super(AnyIncrease, self).__call__(group,
+                                                 length_increase=0,
+                                                 bp_increase=0)
+
+
+class ParentIncrease(Increase):
+    """
+    This is a modification of the percent increase method which uses the
+    parent representative as the initial representative, if possible. If this
+    is not possible then it will go back to the simple bp/nt selection.
+    """
+    method = 'parent-percent-increase'
+
+    def initial_representative(self, group):
+        """
+        Select the initial representative. This will be the representative
+        for the parent if there is only 1 parent. If there is more than 1
+        parent, or none, then this will switch to bp/nt.
+
+        Parameters
+        ----------
+        group : dict
+            A group dict that must have a parent entry.
+
+        Returns
+        -------
+            The initial representative.
+        """
+
+        if not group['parent']:
+            self.logger.debug("No parent to use representative of")
+            return super(ParentIncrease, self).initial_representative(group)
+
+        if len(group['parent']) > 1:
+            self.logger.debug("Multiple parents, using naive")
+            return super(ParentIncrease, self).initial_representative(group)
+
+        parent = group['parent'][0]
+        previous = parent.get('representative', None)
+        if not previous:
+            self.logger.warning("Loaded parent %s has no representative, using"
+                                " naive", parent)
+            return super(ParentIncrease, self).initial_representative(group)
+
+        current_ids = {mem['id'] for mem in group['members']}
+        if previous['id'] not in current_ids:
+            self.logger.warning("Not using previous rep %s, because not in "
+                                "current group: %s",
+                                previous['id'], current_ids)
+            return super(ParentIncrease, self).initial_representative(group)
+
+        return previous
