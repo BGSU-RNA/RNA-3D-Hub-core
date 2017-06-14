964fdc1059324240f23df1fea5a1539455053de3
Blake Sweeney <blakes.85@gmail.com>
2017-06-14 15:14:54 +0100

    Try to keep same interface
    Hopefully this makes the classes findable in the normal way.

---
+++ pymotifs/nr/representatives/__init__.py
@@ -0,0 +1,4 @@
+from pymotifs.nr.representatives.using_structure import *
+from pymotifs.nr.representatives.using_quality import *
+from pymotifs.nr.representatives.core import fetch
+from pymotifs.nr.representatives.core import known
