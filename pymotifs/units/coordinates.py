2ef77e8306f47d9564210f55768746aeb97ffb12
Jamie Cannone <jjcannone@bluehen.udel.edu>
2017-11-17 17:00:26 -0600

    Apply modified call for single atom residues Changes the call to CifAtom to use the protect_lists_of_lists logic added to fr3d/cif/writer.py.  Allows for correct parsing of single atom residues.

--- pymotifs/units/coordinates.py
+++ pymotifs/units/coordinates.py
@@ -1,5 +1,5 @@
 """A loader to store the coordinates for each residue in the database. This
-will write one row per non water residue into the unit_coordinates table in the
+will write one row per non-water residue into the unit_coordinates table in the
 database. Data is written in the CIF format. This will write only the ATOM
 level entries (atom_site in cif) but will not include the header lines like
 'loop_' or '_atom_site.group_PDB'.
@@ -23,8 +23,8 @@ class Loader(core.SimpleLoader):
     dependencies = set([InfoLoader])
 
     def query(self, session, pdb):
-        """Create a query to find all all entries in `units_coordinates` for
-        the given PDB id.
+        """Create a query to find all entries in `units_coordinates` for the
+        given PDB id.
 
         Parameters
         ----------
@@ -37,7 +37,7 @@ class Loader(core.SimpleLoader):
         Returns
         -------
         query : Query
-            The query for the given structure
+            The query for the given structure.
         """
 
         return session.query(mod.UnitCoordinates).\
@@ -47,35 +47,33 @@ class Loader(core.SimpleLoader):
 
     def coordinates(self, pdb, residue):
         """Compute a string of the coordinates in CIF format (the atom_site
-        block) for the given residue. This will exclude the header and trailing
-        lines that are part of the atom_site entries. This is because these are
-        entries are meant to be concatenated together for the coordinate server
-        later.
+        block) for the given residue. Exclude the header and trailing lines
+        that are part of the atom_site entries, because these entries are meant 
+        to be concatenated together for the coordinate server later.
 
         Parameters
         ----------
         pdb : str
-            The PDB id to use
+            The PDB id to use.
 
         residue : fr3d.data.Component
-            The residue to convert
+            The residue to convert.
 
         Returns
         -------
         coordinates : str
-            A string that represents CIF data formated data for the given
-            residue.
+            A string that represents CIF-formatted data for the given residue.
         """
 
         structure = Structure([residue], pdb=pdb)
         sio = StringIO()
-        writer = CifAtom(sio, unit_ids=False)
+        writer = CifAtom(sio, unit_ids=False, protect_lists_of_lists=True)
         writer(structure)
         raw = sio.getvalue()
-        # Lines that star with data_, loop_ and _ or # are header/comment lines
-        # and so are excluded
         coords = []
         for line in raw.split('\n'):
+            # Exclude header/comment lines that start with: 1) "data_", 
+            # 2) "loop_",  3) "_", or 4) "#".
             if not line or \
                     line.startswith('data_') or \
                     line.startswith('loop_') or \
@@ -86,13 +84,13 @@ class Loader(core.SimpleLoader):
 
     def data(self, pdb, **kwargs):
         """Compute the coordinate entries for the given PDB. This will exclude
-        water molecules as those aren't generally worth displaying in
+        water molecules as those aren't generally worth displaying in the
         coordinate server.
 
         Parameters
         ----------
         pdb : str
-            The PDB id to use
+            The PDB id to use.
 
         Yields
         ------
@@ -105,6 +103,9 @@ class Loader(core.SimpleLoader):
             if unit.sequence == 'HOH':
                 continue
             coord = self.coordinates(pdb, unit)
+            self.logger.debug("data: PDB: %s" % pdb)
+            self.logger.debug("data: unit: %s" % unit)
+            self.logger.debug("data: coordinates: %s" % coord)
             if not coord:
                 raise core.InvalidState("No coordinates computed for %s" %
                                         unit)
