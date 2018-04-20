41db869d7d74a7ac065058bab8254f8498055c79
Jamie Cannone <jjcannone@bluehen.udel.edu>
2018-04-20 12:49:17 -0400

    Include NrQualityLoader

--- pymotifs/nr/loader.py
+++ pymotifs/nr/loader.py
@@ -1,19 +1,22 @@
 """Run all nr level stages. This runs:
 
 nr.release
-    Create a new NR release and cache the NR data
+    Create a new NR release and cache the NR data.
 nr.classes
-    Store all NR classes (equivleance groups)
+    Store all NR classes (equivleance groups).
 nr.parents
-    Store the parent information for the cached NR release
+    Store the parent information for the cached NR release.
 nr.parent_counts
-    Store the counts of changes from the parents
+    Store the counts of changes from the parents.
 nr.id_mapping
     Modified the cached data with database ids.
+nr.cqs
+    Compute per-class Composite Quality Score data and
+    store in the database.
 nr.ordering
-    Compute the ordering of chains within an nr class
+    Compute the ordering of chains within an nr class.
 nr.cleanup
-    Cleanup the cached data
+    Cleanup the cached data.
 """
 import pymotifs.core as core
 
@@ -23,10 +26,11 @@ from pymotifs.nr.chains import Loader as ChainLoader
 from pymotifs.nr.parents import Loader as ParentLoader
 from pymotifs.nr.parent_counts import Loader as CountLoader
 from pymotifs.nr.id_mapping import Loader as IdLoader
+from pymotifs.nr.cqs import NrQualityLoader
 from pymotifs.nr.ordering import Loader as OrderingLoader
 from pymotifs.nr.cleanup import Cleanup
 
 
 class Loader(core.StageContainer):
     stages = [ReleaseLoader, IdLoader, ClassLoader, ChainLoader, ParentLoader,
-              CountLoader, OrderingLoader, Cleanup]
+              CountLoader, NrQualityLoader, OrderingLoader, Cleanup]
