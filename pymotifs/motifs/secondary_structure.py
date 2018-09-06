8c476c04649a3f638f0760aa40dce2e8460a3149
Jamie Cannone <jjcannone@bluehen.udel.edu>
2018-09-06 15:17:15 -0400

    Changes to Motif Atlas output files Copies the VARNA files to the appropriate website directory.
    N.B. Beware of the filesystem structure being different on test
    and production.  Not a problem this time.
    
    Copies and renames the mat output files and stores them in a
    directory parallel to the VARNA files.  Ideally, this would be
    contained in its own class, but it is logically quite similar to
    the VARNA renaming process.

--- pymotifs/motifs/secondary_structure.py
+++ pymotifs/motifs/secondary_structure.py
@@ -1,9 +1,12 @@
 """
-This is a loader which will move the generated motif files into the correct
-directories for the website.
+This is a loader which will copy the generated motif files into the correct
+directories for the website and JAR3D work.
 """
 import os
+import re
 import shutil
+
+from pprint import pformat
 
 from pymotifs import core
 from pymotifs.motifs.release import Loader as ReleaseLoader
@@ -65,6 +68,13 @@ class Loader(core.Loader):
             motif['motif_id'] + '.png',
         )
 
+    def mat_location(self, mat_dir, release, motif):
+        type = motif['motif_id'].split('_')[0]
+        return os.path.join(
+            mat_dir,
+            motif['motif_id'] + '.mat',
+        )
+
     def has_data(self, pair, **kwargs):
         return False
 
@@ -82,6 +92,8 @@ class Loader(core.Loader):
         if not cached:
             raise core.InvalidState("No cached data")
 
+        self.logger.debug("Cached data for %s: %s", loop_type, pformat(cached['motifs']))
+
         directory = self.final_directory(loop_type, release)
         self.logger.debug("Use directory %s...", directory)
         if not os.path.isdir(directory):
@@ -95,19 +107,38 @@ class Loader(core.Loader):
         self.logger.debug("Web directory: %s", web_dir)
         if not os.path.isdir(web_dir):
             os.makedirs(web_dir)
-            self.logger.debug("Needed to create directory %s", web_dir)
+            self.logger.debug("Needed to create web directory %s", web_dir)
 
         if not os.path.isdir(web_dir):
-            self.logger.debug("Failed to create directory %s", web_dir)
+            self.logger.debug("Failed to create web directory %s", web_dir)
+
+        mat_check = 0
 
         for motif in cached['motifs']:
+            if mat_check == 0:
+                mat_dir = re.sub('/2ds/.*$','/mat',motif['2d'])
+                self.logger.debug("mat directory: %s", mat_dir)
+                if not os.path.isdir(mat_dir):
+                    os.makedirs(mat_dir)
+                    self.logger.debug("Needed to create mat directory %s", mat_dir)
+                    mat_check = 1
+
+                if not os.path.isdir(mat_dir):
+                    self.logger.debug("Failed to create mat directory %s", mat_dir)
+
             location = self.final_location(release, motif)
             web_location = self.web_location(release, motif)
+            mat_file = motif['2d'].replace("2ds","Groups").replace("png","mat")
+            mat_out = self.mat_location(mat_dir, release, motif)
+            #mat_out = os.path.join(mat_dir, mat_file)
             self.logger.debug("Copying motif 2d %s %s to %s",
                               motif['motif_id'], motif['2d'], location)
             shutil.copy(motif['2d'], location)
             self.logger.debug("Copying motif 2d %s %s to %s",
                               motif['motif_id'], motif['2d'], web_location)
             shutil.copy(motif['2d'], web_location)
+            self.logger.debug("To be copying motif mat file %s %s to %s",
+                              motif['motif_id'], mat_file, mat_out)
+            shutil.copy(mat_file, mat_out) 
 
         return None
