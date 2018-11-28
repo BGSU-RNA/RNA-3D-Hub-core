c01a04fb378b8382972c8388611daae7fe7bd35e
Craig L. Zirbel <zirbel@bgsu.edu>
2018-11-28 11:29:49 -0500

    Deal with J3, J4, etc.

--- FR3DMotifs/extractLoops.m
+++ FR3DMotifs/extractLoops.m
@@ -15,10 +15,10 @@ function [Loops, L, err_msg] = extractLoops(pdb_id,loopType)
         L         = 0;
         err_msg   = '';
 
-        if strcmp(loopType,'IL')
-            Loops = aCombinedInternalLoopSearch(pdb_id);
-        elseif strcmp(loopType,'HL')
+        if strcmp(loopType,'HL')
             Loops = aFlankHairpinSearch(pdb_id);
+        elseif strcmp(loopType,'IL')
+            Loops = aCombinedInternalLoopSearch(pdb_id);
         elseif strfind(loopType,'J')
             Loops = aFlankJunctionSearch(pdb_id,loopType);
         end
@@ -45,7 +45,8 @@ function [Loops] = aSearchToLoops(Search)
 
         pdbfile = Search.Candidates(i,end);
         File    = Search.File(pdbfile);
-        range   = aGetRange(Search.Candidates(i,:));
+        [range,breaks,ranges,nwcranges] = aGetRange(Search.Candidates(i,:));
+        File.chain_breaks = breaks;
 
         fn = fieldnames(File);
         for j = 1:length(fn)
@@ -71,34 +72,41 @@ function [Loops] = aSearchToLoops(Search)
         File.AllLoops_table.full_id = [ids{:}];
         File.AllLoops_table.full_id = File.AllLoops_table.full_id(1:end-1);
 
-        if strcmp(loop_type,'IL')
-            File.chain_breaks = find(range==Search.Candidates(i,2));
-        elseif strcmp(loop_type,'J3')
-            File.chain_breaks = find(range==Search.Candidates(i,2) | ...
-                                     range==Search.Candidates(i,4));
-        elseif strcmp(loop_type,'HL') % a hairpin
-            File.chain_breaks = [];
-        end
-
         File.AllLoops_table.loop_name = aGetLoopName(File);
 
         nt_seq   = [File.NT.Base];
-        if strcmp(loop_type,'IL')
-            breaks = File.chain_breaks;
-            File.AllLoops_table.seq   = [nt_seq(1:breaks)       '*' nt_seq(breaks+1:end)];
-            File.AllLoops_table.r_seq = [nt_seq(breaks+1:end)   '*' nt_seq(1:breaks)];
-            File.AllLoops_table.nwc   = [nt_seq(2:breaks-1)     '*' nt_seq(breaks+2:end-1)];
-            File.AllLoops_table.r_nwc = [nt_seq(breaks+2:end-1) '*' nt_seq(2:breaks-1)];
-        elseif strcmp(loop_type(1),'J')
-            File.AllLoops_table.seq   = nt_seq;
-            File.AllLoops_table.r_seq = '';
-            File.AllLoops_table.nwc   = ''; %seq(2:end-1);
-            File.AllLoops_table.r_nwc = '';
-        elseif strcmp(loop_type,'HL')
+        if strcmp(loop_type,'HL')
             File.AllLoops_table.seq   = nt_seq;
             File.AllLoops_table.r_seq = '';
             File.AllLoops_table.nwc   = File.AllLoops_table.seq(2:end-1);
             File.AllLoops_table.r_nwc = '';
+            File.AllLoops_table.sequence{1}    = nt_seq;
+            File.AllLoops_table.nwcsequence{1} = nt_seq(2:end-1);
+        elseif strcmp(loop_type,'IL')
+            File.AllLoops_table.seq   = [nt_seq(1:breaks)       '*' nt_seq(breaks+1:end)];
+            File.AllLoops_table.r_seq = [nt_seq(breaks+1:end)   '*' nt_seq(1:breaks)];
+            File.AllLoops_table.nwc   = [nt_seq(2:breaks-1)     '*' nt_seq(breaks+2:end-1)];
+            File.AllLoops_table.r_nwc = [nt_seq(breaks+2:end-1) '*' nt_seq(2:breaks-1)];
+            File.AllLoops_table.sequence{1} = [nt_seq(1:breaks)       '*' nt_seq(breaks+1:end)];
+            File.AllLoops_table.sequence{2} = [nt_seq(breaks+1:end)   '*' nt_seq(1:breaks)];
+            File.AllLoops_table.nwcsequence{1} = [nt_seq(2:breaks-1)     '*' nt_seq(breaks+2:end-1)];
+            File.AllLoops_table.nwcsequence{2} = [nt_seq(breaks+2:end-1) '*' nt_seq(2:breaks-1)];
+        elseif strcmp(loop_type(1),'J')
+            rangenumbers = [1:length(ranges) 1:length(ranges)];  % easier to cycle
+            for rotation = 1:length(ranges),
+                sequence{rotation} = nt_seq(ranges{rotation});
+                nwcsequence{rotation} = nt_seq(nwcranges{rotation});
+                for kk = 1:length(ranges),
+                    sequence{rotation} = [sequence{rotation} '*' nt_seq(ranges{rangenumbers(rotation+kk)})];
+                    nwcsequence{rotation} = [nwcsequence{rotation} '*' nt_seq(nwcranges{rangenumbers(rotation+kk)})];
+                end
+                File.AllLoops_table.sequence{rotation} = sequence{rotation};
+                File.AllLoops_table.nwcsequence{rotation} = nwcsequence{rotation};
+            end
+            File.AllLoops_table.seq   = sequence{1};
+            File.AllLoops_table.r_seq = sequence{2};       % for backward compatibility
+            File.AllLoops_table.nwc   = nwcsequence{1};
+            File.AllLoops_table.r_nwc = nwcsequence{2};
         end
 
         if i == 1
@@ -251,7 +259,7 @@ function [Loops] = aFlankJunctionSearch(pdb_id,loopType)
 
     for i = 1:Query.NumNT-1
         if mod(i,2) == 0
-            Query.Edges{i,i+1} = 'flank';
+            Query.Edges{i,i+1} = 'flank';     % may only find strands with non-empty interior
 %             Query.Diff{i+1,i}  = '>';
         else
             Query.Edges{i,i+1} = 'cWW';
@@ -279,20 +287,36 @@ function [Query] = aSetUpGenericQuery()
 
 end
 
-function [range] = aGetRange(row)
+% aGetRange sorts the indices in the given row and interprets them as
+% starting and ending ranges or strands of nucleotides, because we are
+% using the result of a search for flanking cWW basepairs
+% The result, range, will be all indices of nucleotides on all strands.
+function [range,breaks,ranges,nwcranges] = aGetRange(row)
 
     % accepts Search.Candidates(i,:) as an input
-    c = length(row);
     row = sort(row(1:end-1)); % discard the pdbfile id
+    c = length(row);
 
-    if c == 3 % hairpins
+    if c == 2 % hairpins
         range = row(1):row(2);
-    elseif c == 5 % internal loops
+        breaks = [];
+        ranges{1} = row(1):row(2);
+        nwcranges{1} = (row(1)+1):(row(2)-1);
+    elseif c == 4 % internal loops
         range = [row(1):row(2) row(3):row(4)];
-    elseif mod(c,2) == 1 % junctions
+        breaks = row(2);
+        ranges{1} = row(1):row(2);
+        ranges{2} = row(3):row(4);
+        nwcranges{1} = (row(1)+1):(row(2)-1);
+        nwcranges{2} = (row(3)+1):(row(4)-1);
+    elseif mod(c,2) == 0 % junctions
         range = [];
-        for q = 1:2:c-1
+        breaks = [];
+        for q = 1:2:(c-1)
             range = [range row(q):row(q+1)];  %#ok<AGROW>
+            breaks = [breaks row(q+1)];
+            ranges{(q+1)/2} = row(q):row(q+1);
+            nwcranges{(q+1)/2} = (row(q)+1):(row(q+1)-1);
         end
     else
         error('Wrong number of nucleotides');
