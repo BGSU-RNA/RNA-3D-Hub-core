f10319140756df3419b613f62a9d39460ef1714c
Matlab Pipeline User <pipeline@rnaprod1.bgsu.edu>
2020-11-01 10:42:19 -0500

    Replace REST with graphQL query for PDB info

--- pymotifs/pdbs/info.py
+++ pymotifs/pdbs/info.py
@@ -34,7 +34,7 @@ class Loader(core.MassLoader):
     """
 
     def rename(self, report):
-        """Rename the columns of the report, and cover some values to the
+        """Rename the columns of the report, and convert some values to the
         correct types. The columns will be renamed according to `Loader.names`
         and the resolution value will be converted to a float if it exists.
 
@@ -86,7 +86,7 @@ class Loader(core.MassLoader):
                 limit(1)
             return bool(query.count())
 
-    def data(self, pdbs, **kwargs):
+    def olddata(self, pdbs, **kwargs):
         """Compute a report of the given PDBs. This will use PDB's custom REST
         service to compute a report for the given PDBs. This will then be
         processed slightly for storage in our database. The columns used in the
@@ -105,6 +105,12 @@ class Loader(core.MassLoader):
 
         helper = CustomReportHelper(fields=self.names.keys())
         data = helper(pdbs)
+
+        print("pymotifs/pdbs/info.py gets this data:")
+        print(data)
+        print("for these pdb ids:")
+        print(pdbs)
+
         if not data:
             raise core.StageFailed("Could not load data for all pdbs %s" %
                                    str(pdbs))
@@ -113,3 +119,81 @@ class Loader(core.MassLoader):
             self.logger.error("Could not get all data for all pdbs")
 
         return [mod.PdbInfo(**self.rename(report)) for report in data]
+
+    def data(self, pdbs, **kwargs):
+        """New in November 2020.
+        Get data from PDB's graphQL query.
+        We loop over PDB IDs, make a query for each one, and
+        run the query, then split out the data that is returned.
+
+        Parameters
+        ----------
+        pdbs : list
+            A list of PDB ids
+
+        Returns
+        -------
+        reports : list
+            A list of PdbInfo objects to write to the database.
+        """
+
+        # The GraphQL query is defined as a multi-line string.
+        query = """
+        {
+          entry(entry_id:"XXXX") {
+            entry {
+              id
+            }
+            struct {
+              title
+            }
+            exptl {
+              method
+            }
+            rcsb_entry_info {
+              resolution_combined
+            }
+            rcsb_accession_info {
+              deposit_date
+              initial_release_date
+              revision_date
+            }
+          }
+        }
+        """
+
+        data = []
+
+        for pdb in pdbs:
+
+            self.logger.info("Using PDB graphQL to get data for " % pdb)
+
+            currentquery = query.replace("XXXX",pdb)
+
+            request = requests.post('https://data.rcsb.org/graphql', json={'query': currentquery})
+            if request.status_code == 200:
+                result = request.json()
+            else:
+                self.logger.error("Could not get data for %s" % pdb)
+                raise core.StageFailed("Could not load data for all pdbs")
+
+            renamed = {}
+            renamed["pdb_id"] = result["data"]["entry"]["entry"]["id"]
+            renamed["title"] = result["data"]["entry"]["struct"]["title"]
+            renamed["experimental_technique"] = result["data"]["entry"]["exptl"][0]["method"]
+            renamed["deposition_date"] = result["data"]["entry"]["rcsb_accession_info"]["deposit_date"][0:10]
+            renamed["release_date"] = result["data"]["entry"]["rcsb_accession_info"]["initial_release_date"][0:10]
+            renamed["revision_date"] = result["data"]["entry"]["rcsb_accession_info"]["revision_date"][0:10]
+            renamed["ndb_id"] = result["data"]["entry"]["entry"]["id"]
+            renamed["resolution"] = result["data"]["entry"]["rcsb_entry_info"]["resolution_combined"]
+
+            if renamed['resolution']:
+                try:
+                    renamed['resolution'] = float(renamed['resolution'][0])
+                except:
+                    renamed['resolution'] = None
+                    self.logger.error("Resoultion entry for %s is not a number" % pdb)
+
+            data.append(renamed)
+
+        return [mod.PdbInfo(report) for report in data]
