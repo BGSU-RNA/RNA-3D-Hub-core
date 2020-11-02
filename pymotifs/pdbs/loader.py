71a40c53446b8fc4fae48d10fc9e2a41a05509db
Matlab Pipeline User <pipeline@rnatest1.bgsu.edu>
2020-11-01 23:35:39 -0500

    New PDB queries exclude obsolete files already

--- pymotifs/pdbs/loader.py
+++ pymotifs/pdbs/loader.py
@@ -2,18 +2,15 @@
 
 pdbs.info
     Fetch the basic information about a PDB.
-pdbs.obsolete
-    Fetch the data on obsolete pdbs.
 """
 
 import pymotifs.core as core
 
 from pymotifs.pdbs.info import Loader as InfoLoader
-from pymotifs.pdbs.obsolete import Loader as ObsoleteLoader
 
 
 class Loader(core.StageContainer):
     """The loader for all PDB level data."""
 
-    stages = set([InfoLoader, ObsoleteLoader])
+    stages = set([InfoLoader])
     """Stages to run."""
