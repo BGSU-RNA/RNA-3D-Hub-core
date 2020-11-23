92ff63c7e05fdc9060ccfc95b625d0727292a13b
Matlab Pipeline User <pipeline@rnatest1.bgsu.edu>
2020-11-22 20:44:11 -0500

    Better logging, print keys

--- pymotifs/md_units/rotation.py
+++ pymotifs/md_units/rotation.py
@@ -5,6 +5,7 @@ from itertools import groupby
 from pymotifs import core
 from pymotifs import models as mod
 from pymotifs.units.rotation import Loader as RotationLoader
+from fr3d.modified_parent_mapping import modified_nucleotides
 
 
 # list the modified nucleotides that should be checked to see if they have a rotation matrix
@@ -12,7 +13,11 @@ from pymotifs.units.rotation import Loader as RotationLoader
 # there needs to be a mapping of atoms in each modified nucleotide to the parent atoms,
 # in fr3d-python.fr3d.modified_parent_mapping.py
 # Apparently this variable needs to be a tuple for the SQL query in get_units_without_rotation
-mod_ref = ('DA','DC','DG','DT','A2M', 'CAR', '1MG', '5MC', '5MU', 'H2U', '4SU', 'MIA', 'U34', 'G7M', 'QUO', '1MA', '7MG', 'UDP', 'SRA', 'A23', 'GDP', 'C31', 'U31', 'PGP', 'CCC', 'N', '2MA', 'OMC', '2MG', 'M2G', 'OMG', 'YYG', 'YG', '5BU', 'CG1', 'I', 'T6A', 'PU', 'PPU', '12A', '2MU', '70U', 'U8U', 'PYY', 'IC', 'IG', 'PSU', 'OMU', 'ATL', 'LCA', 'LCC', 'LCG', 'LKC', 'TLN', 'MAD', 'GAO', 'UAR', '5IC', 'FHU', 'CH', 'CBV', 'IU', 'AVC', '5CG', 'P5P', 'GH3', 'GOM', 'SSU', 'PQ1', 'PUY', 'AET', '0C', '0G', '0U', 'UR3', '3DA', 'MTU', 'AP7', '5AA', 'F3N', '31H', '31M', '23G', 'MA6', 'MNU', 'A2L', 'C2L', 'G2L', 'U2L', 'RIA', 'A5M', 'M5M', 'UD5', 'S4C', 'FMU', 'PYO', 'SUR', 'P1P', '6IA', 'RTP', '5FU', 'N6G', 'NF2', 'T2T', 'O2C', 'ONE', 'ZAD', 'ZCY', 'ZGU', 'ZHP', 'ZTH', '6MZ', 'CM0', 'A5O', 'RSP', 'RSQ', 'UPV', 'A44', 'C5L', 'G48', 'T39', '2AU', '10C', '4OC', '8AN', 'ZBC', 'ZBU', 'A3P', 'G46', '1SC', 'N5M', 'US5', '1DP', '3TD', '3AU', 'F3O', '5CF', 'A6A', 'A6C', 'A6G', 'A6U', 'GRB', 'RUS', '574', 'DJF', 'C43', 'U36', 'LMS', 'GDO', 'RPC', 'A9Z', 'UBD', 'U37', '1RN', 'A7E', '7AT', '6FC', '6FU', 'URU', '0A', '0U1', '2SG', 'Y5P', '45A', 'M3O', '50L', '50N', '56B', 'U5M', 'CVC', '8AZ', '6F7', '6NW', '6OO', '6OP', 'F2T', '5HM', '73W', '75B', '8OS', '8RJ', 'G4P', '9QV', 'MUM', 'C4J', 'M1Y', 'A7C', 'EQ4', 'U4M', 'UFB', 'UOA', 'UOB', 'B8H', 'B8K', 'B8Q', 'B8T', 'B8W', 'B9B', 'B9H', 'BGH', 'E6G', 'E7G', 'I4U', 'M7A', 'MHG', 'P4U', 'P7G', '4AC', 'B8N', 'E3C', 'RY', 'KGV', 'JMH', 'KAK', 'LHH', 'LV2', 'UY4', 'UY1', 'O2Z', 'U23')
+mod_ref = ('DA','DC','DG','DT','A2M', 'CAR', '1MG', '5MC', '5MU', 'H2U', '4SU', 'MIA', 'U34', 'G7M', 'QUO', '1MA', '7MG', 'UDP', 'SRA', 'A23', 'GDP', 'C31', 'U31', 'PGP', 'CCC', '2MA', 'OMC', '2MG', 'M2G', 'OMG', 'YYG', 'YG', '5BU', 'CG1', 'T6A', 'PU', 'PPU', '12A', '2MU', '70U', 'U8U', 'PYY', 'IC', 'IG', 'PSU', 'OMU', 'LCA', 'LCC', 'LCG', 'LKC', 'MAD', 'GAO', 'UAR', '5IC', 'FHU', 'CH', 'CBV', 'IU', 'AVC', '5CG', 'P5P', 'GH3', 'GOM', 'SSU', 'PQ1', 'PUY', 'AET', '0C', '0G', '0U', 'UR3', 'MTU', 'AP7', '31M', '23G', 'MA6', 'MNU', 'A2L', 'C2L', 'G2L', 'U2L', 'RIA', 'A5M', 'M5M', 'UD5', 'S4C', 'FMU', 'PYO', 'SUR', 'P1P', '6IA', 'RTP', '5FU', 'N6G', 'NF2', 'T2T', 'O2C', 'ONE', 'ZAD', 'ZCY', 'ZGU', 'ZHP', 'ZTH', 'A5O', 'RSP', 'RSQ', 'UPV', 'A44', 'G48', 'T39', '2AU', '10C', '4OC', '8AN', 'ZBC', 'ZBU', 'A3P', 'G46', '1SC', 'N5M', 'US5', '1DP', '3TD', '3AU', 'F3O', '5CF', 'A6A', 'A6C', 'A6G', 'A6U', 'GRB', 'RUS', '574', 'DJF', 'C43', 'U36', 'LMS', 'GDO', 'RPC', 'A9Z', 'UBD', 'U37', '1RN', 'A7E', '7AT', '6FC', '6FU', 'URU', '0A', '0U1', '2SG', 'Y5P', '45A', 'M3O', '50L', '50N', '56B', 'U5M', 'CVC', '8AZ', '6F7', '6NW', '6OO', '6OP', 'F2T', '5HM', '73W', '75B', '8OS', '8RJ', 'G4P', '9QV', 'MUM', 'C4J', 'M1Y', 'A7C', 'EQ4', 'U4M', 'UFB', 'UOA', 'UOB', 'B8H', 'B8K', 'B8Q', 'B8T', 'B8W', 'B9B', 'B9H', 'BGH', 'E6G', 'E7G', 'I4U', 'M7A', 'MHG', 'P4U', 'P7G', '4AC', 'B8N', 'E3C', 'RY', 'KGV', 'JMH', 'KAK', 'LHH', 'LV2', 'UY4', 'UY1', 'O2Z', 'U23')
+# removed from the list because they are not in modified_parent_mapping:  'TLN', 'ATL', 'N', '3DA', 'C5L', '6MZ', 'I', '5AA', 'F3N', '31H', 'CM0',
+
+# this list was produced directly from modified_parent_mapping, plus DNA nucleotides
+mod_ref = ('DA','DC','DG','DT','10C', '125', '126', '127', '12A', '1MA', '1MG', '1SC', '23G', '2AU', '2MA', '2MG', '2MU', '2OM', '3AU', '3MU', '3TD', '4OC', '4SU', '5BU', '5FA', '5FU', '5IC', '5MC', '5MU', '6IA', '70U', '7MG', '8AN', 'A23', 'A2L', 'A2M', 'A3P', 'A44', 'A5M', 'A5O', 'AET', 'AP7', 'AVC', 'C2L', 'C31', 'C43', 'CBV', 'CCC', 'CG1', 'CH', 'CNU', 'CSF', 'FHU', 'G25', 'G2L', 'G46', 'G48', 'G7M', 'GAO', 'GDP', 'GH3', 'GOM', 'GRB', 'GTP', 'H2U', 'IC', 'IG', 'IU', 'KAG', 'LCA', 'M2G', 'M4C', 'M5M', 'MA6', 'MAD', 'MGQ', 'MGV', 'MIA', 'MNU', 'MTU', 'N5M', 'N6G', 'O2G', 'OMC', 'OMG', 'OMU', 'ONE', 'P5P', 'PGP', 'PMT', 'PPU', 'PSU', 'PU', 'PYO', 'QUO', 'RIA', 'RSQ', 'RUS', 'S4C', 'SRA', 'SSU', 'SUR', 'T6A', 'TPG', 'U2L', 'U2P', 'U31', 'U34', 'U36', 'U37', 'U8U', 'UAR', 'UD5', 'UMP', 'UR3', 'URD', 'US5', 'XTS', 'YG', 'YYG', 'ZAD', 'ZBC', 'ZBU', 'ZCY', 'ZGU')
 
 class Loader(core.SimpleLoader):
     """A class to load rotation matrices into the database.
@@ -78,6 +83,15 @@ class Loader(core.SimpleLoader):
         :yields: Yields a series of rotation matrices.
         """
 
+        # print out the sequence identifiers for which a mapping to a parent nucleotide is available
+        tuple_text = "("
+        for k in sorted(modified_nucleotides.keys()):
+            tuple_text += "'" + k + "', "
+        tuple_text += ")"
+        tuple_text = tuple_text.replace(", )",")")
+        print(tuple_text)
+        self.logger.info("Sequence identifiers with known parent nucleotide: %s" % tuple_text)
+
         # The get_units_without_rotation method will get all modified RNA unit_id without rotation
         # based on the list of modified units given in the mod_ref tuple.
         units_without_rotation = self.get_units_without_rotation(pdb)
@@ -102,8 +116,11 @@ class Loader(core.SimpleLoader):
                 print('Loading 3D structure file %s' % pdb_id)
                 structure = self.structure(pdb_id)      # read .cif file
 
+                foundone = False
+
                 for residue in structure.residues():
                     if residue.unit_id() in unit_id_list:
+                        foundone = True
                         if hasattr(residue, 'rotation_matrix'):
                             matrix = residue.rotation_matrix
 
@@ -123,3 +140,15 @@ class Loader(core.SimpleLoader):
                                                 cell_2_1=float(matrix[2, 1]),
                                                 cell_2_2=float(matrix[2, 2]))
 
+                            else:
+                                self.logger.info('%s rotation matrix is None' % residue.unit_id())
+                                print('%s rotation matrix is None' % residue.unit_id())
+
+                        else:
+                            self.logger.info('%s has no rotation matrix' % residue.unit_id())
+                            print('%s rotation matrix is None' % residue.unit_id())
+
+
+                if not foundone:
+                    print("No matches to %s" % unit_id_list)
+                    self.logger.info("No matches to %s" % unit_id_list)
