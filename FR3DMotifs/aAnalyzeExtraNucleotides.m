76dabe0b3c0390c7369886e90663dad32c490643
Matlab Pipeline User <pipeline@rnaprod1.bgsu.edu>
2021-06-29 11:23:04 -0400

    Push apart IL with different single base bulges

--- FR3DMotifs/aAnalyzeExtraNucleotides.m
+++ FR3DMotifs/aAnalyzeExtraNucleotides.m
@@ -15,6 +15,7 @@ function [MM] = aAnalyzeExtraNucleotides(MM, loop_ids, saveMatFile)
     BP_PENALTY      = 4;
     NEAR_BP_PENALTY = 5;
     STACK_PENALTY   = 6;
+    MISMATCHED_BULGE = 6;           % these may need to be integers
     FILENAME = 'MM_extraNTs.mat';
     LOGFILE  = 'MM_extraNTs.txt';
 
@@ -24,6 +25,7 @@ function [MM] = aAnalyzeExtraNucleotides(MM, loop_ids, saveMatFile)
     NEAR_PAIRS = 101:112;
 
     isHairpin = strcmp(loop_ids{1}(1:2), 'HL');
+    isInternal = strcmp(loop_ids{1}(1:2), 'IL');
 
     N = length(MM(1,:));
 
@@ -39,24 +41,33 @@ function [MM] = aAnalyzeExtraNucleotides(MM, loop_ids, saveMatFile)
 
             load(getSearchAddress(loop_ids{i}, loop_ids{j}), 'Search');
 
+            % find the lowest-discrepancy match
             cand = find(Search.Discrepancy == min(Search.Discrepancy));
+            % index of the PDB file with minimum discrepancy
             pdb  = Search.Candidates(cand(1),end); %#ok<FNDSB>
-            F1   = Search.File(pdb); % structure found in this search
+            F1   = Search.File(pdb); % nts of loop j found when i is searched in j
 
             load(getPrecomputedDataAddress(loop_ids{j}), 'File');
-            F2 = File; % the same structure taken independently
+            F2 = File; % original coordinates of loop j
 
+            % indices of the nts in j that are matched by nts in i
             coreNts  = Search.Candidates(cand(1),1:end-1);
+            % cell array of core nucleotide numbers
             indices1 = {F1.NT(coreNts).Number};
+            % cell array of all numbers from the original PDB file
             indices2 = {F2.NT.Number};
 
+            % there are no extra nts in j compared to i
+            % recall that bulged nts in i were not part of the search of i within j
             if length(indices1) == length(indices2)
                 continue;
             end
 
+            % cell arrays for the chain identifiers for the nts
             chains1 = {F1.NT(coreNts).Chain};
             chains2 = {F2.NT.Chain};
 
+            % cell arrays for model numbers
             if isfield(F1.NT(1),'ModelNum')
                 models1 = {F1.NT(coreNts).ModelNum};
                 models2 = {F2.NT.ModelNum};
@@ -70,10 +81,11 @@ function [MM] = aAnalyzeExtraNucleotides(MM, loop_ids, saveMatFile)
             indices1 = strcat(models1, indices1, chains1);
             indices2 = strcat(models2, indices2, chains2);
 
-             % extra nucleotides in the original file
+            % extra nucleotides in the original file of loop j
             [extra,indExtra] = setdiff(indices2, indices1);
             indExtra         = reshape(indExtra, 1, []);
 
+            % collect together any interactions made by these extra nucleotides
             F2.Edge = fix(abs(F2.Edge));
             interactions         = reshape(F2.Edge(indExtra,:),1,[]);
             interactionsWithCore = reshape(F2.Edge(indExtra,coreNts),1,[]);
@@ -115,8 +127,32 @@ function [MM] = aAnalyzeExtraNucleotides(MM, loop_ids, saveMatFile)
                 MM(j,i) = HAIRPIN_STACK_PENALTY2;
                 annotate_extra_stacked_nucleotides(HAIRPIN_STACK_PENALTY2);
 
-            end
+            % loop j is an IL with just two cWW pairs as the core
+            elseif isInternal && length(coreNts) == 4
+                jBulged = aDetectBulgedBases(F2)
 
+                % if the other bases of j are all bulged
+                if 4+length(jBulged) == F2.NumNT
+
+                    % load loop i
+                    load(getPrecomputedDataAddress(loop_ids{i}), 'File');
+                    Fi = File; % original coordinates of loop i
+                    iBulged = aDetectBulgedBases(Fi)
+
+                    % loop i has exactly 4 core nts and all others bulged
+                    if 4+length(iBulged) == Fi.NumNT
+
+                        if length(iBulged) == 1 && length(jBulged) == 1 && F2.NT(jBulged).Base ~= Fi.NT(iBulged).Base
+                            MM(i,j) = MISMATCHED_BULGE
+                            MM(j,i) = MISMATCHED_BULGE
+                        if length(iBulged) == 1 && length(jBulged) > 1
+                            MM(i,j) = MISMATCHED_BULGE
+                            MM(j,i) = MISMATCHED_BULGE
+                        if length(iBulged) > 1 && length(jBulged) == 1
+                            MM(i,j) = MISMATCHED_BULGE
+                            MM(j,i) = MISMATCHED_BULGE
+
+            end
         end
     end
 
