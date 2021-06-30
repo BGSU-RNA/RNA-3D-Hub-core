42952eeccacc4bad0e60f0f98f77563447d74a5d
Matlab Pipeline User <pipeline@rnaprod1.bgsu.edu>
2021-06-30 12:41:16 -0400

    Better logging

--- FR3DMotifs/aCreateMM.m
+++ FR3DMotifs/aCreateMM.m
@@ -6,8 +6,10 @@ function [MM] = aCreateMM(loop_ids,output_dir)
 
     % if the file has already been created, use that
     if exist(FILENAME,'file')
-        MM = load(FILENAME);
+        disp('Loading MM matrix from MM_initial.mat')
+        load(FILENAME,'MM');
         return
+    end
 
     N = length(loop_ids);
 
