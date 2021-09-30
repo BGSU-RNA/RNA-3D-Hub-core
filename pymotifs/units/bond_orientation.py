6d1691ccbc1036b3d61f8a9f565d4f1378922915
Matlab Pipeline User <pipeline@rnatest1.bgsu.edu>
2021-09-29 23:46:34 -0400

    Fixed calls for modified nucleotides

--- pymotifs/units/bond_orientation.py
+++ pymotifs/units/bond_orientation.py
@@ -56,7 +56,7 @@ class Loader(core.SimpleLoader):
                 N1N9 = nt.centers["N1"]
                 C2C4 = nt.centers["C2"]
             elif nt.sequence in modified_nucleotides:
-                parent = get_parent(nt.sequence)
+                parent = modified_nucleotides[nt.sequence]["standard"]
                 if parent in ['A','G','DA','DG']:
                     N1N9 = nt.centers[modified_nucleotides[nt.sequence]["atoms"]["N9"]]
                     C2C4 = nt.centers[modified_nucleotides[nt.sequence]["atoms"]["C4"]]
@@ -65,53 +65,58 @@ class Loader(core.SimpleLoader):
                     C2C4 = nt.centers[modified_nucleotides[nt.sequence]["atoms"]["C2"]]
                 else:
                     print("%s has no identified parent" % nt.unit_id())
+                    self.logger.info("%s has no identified parent" % nt.unit_id())
 
             if len(N1N9) == 3 and len(C2C4) == 3:
 
-                O4P_C1P = nt.centers["C1'"] - nt.centers["O4'"]
-                N1N9_C1P  = nt.centers["C1'"] - N1N9
-                C2C4_N1N9 = N1N9 - C2C4
+                try:
+                    O4P_C1P = nt.centers["C1'"] - nt.centers["O4'"]
+                    N1N9_C1P  = nt.centers["C1'"] - N1N9
+                    C2C4_N1N9 = N1N9 - C2C4
 
-                # chi angle definition:
-                # O4*_C1*_N1_C2 (for pyrimidines)
-                # O4*_C1*_N9_C4 (for purines):
+                    # chi angle definition:
+                    # O4*_C1*_N1_C2 (for pyrimidines)
+                    # O4*_C1*_N9_C4 (for purines):
 
-                perp_to_sugar       = np.cross(O4P_C1P,N1N9_C1P)
-                norm_perp_to_sugar  = np.linalg.norm(perp_to_sugar)
-                if norm_perp_to_sugar != 0:
-                    perp_to_sugar   = perp_to_sugar/norm_perp_to_sugar
+                    perp_to_sugar       = np.cross(O4P_C1P,N1N9_C1P)
+                    norm_perp_to_sugar  = np.linalg.norm(perp_to_sugar)
+                    if norm_perp_to_sugar != 0:
+                        perp_to_sugar   = perp_to_sugar/norm_perp_to_sugar
 
-                perp_to_base        = np.cross(-N1N9_C1P,C2C4_N1N9)
-                norm_perp_to_base   = np.linalg.norm(perp_to_base)
-                perp_to_base        = perp_to_base/norm_perp_to_base
+                    perp_to_base        = np.cross(-N1N9_C1P,C2C4_N1N9)
+                    norm_perp_to_base   = np.linalg.norm(perp_to_base)
+                    perp_to_base        = perp_to_base/norm_perp_to_base
 
-                cross_cross_chi = np.cross(perp_to_base,perp_to_sugar)
+                    cross_cross_chi = np.cross(perp_to_base,perp_to_sugar)
 
-                # Take the dot product of the vectors perp_to_base &
-                # perp_to_sugar to get cos(chi).
-                # Take norm(cross product) to get sin(chi).
+                    # Take the dot product of the vectors perp_to_base &
+                    # perp_to_sugar to get cos(chi).
+                    # Take norm(cross product) to get sin(chi).
 
-                cos_chi = np.dot(perp_to_sugar,perp_to_base)
-                if np.dot(cross_cross_chi,N1N9_C1P) > 0:
-                    sin_chi = np.linalg.norm(cross_cross_chi)
-                else:
-                    sin_chi = -np.linalg.norm(cross_cross_chi)
+                    cos_chi = np.dot(perp_to_sugar,perp_to_base)
+                    if np.dot(cross_cross_chi,N1N9_C1P) > 0:
+                        sin_chi = np.linalg.norm(cross_cross_chi)
+                    else:
+                        sin_chi = -np.linalg.norm(cross_cross_chi)
 
-                # sign of chi_degree matches Bevilacqua 2011 paper on syn and anti
-                # this definition matches the IUPAC definition from http://www.chem.qmul.ac.uk/iupac/misc/pnuc2.html#230
+                    # sign of chi_degree matches Bevilacqua 2011 paper on syn and anti
+                    # this definition matches the IUPAC definition from http://www.chem.qmul.ac.uk/iupac/misc/pnuc2.html#230
 
-                chi  = 180*math.atan2(sin_chi,cos_chi)/math.pi # glycosidic bond angle
+                    chi  = 180*math.atan2(sin_chi,cos_chi)/math.pi # glycosidic bond angle
 
-                # Giving nomenclature according to chi values: anti (most common), or syn
-                if chi > -90 and chi < -45:
-                    orientation = 'Intermediate Syn'
-                elif chi >= -45 and chi < 90:
-                    orientation = 'Syn'
-                else:
-                    orientation = 'Anti'
+                    # Giving nomenclature according to chi values: anti (most common), or syn
+                    if chi > -90 and chi < -45:
+                        orientation = 'Intermediate Syn'
+                    elif chi >= -45 and chi < 90:
+                        orientation = 'Syn'
+                    else:
+                        orientation = 'Anti'
 
-                if chi and orientation:
-                    yield mod.UnitOrientations(unit_id=nt.unit_id(),
-                                            pdb_id=pdb,
-                                            orientation=orientation,
-                                            chi_degree=chi)
+                    if chi and orientation:
+                        yield mod.UnitOrientations(unit_id=nt.unit_id(),
+                                                pdb_id=pdb,
+                                                orientation=orientation,
+                                                chi_degree=chi)
+
+                except:
+                    self.logger.info("%s glycosidic bond orientation calculation failed" % nt.unit_id())
