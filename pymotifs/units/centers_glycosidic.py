ab50c6b5321ecf61672fbc17382b75b9358b95de
Matlab Pipeline User <pipeline@rnatest1.bgsu.edu>
2022-05-23 08:36:53 -0400

    adding skip structures

--- pymotifs/units/centers_glycosidic.py
+++ pymotifs/units/centers_glycosidic.py
@@ -7,23 +7,46 @@ from pymotifs import models as mod
 
 from pymotifs.units.info import Loader as InfoLoader
 
+from pymotifs.skip_files import SKIP
+
+from sqlalchemy import and_
+
 
 class Loader(core.SimpleLoader):
-    """A class to load all base centers into the database.
+    """A class to load all glycosidic centers into the database.
     """
 
     dependencies = set([InfoLoader])
     allow_no_data = True
 
     def to_process(self, pdbs, **kwargs):
-        with self.session() as session:
-            query = session.query(mod.ChainInfo.pdb_id).distinct()
+        # with self.session() as session:
+        #     query_all_pdb_ids = session.query(mod.ChainInfo.pdb_id).distinct()
+        #     query_glycosidic_present = session.query(mod.UnitCenters.pdb_id).\
+        #                                 filter(mod.UnitCenters.name == 'glycosidic').distinct()
+        #                                 #filter(and_(mod.UnitCenters.name == 'base',mod.UnitCenters.name != 'glycosidic')).distinct()
 
-        result = []
-        for id in query:
-            result.append(str(id)[2:6])
+        # pdb_ids = []
+        # for id in query_all_pdb_ids:
+        #     pdb_ids.append(str(id)[2:6])
+        # existed_ids = []
+        # for id in query_glycosidic_present:
+        #     existed_ids.append(str(id)[2:6])
+        # passing_pdb_ids = set()
+        # for pdb_id in pdb_ids:
+        #     if (pdb_id not in SKIP) and (pdb_id not in existed_ids):
+        #         passing_pdb_ids.add(pdb_id)
+        # list(set-set-set)
 
-        return result
+        # # return [pdb_id for pdb_id in result for skip_id in SKIP if pdb_id != skip_id]
+        # # print(len([pdb_id for pdb_id in result for skip_id in SKIP if pdb_id != skip_id]))
+        # # print(len(pdb_ids))
+        # # print(len(passing_pdb_ids))
+        # # print(len(SKIP))
+        # # self.logger.info('length of pdb_ids:%s length of processing:%s length of existed ids:%s' % (len(pdb_ids),len(passing_pdb_ids),len(existed_ids)))
+
+        # return passing_pdb_ids
+        return ['4V3P']
 
     def query(self, session, pdb):
         return session.query(mod.UnitCenters).\
