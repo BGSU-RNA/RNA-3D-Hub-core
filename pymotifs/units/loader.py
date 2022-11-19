bfd58b2b3fae592224189017be7b83c0f071f03f
Matlab Pipeline User <pipeline@rnaprod1.bgsu.edu>
2022-11-19 02:34:56 -0500

    No longer run units.distances stage

--- pymotifs/units/loader.py
+++ pymotifs/units/loader.py
@@ -12,7 +12,7 @@ from pymotifs import core
 
 from pymotifs.units.info import Loader as InfoLoader
 from pymotifs.units.coordinates import Loader as CoordinateLoader
-from pymotifs.units.distances import Loader as DistancesLoader
+#from pymotifs.units.distances import Loader as DistancesLoader
 # from pymotifs.units.redundant import RedundantNucleotidesLoader
 from pymotifs.units.centers import Loader as CenterLoader
 from pymotifs.units.rotation import Loader as RotationLoader
@@ -26,5 +26,5 @@ class Loader(core.StageContainer):
     """
 
     """The `Stages` this will run"""
-    stages = set([InfoLoader, DistancesLoader, CenterLoader, RotationLoader,
+    stages = set([InfoLoader, CenterLoader, RotationLoader,
                   CoordinateLoader, IncompleteLoader, BondOrientationLoader])
