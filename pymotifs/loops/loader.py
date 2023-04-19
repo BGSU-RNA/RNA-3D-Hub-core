02911a64cda27643a29b3ab1e41b8c0e7a36fad5
Matlab Pipeline User <pipeline@rnaprod1.bgsu.edu>
2023-04-19 12:32:52 -0400

    Import from the right place

--- pymotifs/loops/loader.py
+++ pymotifs/loops/loader.py
@@ -3,7 +3,7 @@ from pymotifs import core
 from pymotifs.loops.extractor import Loader as Extractor
 from pymotifs.loops.positions import Loader as PositionLoader
 from pymotifs.loops.quality import Loader as QALoader
-from pymotifs.loops.loops.add_modified_nucleotides import Loader as ModifiedNucleotidesLoader
+from pymotifs.loops.add_modified_nucleotides import Loader as ModifiedNucleotidesLoader
 
 
 class Loader(core.StageContainer):
