bd598445613cfad2891e96e7d4e3992e36e712b9
Matlab Pipeline User <pipeline@rnatest1.bgsu.edu>
2023-04-20 15:36:23 -0400

    create a new stage for filling in glycosidc centers, and finished debugging

--- pymotifs/units/glycosidic_center_filled.py
+++ pymotifs/units/glycosidic_center_filled.py
@@ -29,60 +29,44 @@ class Loader(core.SimpleLoader):
 
         unit_id_with_base = set()
         unit_id_with_glycosidic = set()
-        pdb_id_to_unit_ids  = defaultdict(dict)
+        pdb_id_to_unit_ids  = defaultdict(list)
         for row in query_glycosidic_fill:
             if row.name == 'base':
                 unit_id_with_base.add(row.unit_id)
-            if row.name == 'glycosidc':
+            if row.name == 'glycosidic':
                 unit_id_with_glycosidic.add(row.unit_id)
         unit_id_without_glycosidic = unit_id_with_base - unit_id_with_glycosidic
 
         for unit_id in unit_id_without_glycosidic:
-            if pdb_id_to_unit_ids.get(str(unit_id).split('|')[0]):
-                pdb_id_to_unit_ids[str(unit_id).split('|')[0]].append(unit_id)
-            else:
-                pdb_id_to_unit_ids[str(unit_id).split('|')[0]] = []
-                pdb_id_to_unit_ids[str(unit_id).split('|')[0]].append(unit_id)
+            # if pdb_id_to_unit_ids.get(str(unit_id).split('|')[0]):
+            #     pdb_id_to_unit_ids[str(unit_id).split('|')[0]].append(unit_id)
+            # else:
+            #     pdb_id_to_unit_ids[str(unit_id).split('|')[0]] = []
+            pdb_id_to_unit_ids[str(unit_id).split('|')[0]].append(unit_id)
 
-
+        # print(pdb_id_to_unit_ids)
 
         # extend_pdb_id = set()
         # for unit_id in unit_id_without_glycosidic:
         #     extend_pdb_id.add(str(unit_id).split("|")[0])
-        print(222)
-        print(pdb_id_to_unit_ids.keys())
-        return pdb_id_to_unit_ids
+        # print(222)
+        # print(pdb_id_to_unit_ids)
+        # print([{pdb_id: unit_ids} for pdb_id, unit_ids in pdb_id_to_unit_ids.items()])
+        return [{pdb_id: unit_ids} for pdb_id, unit_ids in pdb_id_to_unit_ids.items()]
 
 
 
     def query(self, session, pdb):
+        # return bool(self.query(session, args).limit(1).count())
+        # so we cannot just return False here
         return session.query(mod.UnitCenters).\
-            filter(mod.UnitCenters.pdb_id == pdb).\
+            filter(mod.UnitCenters.pdb_id == '1').\
             filter(mod.UnitCenters.name == 'glycosidic')
-
-    # def type(self, unit):
-    #     """Compute the component type, ie A, C, G, U is RNA, DA, DC, etc is DNA
-    #     and so forth.
-
-    #     Parameters
-    #     ----------
-    #     unit : Component
-    #         The unit to get the component for
-
-    #     Returns
-    #     -------
-    #     component_type : str
-    #         The component type.
-    #     """
-    #     print(444)
-    #     return units.component_type(unit)      
+ 
 
 
-    def data(self, as_dict, **kwargs):
-        print(333)
-        print(as_dict.keys()[0])
-
-        pdb = list(as_dict.keys())[0]
+    def data(self, pdb_id_to_unit_ids, **kwargs):
+        pdb = pdb_id_to_unit_ids.keys()[0]
         structure = self.structure(pdb)
 
 
@@ -92,7 +76,13 @@ class Loader(core.SimpleLoader):
             for name in residue.centers.definitions():
                 center = residue.centers[name]
                 if len(center) == 3 and name == 'glycosidic':
-                    if residue.unit_id() not in as_dict.values():
+                    if residue.unit_id() in pdb_id_to_unit_ids.values():
+                        print(residue.unit_id(),
+                                            name,
+                                            pdb,
+                                            float(center[0]),
+                                            float(center[1]),
+                                            float(center[2]))
                         yield mod.UnitCenters(unit_id=residue.unit_id(),
                                             name=name,
                                             pdb_id=pdb,
