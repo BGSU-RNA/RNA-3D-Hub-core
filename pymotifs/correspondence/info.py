5574ff1146f33d31ddb3f2d8c867ba8ce34c8384
Matlab Pipeline User <pipeline@rnatest1.bgsu.edu>
2024-06-16 18:00:42 -0400

    With DNA, allow species to be different when aligning

--- pymotifs/correspondence/info.py
+++ pymotifs/correspondence/info.py
@@ -1,6 +1,9 @@
-"""Compute all new pairs of experimental sequences to compare. This will load
+"""
+Compute all new pairs of experimental sequences to compare. This will load
 pairs for all given structures. It does not create comparisons to structures
 not included with the given pdbs.
+
+It pairs rna with rna, dna with dna, hybrid with hybrid
 """
 
 import itertools as it
@@ -28,7 +31,8 @@ from pymotifs import models as mod
 
 
 class Loader(core.MassLoader):
-    """A class to load up all pairs of experimental sequences that should have
+    """
+    A class to load up all pairs of experimental sequences that should have
     an alignment attempted. This does not work per structure as many other
     things do, but instead will compute all possible pairs and then work per
     pair, inserting or storing each pair as needed.
@@ -64,27 +68,31 @@ class Loader(core.MassLoader):
     #         return pdbs
 
     def has_data(self, pdb, **kwargs):
-        """Always returns false. This stage is odd in that we do the filtering
+        """
+        Always returns false. This stage is odd in that we do the filtering
         later on, so we always skip to computing data.
         """
         return False
 
-    def lookup_sequences(self, pdb):
-        """Return all exp_seq_ids for the given pdb. This only assign the
+    def look_up_sequences(self, pdb):
+        """
+        Return all exp_seq_ids for the given pdb. This only assign the
         species id from the given pdb.
+
+        We will need to get the taxonomy id from taxid_species_domain table
 
         :param str pdb: The pdb id to get all sequences for.
         :returns: A list of dictionaries of unique sequences.
         """
 
         with self.session() as session:
-            query = session.query(ExpSeqPdb.exp_seq_id.label('id'),                 ## There are some label functions, they are here for rename the column to "id", "length", and "species" 
+            query = session.query(ExpSeqPdb.exp_seq_id.label('id'),                 ## There are some label functions, they are here for rename the column to "id", "length", and "species"
                                   ExpSeqInfo.normalized_length.label('length'),
                                   mod.ChainInfo.taxonomy_id, # ChainInfo.taxonomy_id.lable('species') # ChainSpecies.species_id.label('species')
                                   ExpSeqInfo.entity_type.label('entity_type')).\
                 join(ExpSeqInfo,
                      ExpSeqInfo.exp_seq_id == ExpSeqPdb.exp_seq_id).\
-                outerjoin(mod.ChainInfo,                                      
+                outerjoin(mod.ChainInfo,
                           mod.ChainInfo.chain_id == ExpSeqPdb.chain_id).\
                 filter(ExpSeqPdb.pdb_id == pdb).\
                 filter(ExpSeqInfo.was_normalized).\
@@ -92,16 +100,17 @@ class Loader(core.MassLoader):
 
             if not query.count():
                 self.logger.warning("No sequences for %s" % pdb)
-            
+
             # for result in query:
             #     self.logger.info("show the query result:%s %s %s %s" % (result.id, result.length, result.species, result.entity_type))
             #     self.logger.info(type(result.species))
-            #     self.logger.info("show the lookup_sequences return: %s" % ut.row2dict(result))                  ## the return value example: {'length': 76L, 'id': 4177L, 'species': 4932L}
-                
+            #     self.logger.info("show the look_up_sequences return: %s" % ut.row2dict(result))                  ## the return value example: {'length': 76L, 'id': 4177L, 'species': 4932L}
+
             return [ut.row2dict(result) for result in query]
 
     def length_match(self, pair):
-        """Determine if the length of the sequences in the pair match.
+        """
+        Determine if the length of the sequences in the pair match.
 
         Parameters
         ----------
@@ -134,15 +143,20 @@ class Loader(core.MassLoader):
             if pair[0]['length'] == pair[1]['length']:
                 # self.logger.info('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
                 return True
-            
+
 
     def species_matches(self, pair):
-        """Check if a pair of sequences have compatible species. This means the
+        """
+        Check if a pair of sequences have compatible species. This means the
         same species or having None and synthetic (32360) assigned.
 
         :param tuple pair: The pair of sequences to compare.
         :returns: The
         """
+
+        # when matching dna, allow species to be different
+        if pair[0]['entity_type'] == 'dna':
+            return True
 
         # common = pair[0]['species'].intersection(pair[1]['species'])
         common = pair[0]['taxonomy_id'].intersection(pair[1]['taxonomy_id'])
@@ -157,8 +171,11 @@ class Loader(core.MassLoader):
 
     @property
     def known(self):
-        """Get all known pairs. This will look up the known pairs the first
+        """
+        Get all known pairs. This will look up the known pairs the first
         time it is used, otherwise it will return the same set of known pairs.
+
+        self.table here refers to the correspondence_info table
 
         :returns: A set of all known pairs. The entries in the set will be of
         the form (exp_seq_id_1, exp_seq_id_2).
@@ -200,7 +217,7 @@ class Loader(core.MassLoader):
         into a single set.
 
         :param list sequences: A list of the sequences loaded from
-        `lookup_sequences`.
+        `look_up_sequences`.
         :returns: A list of unique sequences.
         """
 
@@ -219,7 +236,7 @@ class Loader(core.MassLoader):
         return mapping.values()
 
     def entity_type_matches(self, pair):
-        """Check if a pair of entity_types have compatible species. 
+        """Check if a pair of entity_types have compatible species.
 
         :param tuple pair: The pair of entity_types to compare.
         :returns: The boolean
@@ -230,12 +247,19 @@ class Loader(core.MassLoader):
 
 
     def is_match(self, pair):
-        """Check if a pair of sequences are a pair. A pair is a match if they
+        """
+        Check if a pair of sequences are a pair. A pair is a match if they
         are a good length, have matching sequences and is not known.
 
         :param tuple pair: A pair of sequences to compare.
         :returns: A boolean.
         """
+
+        # if pair[0]['id'] == '7087':
+        #     self.logger.info("pair ids: %s %s" % (pair[0]['id'], pair[1]['id']))
+        #     self.logger.info("pair lengths: %s %s" % (pair[0]['length'], pair[1]['length']))
+        #     self.logger.info("pair species: %s %s" % (pair[0]['taxonomy_id'], pair[1]['taxonomy_id']))
+        #     self.logger.info("pair entity_type: %s %s" % (pair[0]['entity_type'], pair[1]['entity_type']))
 
         return self.length_match(pair) and \
             self.species_matches(pair) and \
@@ -250,7 +274,7 @@ class Loader(core.MassLoader):
         """
 
         self.logger.info("Using %i pdbs", len(pdbs))
-        seqs = it.imap(self.lookup_sequences, pdbs)
+        seqs = it.imap(self.look_up_sequences, pdbs)
         # self.logger.info("show the it.map's result: %s" % seqs)
         seqs = it.chain.from_iterable(seqs)
         seqs = self.unique_sequences(seqs)
@@ -268,7 +292,7 @@ class Loader(core.MassLoader):
         :returns: The list pairs
         """
 
-        seqs = self.sequences(pdbs)                                         ## seqs is a list of dicts with information about sequences.                       
+        seqs = self.sequences(pdbs)                                         ## seqs is a list of dicts with information about sequences.
         # self.logger.info("show the seqs info of pairs function: %s" % seqs[0]) ## [{'species': set(4932), 'length': 76, 'id': 4177, 'entity_type': 'rna'}]
         # pairs = it.combinations(seqs, 2)                                    ## just a guess, the function will return a list of diff combinations if we have more than 2 seqs. Thus, if we only have one seq, it will return [].
         # Thus, the problem is here, we can add condiction for each entity type.
@@ -280,12 +304,12 @@ class Loader(core.MassLoader):
         # pairs = list(rna_pairs)+list(dna_pairs)+list(hybrid_pairs)
         # self.logger.info("Found pairs %s", pairs)                           ## <itertools.combinations object at 0x7fb8f641dec0>
         self_pairs = it.izip(seqs, seqs)                                    ## Ex. [({'species': {4932}, 'length': 76, 'id': 4177, 'entity_type': 'rna'}, {'species': {4932}, 'length': 76, 'id': 4177, 'entity_type': 'rna'})]
-        self.logger.info("Found self_pairs %s", self_pairs)                 ## <itertools.izip object at 0x7fb8f6405bd8>
+        # self.logger.info("Found self_pairs %s" % (self_pairs))                 ## <itertools.izip object at 0x7fb8f6405bd8>
         # return sorted(it.chain.from_iterable([self_pairs, pairs]),
         #               key=lambda p: (p[0]['id'], p[1]['id']))
-        
+
         return sorted(it.chain.from_iterable([self_pairs,rna_pairs,dna_pairs,hybrid_pairs]),
-                      key=lambda p: (p[0]['id'], p[1]['id']))        
+                      key=lambda p: (p[0]['id'], p[1]['id']))
 
     def data(self, pdbs, **kwargs):
         """Compute all new correspondences pairs.
@@ -294,10 +318,23 @@ class Loader(core.MassLoader):
         :returns: A list of pairs to store.
         """
 
+        # get all rna pairs, dna pairs, hydrid pairs, and self pairs
         pairs = self.pairs(pdbs)
+
+        # keep only the ones that are close enough to align and not already in the table
         pairs = it.ifilter(self.is_match, pairs)
+
+        # sort the pairs by the sequence ids, only store them in that order
         pairs = sorted(pairs, key=lambda p: (p[0]['id'], p[1]['id']))
+
+        # convert the pairs into the database format
         pairs = it.imap(self.ids_with_column_names, pairs)
+
+        # convert to list to execute all the steps above
+        self.logger.info("Converting pairs to list")
         pairs = list(pairs)
-        self.logger.info("Found %i new correspondence pairs", len(pairs))
+        self.logger.info("Found %i new correspondence pairs" % len(pairs))
+
+        # these pairs get stored in the correspondence_info table
+        # then they are aligned later
         return pairs
