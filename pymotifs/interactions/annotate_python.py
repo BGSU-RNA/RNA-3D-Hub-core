3d057ab7d695c761c157fd70f344c3634c278582
Matlab Pipeline User <pipeline@rnaprod1.bgsu.edu>
2024-07-20 15:20:38 -0400

    Annotate all pairwise interactions with python code

--- pymotifs/interactions/annotate_python.py
+++ pymotifs/interactions/annotate_python.py
@@ -1,10 +1,7 @@
 """
 Run Python code to annotate pairwise interactions
-and store them in unit_pairs_annotations alongside
-the Matlab annotations.
+and store them in unit_pairs_annotations, noting that program = 'python'
 """
-
-import operator as op
 
 from pymotifs import core
 from pymotifs import models as mod
@@ -14,7 +11,6 @@ from pymotifs.pdbs.info import Loader as PdbLoader
 
 # import these from the interactions directory; in the future, they'll be updated with fr3d-python
 from fr3d.classifiers.NA_pairwise_interactions import annotate_nt_nt_in_structure
-from fr3d.classifiers.class_limits_2023 import nt_nt_cutoffs   # use latest cutoffs
 
 class Loader(core.SimpleLoader):
     allow_no_data = True
@@ -44,23 +40,29 @@ class Loader(core.SimpleLoader):
         with self.session() as session:
             query = session.query(mod.UnitPairsInteractions.pdb_id).\
             filter(mod.UnitPairsInteractions.program == 'python').\
+            filter(mod.UnitPairsInteractions.pdb_id.in_(pdbs)).\
             distinct()
 
             annotated = set([result.pdb_id for result in query])
 
         self.logger.info('Found %d pdb ids already annotated by python' % len(annotated))
 
-        # temporary:  DNA-containing structures
-        with self.session() as session:
-            query = session.query(mod.ChainInfo.pdb_id).\
-            filter(mod.ChainInfo.macromolecule_type == 'DNA').\
-            distinct()
+        # # temporary:  DNA-containing structures
+        # with self.session() as session:
+        #     query = session.query(mod.ChainInfo.pdb_id).\
+        #     filter(mod.ChainInfo.macromolecule_type == 'DNA').\
+        #     distinct()
 
-            DNA = set([result.pdb_id for result in query])
+        #     DNA = set([result.pdb_id for result in query])
 
-        DNA_list = sorted(DNA - annotated)
+        # DNA_list = sorted(DNA - annotated)
 
-        return DNA_list
+        need_to_annotate = set(pdbs) - annotated
+
+        if len(need_to_annotate) == 0:
+            raise core.Skip("No PDB files need pair interactions annotated")
+        else:
+            return sorted(need_to_annotate)
 
 
 
@@ -74,40 +76,46 @@ class Loader(core.SimpleLoader):
         #pdb_list = pdb_list[:3800]
         #pdb_list = pdb_list[3800:]
 
-        if len(pdb_list) == 0:
-            raise core.Skip("No PDB files need pair interactions annotated")
-
         return pdb_list
 
 
     def data(self, pdb, **kwargs):
         """
-        read the .cif file for the pdb id, annotate interactions, store
+        read the .cif file for the pdb id, annotate interactions, store them
         """
 
-        # future:  query the unit_pairs_interactions table to find interactions with this pdb id that are already stored
-        # future:  and only record new interactions
-
-        # interaction categories to process
+        # interaction categories to process; names from NA_pairwise_interactions.py
+        # basepair,basepair_detail,stacking,so,backbone,coplanar,covalent,sugar_ribose,near,bss,loops
         categories = {}
-        categories['sO'] = []
         categories['basepair'] = []
-        categories['coplanar'] = []
+        categories['basepair_detail'] = []
         categories['stacking'] = []
+        categories['sO'] = []
+        categories['backbone'] = []
+        categories['coplanar'] = []
+        categories['covalent'] = []
+        categories['sugar_ribose'] = []
+        categories['bss'] = []
+        categories['loops'] = []
 
         category_to_heading = {}
         category_to_heading['basepair'] = 'f_lwbp'
+        category_to_heading['basepair_detail'] = 'f_lwbp_detail'
         category_to_heading['stacking'] = 'f_stacks'
-        category_to_heading['backbone'] = 'backbone'
+        category_to_heading['backbone'] = ''  # need special processing for BPh, BR
         category_to_heading['sO'] = 'f_so'
         category_to_heading['coplanar'] = 'f_coplanar'
+        category_to_heading['sugar_ribose'] = 'f_sugar_ribose'
+        category_to_heading['bss'] = 'f_bss'
+        category_to_heading['covalent'] = 'f_covalent'
 
         # load the .cif file
         structure = self.structure(pdb)
 
-        # annotate
+        # annotate interactions
         interaction_to_triple_list, category_to_interactions, timerData, pair_to_data = annotate_nt_nt_in_structure(structure,categories)
 
+        # actual categories found
         all_categories = list(set(categories.keys()) & set(category_to_interactions.keys()))
 
         pair_to_dictionary = {}
@@ -116,37 +124,32 @@ class Loader(core.SimpleLoader):
             heading = category_to_heading[category]
 
             for interaction in category_to_interactions[category]:
-                if category == 'basepair':
-                    if len(interaction) == 3:
-                        interaction_simple = interaction[0] + interaction[1:3].upper()
-                    elif len(interaction) == 4:
-                        interaction_simple = interaction[0:2] + interaction[2:4].upper()
-                    else:
-                        interaction_simple = interaction
-                    interaction_detail = interaction
-                else:
-                    interaction_simple = interaction
-                    interaction_detail = None
+                # if category == 'basepair':
+                #     if len(interaction) == 3:
+                #         interaction_simple = interaction[0] + interaction[1:3].upper()
+                #     elif len(interaction) == 4:
+                #         interaction_simple = interaction[0:2] + interaction[2:4].upper()
+                #     else:
+                #         interaction_simple = interaction
+                #     interaction_detail = interaction
+                # else:
+                #     interaction_simple = interaction
+                #     interaction_detail = None
 
-                if 'BPh' in interaction:
-                    heading = 'f_bphs'
-                elif 'BR' in interaction:
-                    heading = 'f_brbs'
+
+
+                if category == 'backbone':
+                    if 'BPh' in interaction:
+                        heading = 'f_bphs'
+                    elif 'BR' in interaction:
+                        heading = 'f_brbs'
 
                 for triple in interaction_to_triple_list[interaction]:
                     u1 = triple[0]
                     u2 = triple[1]
                     pair = u1 + "+" + u2
 
-                    f1 = u1.split("|")
-                    f2 = u2.split("|")
-
-                    b1 = f1[3]
-                    b2 = f2[3]
-
-                    # temporary in 2023-02-27: skip anything Matlab may have already annotated
-                    if b1 in ['A','C','G','U'] and b2 in ['A','C','G','U']:
-                        continue
+                    self.logger.info('Adding interaction %s between %s and %s in %s' % (interaction,u1,u2,pdb))
 
                     if not pair in pair_to_dictionary:
                         pair_to_dictionary[pair] = {}
@@ -154,15 +157,19 @@ class Loader(core.SimpleLoader):
                     pair_to_dictionary[pair]['unit_id_1'] = u1
                     pair_to_dictionary[pair]['unit_id_2'] = u2
                     pair_to_dictionary[pair]['pdb_id'] = pdb
-                    pair_to_dictionary[pair][heading] = interaction_simple
+                    pair_to_dictionary[pair]['program'] = 'python'
+                    if category == 'bss':
+                        # if the pair is here, bss interaction is true
+                        pair_to_dictionary[pair][heading] = 1
+                    else:
+                        pair_to_dictionary[pair][heading] = interaction
 
                     if 'f_crossing' in pair_to_dictionary[pair]:
                         if not pair_to_dictionary[pair]['f_crossing'] == triple[2]:
-                            print('Different crossing numbers for %s and %s interaction %s' % (u1,u2,interaction_simple))
+                            print('Different crossing numbers for %s and %s interaction %s' % (u1,u2,interaction))
                     else:
                         pair_to_dictionary[pair]['f_crossing'] = triple[2]
 
-                    pair_to_dictionary[pair]['program'] = 'python'
 
         # list of annotations to return
         annotations = []
