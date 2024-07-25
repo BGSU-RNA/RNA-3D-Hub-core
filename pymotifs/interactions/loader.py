2038dfdb91799b9e15deb2b9309c8f5892979e3d
Matlab Pipeline User <pipeline@rnaprod1.bgsu.edu>
2024-07-25 13:12:00 -0400

    Run the python interaction loader

--- pymotifs/interactions/loader.py
+++ pymotifs/interactions/loader.py
@@ -13,7 +13,7 @@ import pymotifs.core as core
 from pymotifs.interactions.pairwise import Loader as PairwiseLoader
 from pymotifs.interactions.flanking import Loader as FlankingLoader
 #from pymotifs.interactions.summary import Loader as SummaryLoader # remove 2023-04-19
-from pymotifs.interactions.annotate_python import Loader as AnnotationLoader  # omit 2023-04-19
+from pymotifs.interactions.annotate_python import Loader as AnnotationLoader
 
 
 class Loader(core.StageContainer):
