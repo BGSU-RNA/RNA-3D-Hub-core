bd761b476936bf0739c5ce8a228ed86e692fd5bd
Matlab Pipeline User <pipeline@rnaprod1.bgsu.edu>
2024-07-26 11:46:21 -0400

    Dependency on combined center and rotation stage

--- pymotifs/units/loader.py
+++ pymotifs/units/loader.py
@@ -14,8 +14,9 @@ from pymotifs.units.info import Loader as InfoLoader
 from pymotifs.units.coordinates import Loader as CoordinateLoader
 #from pymotifs.units.distances import Loader as DistancesLoader
 # from pymotifs.units.redundant import RedundantNucleotidesLoader
-from pymotifs.units.centers import Loader as CenterLoader
-from pymotifs.units.rotation import Loader as RotationLoader
+# from pymotifs.units.centers import Loader as CenterLoader
+# from pymotifs.units.rotation import Loader as RotationLoader
+from pymotifs.units.center_rotation import Loader as CenterRotationLoader
 from pymotifs.units.incomplete import Loader as IncompleteLoader
 from pymotifs.units.bond_orientation import Loader as BondOrientationLoader
 
@@ -26,5 +27,5 @@ class Loader(core.StageContainer):
     """
 
     """The `Stages` this will run"""
-    stages = set([InfoLoader, CenterLoader, RotationLoader,
+    stages = set([InfoLoader, CenterRotationLoader,
                   CoordinateLoader, IncompleteLoader, BondOrientationLoader])
