a8cbb8fb56b8d9b4d384e6af457406fd28d7ac98
Matlab Pipeline User <pipeline@rnaprod2.bgsu.edu>
2024-09-11 01:10:54 -0400

    Better commenting

--- pymotifs/nr/class_rank.py
+++ pymotifs/nr/class_rank.py
@@ -17,15 +17,36 @@ from pymotifs.nr.id_mapping import Loader as IdMappingLoader
 class Loader(BaseLoader):
 
     dependencies = set([IdMappingLoader])
-    update_gap = dt.timedelta(7)  # Only update every 7 days
+    # update_gap = dt.timedelta(7)  # Only update every 7 days
     allow_no_data = True
+    mark = False
 
     @property
     def table(self):
         return mod.NrClassRank
 
+
+    def has_data(self, *args, **kwargs):
+        """
+        We always want stages.py to think that data needs to be computed
+        for this stage, so it does not get skipped.
+        This works better than trying to manipulate the query method.
+        """
+        return False
+
+
+    # def nr_class_name_checking(self):
+    #     """
+    #     Look for all existing nr_class_names in the nr_class_rank table.
+    #     """
+    #     with self.session() as session:
+    #         query = session.query(mod.NrClassRank.nr_class_name).distinct()
+    #     return [row.nr_class_name for row in query]
+
+
     def chains(self, grouping):
-        """Compute the chain level data to store. The produced chains will be
+        """
+        Compute the chain level data to store. The produced chains will be
         suitable for writing to the ``nr_class_rank`` table in the database.
 
         Parameters
@@ -41,33 +62,22 @@ class Loader(BaseLoader):
         chains : list
             A list of chain dicts to store.
         """
-        # nr_class_name_list = self.nr_class_name_checking()
+
         data = []
         for group in grouping:
-            # if not group['name']['full'] in nr_class_name_list:
-                for chain in group['members']:
-                    data.append({
-                        'ife_id': chain['id'],
-                        'nr_class_name': group['name']['full'],
-                        'rank': chain['rank'],
-                        'nr_class_id': group['name']['class_id']
-                    })
-            # else:
-            #    pass
+            for chain in group['members']:
+                data.append({
+                    'ife_id': chain['id'],
+                    'nr_class_name': group['name']['full'],
+                    'rank': chain['rank'],
+                    'nr_class_id': group['name']['class_id']
+                })
         return data
 
 
-    def nr_class_name_checking(self):
-        """
-        Look for all existing nr_class_names in the nr_class_rank table.
-
-        """
-        with self.session() as session:
-            query = session.query(mod.NrClassRank.nr_class_name).distinct()
-        return [row.nr_class_name for row in query]
-
     def data(self, release, **kwargs):
-        """Compute the data to store.
+        """
+        Compute the data to store.
 
         Parameters
         ----------
