16a07e4f5c64850dad5b520bffb37be1546a1598
Matlab Pipeline User <pipeline@rnaprod2.bgsu.edu>
2024-12-04 16:55:18 -0500

    Remove positions.py and use save_loops.py

--- pymotifs/loops/loader.py
+++ pymotifs/loops/loader.py
@@ -1,10 +1,12 @@
 from pymotifs import core
 
-from pymotifs.loops.extractor import Loader as Extractor
-from pymotifs.loops.positions import Loader as PositionLoader
+# from pymotifs.loops.extractor import Loader as Extractor
+# from pymotifs.loops.positions import Loader as PositionLoader
+from pymotifs.loops.save_loops import Loader as Saver
 from pymotifs.loops.quality import Loader as QALoader
 # from pymotifs.loops.add_modified_nucleotides import Loader as ModifiedNucleotidesLoader
 
 
 class Loader(core.StageContainer):
-    stages = set([Extractor, PositionLoader, QALoader])
+    # stages = set([Extractor, PositionLoader, QALoader])
+    stages = set([Saver, QALoader])
