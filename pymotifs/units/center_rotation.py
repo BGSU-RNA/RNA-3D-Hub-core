11aa17a2bcc0324104d1c15fa5b52efd28d013df
Matlab Pipeline User <pipeline@rnaprod2.bgsu.edu>
2025-03-12 13:44:47 -0400

    Selectively add missing centers

--- pymotifs/units/center_rotation.py
+++ pymotifs/units/center_rotation.py
@@ -15,7 +15,11 @@ class Loader(core.SimpleLoader):
     dependencies = set([InfoLoader])
     allow_no_data = True
 
+    mark = False
+    use_marks = False
+
     # check for units that were missed and fill them in
+    fill_in_missing = True
     fill_in_missing = False
 
     def to_process(self, pdbs, **kwargs):
@@ -28,7 +32,7 @@ class Loader(core.SimpleLoader):
             return pdbs
 
             # takes too much memory to try to find all unit ids that don't
-            # have a center
+            # have a center all at once; do it file by file.
 
         else:
             existing_ids = set()
@@ -43,7 +47,7 @@ class Loader(core.SimpleLoader):
             self.logger.info("Found %d existing ids and %d pdbs to check" % (len(existing_ids),len(pdbs_to_check)))
 
         if len(pdbs_to_check) == 0:
-            raise core.Skip("All pdb ids already represented in unit_rotations table")
+            raise core.Skip("All pdb ids are already represented in unit_centers table")
 
         return sorted(pdbs_to_check)
 
@@ -58,7 +62,7 @@ class Loader(core.SimpleLoader):
         """
 
         if self.fill_in_missing:
-            # return an empty query so we process all pdb ids
+            # return an empty query so we process pdb
             return session.query(mod.UnitCenters).filter_by(pdb_id='nonexistent_pdb_id')
         else:
             return session.query(mod.UnitCenters).filter_by(pdb_id=pdb)
@@ -73,22 +77,30 @@ class Loader(core.SimpleLoader):
         for the corresponding tables
         """
 
+        self.logger.info('Processing %s' % pdb)
+
+        center_to_unit_id = {}
+        center_to_unit_id['glycosidic'] = set()
+
         if self.fill_in_missing:
+            self.logger.info('Processing %s' % pdb)
+
             # find unit ids in this pdb that have a center
             with self.session() as session:
                 query = session.query(mod.UnitCenters).filter_by(pdb_id=pdb)
-                has_center = set([u.unit_id for u in query])
+
+                for u in query:
+                    if not u.name in center_to_unit_id:
+                        center_to_unit_id[u.name] = set()
+                    center_to_unit_id[u.name].add(u.unit_id)
 
             # find all unit ids in this pdb
             with self.session() as session:
                 query = session.query(mod.UnitInfo.unit_id).filter_by(pdb_id=pdb)
                 unit_ids = set([u.unit_id for u in query])
 
-            if len(unit_ids - has_center) == 0:
+            if len(unit_ids - center_to_unit_id['glycosidic']) == 0:
                 raise core.Skip("All unit ids in %s already have centers" % pdb)
-
-        else:
-            has_center = set()
 
         structure = self.structure(pdb)
 
@@ -97,17 +109,19 @@ class Loader(core.SimpleLoader):
             # if the unit has any center at all, skip it
             # if we ever have to add some new center, this logic will need to change
 
-            if residue.unit_id() in has_center:
-                continue
-
             for name in residue.centers.definitions():
+
+                if name in center_to_unit_id and residue.unit_id() in center_to_unit_id[name]:
+                    # already have this center, so don't add it again
+                    continue
+
                 center = residue.centers[name]
 
-                if self.fill_in_missing:
-                    self.logger.info("Adding center for %s value %s" % (residue.unit_id(), center))
-                    print("Adding center for %s value %s" % (residue.unit_id(), center))
-
                 if len(center) == 3:
+                    if self.fill_in_missing:
+                        self.logger.info("Adding center %s for %s value %s" % (residue.unit_id(), name, center))
+                        print("Adding center %s for %s value %s" % (residue.unit_id(), name, center))
+
                     yield mod.UnitCenters(unit_id=residue.unit_id(),
                                           name=name,
                                           pdb_id=pdb,
@@ -115,20 +129,23 @@ class Loader(core.SimpleLoader):
                                           y=float(center[1]),
                                           z=float(center[2]))
 
-            if hasattr(residue, 'rotation_matrix'):
-                matrix = residue.rotation_matrix
-                # if there are not enough atoms for the rotation matrix, it will be None
-                if matrix is not None:
-                    c += 1
-                    yield mod.UnitRotations(unit_id=residue.unit_id(),
-                                            pdb_id=pdb,
-                                            cell_0_0=float(matrix[0, 0]),
-                                            cell_0_1=float(matrix[0, 1]),
-                                            cell_0_2=float(matrix[0, 2]),
-                                            cell_1_0=float(matrix[1, 0]),
-                                            cell_1_1=float(matrix[1, 1]),
-                                            cell_1_2=float(matrix[1, 2]),
-                                            cell_2_0=float(matrix[2, 0]),
-                                            cell_2_1=float(matrix[2, 1]),
-                                            cell_2_2=float(matrix[2, 2]))
+                if name == 'glycosidic':
+                    # only add the rotation matrix after adding a glycosidic atom
+                    # then it only gets added once, right?
+                    if hasattr(residue, 'rotation_matrix'):
+                        matrix = residue.rotation_matrix
+                        # if there are not enough atoms for the rotation matrix, it will be None
+                        if matrix is not None:
+                            c += 1
+                            yield mod.UnitRotations(unit_id=residue.unit_id(),
+                                                    pdb_id=pdb,
+                                                    cell_0_0=float(matrix[0, 0]),
+                                                    cell_0_1=float(matrix[0, 1]),
+                                                    cell_0_2=float(matrix[0, 2]),
+                                                    cell_1_0=float(matrix[1, 0]),
+                                                    cell_1_1=float(matrix[1, 1]),
+                                                    cell_1_2=float(matrix[1, 2]),
+                                                    cell_2_0=float(matrix[2, 0]),
+                                                    cell_2_1=float(matrix[2, 1]),
+                                                    cell_2_2=float(matrix[2, 2]))
 
