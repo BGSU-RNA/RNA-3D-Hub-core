c20a91581799390218e71f33205c4f06f88cc855
Matlab Pipeline User <pipeline@rnaprod2.bgsu.edu>
2025-06-02 15:56:05 -0400

    Load assembly information on each run

--- pymotifs/pdbs/loader.py
+++ pymotifs/pdbs/loader.py
@@ -1,16 +1,17 @@
-"""Load all PDB level infomation. This includes the folowing stages:
+"""
+Load all PDB level infomation. This includes the folowing stages:
 
-pdbs.info
-    Fetch the basic information about a PDB.
+pdbs.info:    Fetch the basic information about a PDB.
+assemblies:   Which chains and symmetries are in which assemblies
 """
 
 import pymotifs.core as core
 
 from pymotifs.pdbs.info import Loader as InfoLoader
+from pymotifs.pdbs.assemblies import Loader as AssemblyLoader
 
 
 class Loader(core.StageContainer):
     """The loader for all PDB level data."""
 
-    stages = set([InfoLoader])
-    """Stages to run."""
+    stages = set([InfoLoader,AssemblyLoader])
