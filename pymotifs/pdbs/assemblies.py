11d0288a870d243671a835cddd2d03f92dc90dde
Matlab Pipeline User <pipeline@rnaprod2.bgsu.edu>
2025-06-02 15:57:03 -0400

    Read assembly information, convert PDB chains to author chains

--- pymotifs/pdbs/assemblies.py
+++ pymotifs/pdbs/assemblies.py
@@ -3,24 +3,20 @@ Read .cif files to get assembly information.
 Read the assembly_gen and struct_oper tables
 """
 
-# import requests
 from pymotifs import core
 from pymotifs import models as mod
-# from pymotifs.utils.pdb import CustomReportHelper
 from pymotifs.download import Downloader
-from pymotifs.pdbs.loader import Loader as PdbLoader
+from pymotifs.pdbs.info import Loader as InfoLoader
 
 from fr3d.cif.reader import Cif
 
 
 class Loader(core.SimpleLoader):
-    """
-    """
 
     merge_data = True
     allow_no_data = True
 
-    dependencies = set([Downloader, PdbLoader])
+    dependencies = set([Downloader, InfoLoader])
 
     @property
     def table(self):
@@ -29,7 +25,8 @@ class Loader(core.SimpleLoader):
 
     def to_process(self, pdbs, **kwargs):
         """
-        Return all PDB ids the first time.
+        From among pdbs, find the ones that do not have assembly information
+        in the assembly_info table.
 
         :param list pdbs: The list of pdb ids of interest on this run.
         :param dict kwargs: The keyword arguments which are ignored.
@@ -39,13 +36,18 @@ class Loader(core.SimpleLoader):
         if len(pdbs) == 1:
             return pdbs
 
-        # process all pdb files
+        # query to find pdb ids that already have assembly information
         with self.session() as session:
-            query = session.query(mod.PdbInfo.pdb_id).\
-            distinct()
-        pdbs = [row.pdb_id for row in query]
+            query = session.query(mod.AssemblyInfo.pdb_id).\
+                distinct()
+            pdbs_with_assembly = set([row.pdb_id for row in query])
 
-        return pdbs
+        pdbs_needed = set(pdbs) - pdbs_with_assembly
+
+        if len(pdbs_needed) == 0:
+            raise core.Skip("No pdbs need assembly data loaded")
+
+        return sorted(pdbs_needed)
 
 
     def has_data(self, pdb, **kwargs):
@@ -98,32 +100,82 @@ class Loader(core.SimpleLoader):
         # open entire cif file, not just residues
         cif_access = self.cif(pdb)
 
+        # loop over residues to build the mapping from PDB chain to author chain
+        pdb_chain_to_author_chain = {}
+        c = 0
+        for atom_site in cif_access.table('atom_site'):
+            pdb_chain = atom_site['label_asym_id']
+            author_chain = atom_site['auth_asym_id']
+            pdb_chain_to_author_chain[pdb_chain] = author_chain
+            c += 1
+
+        print(sorted(pdb_chain_to_author_chain.items()))
+        self.logger.info(sorted(pdb_chain_to_author_chain.items()))
+        # print('Processed %d lines of ATOM records' % c)
+
         # To get the values of oper_expression, read this table:
         symmetry_id_to_name = {}
         operators = cif_access.table('pdbx_struct_oper_list').rows
         for operator in operators:
             print(operator)
             id = operator['id']
-            name = operator['name']
+            name = operator.get('name',None)
             symmetry_id_to_name[id] = name
+
+        # print(symmetry_id_to_name)
 
         # t.rows is a list of dictionaries with these keys: 'assembly_id', 'oper_expression', 'asym_id_list'
         assemblies = cif_access.table('pdbx_struct_assembly_gen').rows
 
+        max_assembly_id = 0
         for assembly in assemblies:
-            print(assembly)
+            print("Working on assembly %s in %s" % (assembly,pdb))
 
-            chains = assembly["asym_id_list"].split(",")
-            symmetry_ids = assembly["oper_expression"].split(",")
+            pdb_chains = assembly["asym_id_list"].split(",")
 
-            for chain in chains:
-                for symmetry_id in symmetry_ids:
-                    data = {}
-                    data["pdb_id"] = pdb
-                    data["assembly_id"] = int(assembly['assembly_id'])     # integer
-                    data["chain_name"] = chain
-                    data["symmetry_id"] = int(symmetry_id)     # integer
-                    data["symmetry"] = symmetry_id_to_name[symmetry_id]
+            try:
+                assembly_id = int(assembly['assembly_id'])
+            except:
+                assembly_id = max_assembly_id + 1
+
+            max_assembly_id = max(max_assembly_id, assembly_id)
+
+            if "(" in assembly["oper_expression"] or "P" in assembly["oper_expression"]:
+                symmetry_ids = assembly["oper_expression"]
+            else:
+                symmetry_ids = assembly["oper_expression"].split(",")
+
+            chain_symmetry_seen = set()
+
+            for pdb_chain in pdb_chains:
+                if pdb_chain in pdb_chain_to_author_chain:
+                    author_chain = pdb_chain_to_author_chain[pdb_chain]
+                    self.logger.info("Mapped pdb_chain %s to author_chain %s" % (pdb_chain,author_chain))
+                else:
+                    self.logger.info("No mapping to author chain for pdb_chain %s" % pdb_chain)
+                    continue
+
+                data = {}
+                data["pdb_id"] = pdb
+                data["chain_name"] = author_chain
+                data["assembly_id"] = max_assembly_id
+
+                if type(symmetry_ids) == list:
+                    for symmetry_id in symmetry_ids:
+                        chain_symmetry = author_chain + "&" + symmetry_id
+                        if not chain_symmetry in chain_symmetry_seen:
+                            # only add each author chain once, even if multiple different pdb chains for it
+                            chain_symmetry_seen.add(chain_symmetry)
+                            data["symmetry_id"] = symmetry_id
+                            data["symmetry"] = symmetry_id_to_name[symmetry_id]
+
+                            print(data)
+
+                            yield(mod.AssemblyInfo(**data))
+                else:
+                    # complex operator expression, just store that as the name of the symmetry
+                    data["symmetry_id"] = '0'
+                    data["symmetry"] = symmetry_ids
 
                     print(data)
 
