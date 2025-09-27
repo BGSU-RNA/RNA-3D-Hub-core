5a763497b59ad7dcf52888b296685a3360723473
Matlab Pipeline User <pipeline@rnaprod2.bgsu.edu>
2025-09-26 20:38:36 -0400

    Extend to J4 and up, even if no data

--- pymotifs/motif_atlas/discrepancies.py
+++ pymotifs/motif_atlas/discrepancies.py
@@ -10,20 +10,31 @@ from pymotifs.motif_atlas.release import Loader as ReleaseLoader
 
 class Loader(core.SimpleLoader):
     dependencies = set([ReleaseLoader])
+
     @property
     def table(self):
         return mod.MlMutualDiscrepancy
 
     def to_process(self, *args, **kwargs):
-        return ReleaseLoader.types
+        if 'loop_type' in kwargs.get('manual', {}):
+            loop_types = kwargs['manual']['loop_type'].split(',')
+        else:
+            loop_types = ReleaseLoader.types
+        return loop_types
 
     def query(self, session, loop_type, **kwargs):
         data = self.cached(loop_type)
+        if data:
+            release = data['release']
+        else:
+            # early J8 releases, for example, had no data
+            release = 'No release'
+
         return session.query(mod.MlMutualDiscrepancy).\
             join(mod.LoopInfo,
                  mod.LoopInfo.loop_id == mod.MlMutualDiscrepancy.loop_id_1).\
             filter(mod.LoopInfo.type == loop_type).\
-            filter(mod.MlMutualDiscrepancy.ml_release_id == data['release'])
+            filter(mod.MlMutualDiscrepancy.ml_release_id == release)
 
     def discrepancies(self, cached):
         data = []
@@ -36,5 +47,9 @@ class Loader(core.SimpleLoader):
     def data(self, loop_type, **kwargs):
         cached = self.cached(loop_type)
         if not cached:
-            raise core.InvalidState("No data cached")
-        return self.discrepancies(cached)
+            raise core.Skip("No %s data cached" % loop_type)
+        discrepancy_data = self.discrepancies(cached)
+        if discrepancy_data:
+            return discrepancy_data
+        else:
+            raise core.Skip("No discrepancy data found, probably because the release is empty")
